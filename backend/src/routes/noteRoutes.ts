17652978d328c80e9a960ab2c229ddd3380d7b96
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-06-21 17:15:14 +0100

    feat(API): add a note route endpoint for creating new note for a specific video

--- backend/src/routes/noteRoutes.ts
+++ backend/src/routes/noteRoutes.ts
@@ -1,15 +1,25 @@
 import { Router } from "express";
+import checkVideoExists from "src/middlewares/checkVideoExists";
 
 import { getVideoNotes } from "../controllers/noteControllers";
 
 const router = Router();
 
 /**
- * Route for retrieving notes for a specific video.
+ * Retrieves notes for a specific video.
+ *
  * @route GET /videos/:video_id/notes
+ * @returns {Array<Note>} An array of notes for the specified video.
  */
 router.route("/videos/:video_id/notes").get(getVideoNotes);
 
-// router.route("/videos/:video_id/notes").post(checkVideoExists, createVideoNote);
+/**
+ * Creates a new note for a specific video.
+ *
+ * @route POST /videos/:video_id/notes
+ * @param {Note} noteData - The data of the note to be created.
+ * @returns {Note} The created note.
+ */
+router.route("/videos/:video_id/notes").post(checkVideoExists, createVideoNote);
 
 export default router;
