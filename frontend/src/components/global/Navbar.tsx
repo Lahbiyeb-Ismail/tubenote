865d0259b3a5739b2da7d6894db4723741af3f6d
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-06-23 13:17:45 +0100

    feat(UI): create and add the Navbar component to the layout page

---
+++ frontend/src/components/global/Navbar.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import { Button } from "./Button";
+import MaxWidthWrapper from "./MaxWidthWrapper";
+import TubeNoteLogo from "./TubenoteLogo";
+
+function Navbar() {
+  return (
+    <header className="sticky inset-x-0 top-0 z-[100] h-14 w-full backdrop-blur-lg transition-all">
+      <MaxWidthWrapper>
+        <nav className="flex h-14 items-center justify-between">
+          <TubeNoteLogo />
+
+          <Button href="/dashboard" size="md">
+            Get Started
+          </Button>
+        </nav>
+      </MaxWidthWrapper>
+    </header>
+  );
+}
+
+export default Navbar;
