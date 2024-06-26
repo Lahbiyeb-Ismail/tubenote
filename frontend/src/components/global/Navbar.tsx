7e9c9ca06b950d2941c9837201d57f9899b63bbe
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-06-26 12:19:05 +0100

    feat: update Navbar component with authentication functionality and dynamic buttons

--- frontend/src/components/global/Navbar.tsx
+++ frontend/src/components/global/Navbar.tsx
@@ -1,19 +1,39 @@
 import React from "react";
+import {
+  getKindeServerSession,
+  LoginLink,
+} from "@kinde-oss/kinde-auth-nextjs/server";
 
 import { Button } from "./Button";
 import MaxWidthWrapper from "./MaxWidthWrapper";
 import TubeNoteLogo from "./TubenoteLogo";
 
-function Navbar() {
+async function Navbar() {
+  const { getUser, isAuthenticated } = getKindeServerSession();
+
+  const isAuth = await isAuthenticated();
+  const user = await getUser();
+
   return (
     <header className="sticky inset-x-0 top-0 z-[100] h-14 w-full backdrop-blur-lg transition-all">
       <MaxWidthWrapper>
         <nav className="flex h-14 items-center justify-between">
           <TubeNoteLogo />
 
-          <Button href="/dashboard" size="md">
-            Get Started
-          </Button>
+          {isAuth && user ? (
+            <div>
+              <Button href="/dashboard" size="md" className="mr-3">
+                Dashboard
+              </Button>
+              <Button href="/api/auth/logout" size="md" variant="secondary">
+                Logout
+              </Button>
+            </div>
+          ) : (
+            <Button href="/api/auth/login" size="md">
+              Get Started
+            </Button>
+          )}
         </nav>
       </MaxWidthWrapper>
     </header>
