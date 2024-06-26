ac420fcfa4fc623799508f18297c47c1cdf421d4
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-06-26 12:10:31 +0100

    feat: add authentication functionality using kind

--- frontend/src/sections/HeroSection.tsx
+++ frontend/src/sections/HeroSection.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable tailwindcss/no-custom-classname */
 import React from "react";
 import { ArrowRightIcon } from "@radix-ui/react-icons";
 import { Video } from "lucide-react";
@@ -5,21 +6,21 @@ import { Video } from "lucide-react";
 import { Button } from "@/components/global/Button";
 import MaxWidthWrapper from "@/components/global/MaxWidthWrapper";
 
-function HeroSection() {
+async function HeroSection() {
   return (
     <MaxWidthWrapper className="mx-0">
       <section className="flex flex-col justify-center">
         <div className="mx-auto max-w-screen-xl px-4 py-8 text-center lg:px-12 lg:py-16">
-          <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
+          <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
             Unlock the Power of{" "}
             <span className="text-primary">Video Learning</span>{" "}
           </h1>
-          <p className="mb-8 text-lg font-normal text-gray-500 dark:text-gray-400 sm:px-16 lg:text-xl xl:px-48">
+          <p className="mb-8 text-lg font-normal text-gray-500 sm:px-16 lg:text-xl xl:px-48 dark:text-gray-400">
             Unleash your inner note-taking powers with TubeNote - the ultimate
             sidekick for conquering knowledge from the video galaxies.
           </p>
           <div className="mb-8 flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-x-4 sm:space-y-0 lg:mb-16">
-            <Button href="/search">
+            <Button href="/api/auth/login">
               Get Started
               <ArrowRightIcon className="-mr-1 ml-2 size-5" />
             </Button>
