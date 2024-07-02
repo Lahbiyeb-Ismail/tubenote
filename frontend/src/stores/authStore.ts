bec2db055c638837425a91747927d50f7b774a13
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-07-02 19:09:40 +0100

    feat(UI): add zustand/middleware for persistent storage in authStore

--- frontend/src/stores/authStore.ts
+++ frontend/src/stores/authStore.ts
@@ -1,17 +1,26 @@
 import type { UserDataType } from "@/types";
 import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
 
 interface AuthData {
   userData: UserDataType | null;
   setAuthData: (data: UserDataType) => void;
 }
 
-const useAuthStore = create<AuthData>((set) => ({
-  userData: null,
-  setAuthData: (data) =>
-    set(() => {
-      return { userData: data };
+export const useAuthStore = create<AuthData>()(
+  persist(
+    (set) => ({
+      userData: null,
+      setAuthData: (data) =>
+        set(() => {
+          return { userData: data };
+        }),
     }),
-}));
+    {
+      name: "userData",
+      storage: createJSONStorage(() => sessionStorage),
+    }
+  )
+);
 
 export default useAuthStore;
