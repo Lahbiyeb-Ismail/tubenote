1d737d3a5536658c8ffa34cf05d21ac083919292
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-09-24 17:00:45 +0100

    feat(UI): add form control and validation using react-hook-form and zod

--- frontend/src/app/(auth)/login/page.tsx
+++ frontend/src/app/(auth)/login/page.tsx
@@ -2,6 +2,9 @@
 
 import Link from "next/link";
 
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import {
@@ -12,16 +15,26 @@ import {
 	FormLabel,
 	FormMessage,
 } from "@/components/ui/form";
-
 import AuthLayout from "@/components/auth/AuthLayout";
 
+import { type LoginFormData, loginFormSchema } from "@/lib/schemas";
+
 export default function LoginPage() {
+	const form = useForm<LoginFormData>({
+		resolver: zodResolver(loginFormSchema),
+		defaultValues: {
+			email: "",
+			password: "",
+		},
+	});
+
 	return (
 		<AuthLayout error="" pageTitle="Login">
-			<Form>
+			<Form {...form}>
 				<form onSubmit={() => {}} className="space-y-4">
 					<FormField
 						name="email"
+						control={form.control}
 						render={({ field }) => (
 							<FormItem>
 								<FormLabel>Email</FormLabel>
@@ -34,6 +47,7 @@ export default function LoginPage() {
 					/>
 					<FormField
 						name="password"
+						control={form.control}
 						render={({ field }) => (
 							<FormItem>
 								<FormLabel>Password</FormLabel>
