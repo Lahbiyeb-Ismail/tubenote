7e601c3caf4c8f1e0ecfe57a402c7d413ec4d471
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-10-04 17:14:52 +0100

    refactor(UI): update LoggedOutButtons component design

--- frontend/src/components/Navbar/LoggedOutButtons.tsx
+++ frontend/src/components/Navbar/LoggedOutButtons.tsx
@@ -1,10 +1,16 @@
-import { LinkButton } from "../global/LinkButton";
+import Link from "next/link";
+import { Button } from "../ui/button";
 
 function LoggedOutButtons() {
 	return (
-		<LinkButton href="/api/auth/login" size="md">
-			Get Started
-		</LinkButton>
+		<div className="hidden md:block">
+			<Button
+				size="lg"
+				className="text-lg bg-gradient-to-r from-red-600 to-purple-600 text-white hover:bg-white hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-red-600 hover:to-purple-600 border-2 border-transparent hover:border-red-600 transition-all duration-300"
+			>
+				<Link href="/login">Get Started</Link>
+			</Button>
+		</div>
 	);
 }
 
