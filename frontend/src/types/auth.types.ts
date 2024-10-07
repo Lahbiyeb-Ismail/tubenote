dbdb7f55cecb4bb2b2222b58929e35fdc8870e36
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-10-07 17:38:38 +0100

    refactor(auth): update AuthState type and add AuthAction types

--- frontend/src/types/auth.types.ts
+++ frontend/src/types/auth.types.ts
@@ -1,9 +1,15 @@
 import type { z } from 'zod';
 
 import type { loginFormSchema, registerFormSchema } from '@/lib/schemas';
+import type { ReactNode } from 'react';
 
 export type LoginFormData = z.infer<typeof loginFormSchema>;
 export type RegisterFormData = z.infer<typeof registerFormSchema>;
+
+type User = {
+  username: string;
+  email: string;
+};
 
 export type RegisterResponse = {
   message: string;
@@ -13,8 +19,32 @@ export type RegisterResponse = {
 export type LoginResponse = {
   message: string;
   accessToken: string;
-  user: {
-    username: string;
-    email: string;
-  };
+  user: User;
+};
+
+export type AuthState = {
+  accessToken: string | null;
+  errorMessage?: string;
+  successMessage?: string;
+  isAuthenticated: boolean;
+  user: User | null;
+};
+
+export type AuthAction =
+  | {
+      type: 'LOGIN_SUCCESS';
+      payload: { message: string; accessToken: string; user: User };
+    }
+  | { type: 'REGISTER_SUCCESS'; payload: { successMessage: string } }
+  | { type: 'REQUEST_FAIL'; payload: { errorMessage: string } };
+
+export type AuthContextType = {
+  state: AuthState;
+  login: (loginCredentials: LoginFormData) => void;
+  register: (registerCredentials: RegisterFormData) => void;
+  isLoading: boolean;
+};
+
+export type AuthProviderProps = {
+  children: ReactNode;
 };
