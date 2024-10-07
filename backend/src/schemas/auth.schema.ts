3069ccee5ecd9f6a7af239f24f9bd000ae3d3339
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-10-07 16:49:48 +0100

    feat(API): add loginSchema for the login route

--- backend/src/schemas/auth.schema.ts
+++ backend/src/schemas/auth.schema.ts
@@ -11,3 +11,12 @@ export const registrationSchema = {
       .min(8, { message: 'Password must be at least 8 characters long.' }),
   }),
 };
+
+export const loginSchema = {
+  body: z.object({
+    email: z.string().email({ message: 'Invalid email address.' }),
+    password: z
+      .string()
+      .min(8, { message: 'Password must be at least 8 characters long.' }),
+  }),
+};
