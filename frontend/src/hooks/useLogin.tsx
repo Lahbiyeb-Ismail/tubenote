bf14fb1c08f1689d833a169cc9063f78e7b69eb3
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-10-07 17:11:29 +0100

    feat(hooks): add useLogin hook for user login functionality

---
+++ frontend/src/hooks/useLogin.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+import { loginUser } from "@/actions/auth.actions";
+
+import type { TypedError } from "@/types";
+
+function useLogin() {
+	const queryClient = useQueryClient();
+	const router = useRouter();
+
+	return useMutation({
+		mutationFn: loginUser,
+		onSuccess: (data) => {
+			queryClient.invalidateQueries({ queryKey: ["user"] });
+
+			console.log(data);
+			// router.push("/");
+		},
+		onError: (error: TypedError) => {
+			if (error.response) {
+				console.log(error.response.data.message);
+			} else {
+				console.log("Login failed. Please try again.");
+			}
+		},
+	});
+}
+
+export default useLogin;
