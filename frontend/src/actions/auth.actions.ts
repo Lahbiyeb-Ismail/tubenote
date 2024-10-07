d65b147f529dfa5c01453a4375c67e9f46202d19
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-10-07 16:59:47 +0100

    feat(auth): add loginUser function for user login functionality

--- frontend/src/actions/auth.actions.ts
+++ frontend/src/actions/auth.actions.ts
@@ -1,6 +1,11 @@
 import axios from 'axios';
 
-import type { RegisterFormData, RegisterResponse } from '@/types/auth.types';
+import type {
+  RegisterFormData,
+  RegisterResponse,
+  LoginFormData,
+  LoginResponse,
+} from '@/types/auth.types';
 import { API_URL } from '@/utils/constants';
 
 /**
@@ -15,6 +20,23 @@ export async function registerUser(
   const response = await axios.post(
     `${API_URL}/auth/register`,
     registerCredentials
+  );
+
+  return response.data;
+}
+
+/**
+ * Logs in a user with the provided login credentials.
+ *
+ * @param loginCredentials - The login form data containing user credentials.
+ * @returns A promise that resolves to the login response.
+ */
+export async function loginUser(
+  loginCredentials: LoginFormData
+): Promise<LoginResponse> {
+  const response = await axios.post(
+    `${API_URL}/auth/register`,
+    loginCredentials
   );
 
   return response.data;
