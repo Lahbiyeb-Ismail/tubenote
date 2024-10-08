3501fba2f4a57287e475d933762107efacaeb127
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-10-08 17:03:50 +0100

    feat(UI): add AddNoteForm component to NotesPage

--- frontend/src/app/(dashboard)/notes/page.tsx
+++ frontend/src/app/(dashboard)/notes/page.tsx
@@ -1,9 +1,15 @@
+import AddNoteForm from "@/components/dashboards/AddNoteForm";
 import Header from "@/components/dashboards/Header";
 
 function NotesPage() {
 	return (
 		<div className="min-h-screen flex-1 bg-gray-100">
 			<Header title="Your Video Notes" />
+			<main className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+				<div className="flex justify-end">
+					<AddNoteForm />
+				</div>
+			</main>
 		</div>
 	);
 }
