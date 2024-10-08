53a9da32ce47e3822d922d8570f4ca9487f88617
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-10-08 15:45:31 +0100

    feat(UI): add RecentUserActions component and update DashboardPage
    Add the RecentUserActions component to display recent user actions on the DashboardPage.

--- frontend/src/app/(dashboard)/dashboard/page.tsx
+++ frontend/src/app/(dashboard)/dashboard/page.tsx
@@ -1,11 +1,14 @@
 import DashboardHeader from "@/components/dashboards/DashboardHeader";
 import UserStats from "@/components/dashboards/UserStats";
+import RecentUserActions from "@/components/dashboards/RecentUserActions";
 
 function DashboardPage() {
 	return (
 		<div className="flex-grow p-8">
 			<DashboardHeader />
 			<UserStats />
+			{/* <div className="w-[90%] bg-slate-400 h-[1px] mx-auto" /> */}
+			<RecentUserActions />
 		</div>
 	);
 }
