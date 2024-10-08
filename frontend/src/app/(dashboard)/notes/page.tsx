20d7e7af210f8a351c348dd4d3519abbe06be721
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-10-08 15:54:28 +0100

    feat(UI): add Notes dasboard page with the Header component

---
+++ frontend/src/app/(dashboard)/notes/page.tsx
@@ -0,0 +1,11 @@
+import Header from "@/components/dashboards/Header";
+
+function NotesPage() {
+	return (
+		<div className="min-h-screen flex-1 bg-gray-100">
+			<Header title="Your Video Notes" />
+		</div>
+	);
+}
+
+export default NotesPage;
