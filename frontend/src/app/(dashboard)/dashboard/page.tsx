85b2f143cb1179742acd8e56bd14e233284980e6
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-10-08 15:31:54 +0100

    feat(UI): add UserStats component to DashboardPage

--- frontend/src/app/(dashboard)/dashboard/page.tsx
+++ frontend/src/app/(dashboard)/dashboard/page.tsx
@@ -1,9 +1,11 @@
 import DashboardHeader from "@/components/dashboards/DashboardHeader";
+import UserStats from "@/components/dashboards/UserStats";
 
 function DashboardPage() {
 	return (
 		<div className="flex-grow p-8">
 			<DashboardHeader />
+			<UserStats />
 		</div>
 	);
 }
