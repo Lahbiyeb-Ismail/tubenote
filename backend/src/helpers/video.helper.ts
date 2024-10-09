f2f021639bc92804dd5e6d14599c23d497e0095c
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-10-09 16:21:18 +0100

    refactor: update getVideoData to include user authentication and save video data to the database

--- backend/src/helpers/video.helper.ts
+++ backend/src/helpers/video.helper.ts
@@ -1,4 +1,7 @@
+import type { Response } from 'express';
 import envConfig from '../config/envConfig';
+import httpStatus from 'http-status';
+import prismaClient from '../lib/prisma';
 
 const YOUTUBE_API_URL = envConfig.youtube.api.url;
 const YOUTUBE_API_KEY = envConfig.youtube.api.key;
@@ -26,4 +29,55 @@ export async function getYoutubeVideoData(videoId: string) {
     console.error('Error fetching video description:', error);
     return undefined;
   }
+}
+
+/**
+ * Saves YouTube video data to the database.
+ *
+ * @param videoId - The ID of the YouTube video.
+ * @param userId - The ID of the user saving the video.
+ * @param res - The HTTP response object.
+ * @returns The saved video data.
+ *
+ * @throws Will return a 404 status if no video is found with the provided ID.
+ */
+export async function saveVideoData(
+  videoId: string,
+  userId: string,
+  res: Response
+) {
+  const videoData = await getYoutubeVideoData(videoId);
+
+  if (!videoData.length) {
+    res.status(httpStatus.NOT_FOUND).json({
+      message:
+        'No Video found with the provided id. Please provide a valid video id.',
+    });
+    return;
+  }
+
+  const { snippet, statistics, player } = videoData[0];
+
+  const video = await prismaClient.video.create({
+    data: {
+      userId,
+      youtubeId: videoId,
+      snippet: {
+        title: snippet.title,
+        categoryId: snippet.categoryId,
+        channelId: snippet.channelId,
+        channelTitle: snippet.channelTitle,
+        description: snippet.description,
+        liveBroadcastContent: snippet.liveBroadcastContent,
+        publishedAt: snippet.publishedAt,
+        defaultAudioLanguage: snippet.defaultAudioLanguage,
+        tags: snippet.tags,
+        thumbnails: snippet.thumbnails,
+      },
+      statistics,
+      player,
+    },
+  });
+
+  return video;
 }
