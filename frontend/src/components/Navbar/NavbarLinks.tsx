8c3f1c92e3cfa37ea308ad631235b4410befbd5e
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-10-09 18:22:03 +0100

    feat(UI): add NavbarLinks component to display navigation links in the Navbar

---
+++ frontend/src/components/Navbar/NavbarLinks.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+
+import { navItems } from "@/utils/constants";
+import NavLink from "./NavLink";
+
+function NavbarLinks() {
+	const pathname = usePathname();
+
+	return (
+		<div className="flex items-center justify-evenly flex-grow">
+			{navItems.map((item) => (
+				<NavLink
+					key={item.name}
+					name={item.name}
+					icon={item.icon}
+					href={item.href}
+					pathname={pathname}
+				/>
+			))}
+		</div>
+	);
+}
+
+export default NavbarLinks;
