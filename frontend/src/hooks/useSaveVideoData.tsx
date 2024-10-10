81723d149cc10810ed03cb02a089f13c737b8071
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-10-10 16:58:08 +0100

    refactor: update useSaveVideoData hook to handle success and error cases
    It now dispatches the appropriate actions based on the response from the server.
    This refactor improves the error handling and provides better feedback to the user.

--- frontend/src/hooks/useSaveVideoData.tsx
+++ frontend/src/hooks/useSaveVideoData.tsx
@@ -5,8 +5,9 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 import type { TypedError } from "@/types";
 import { saveVideoData } from "@/actions/video.actions";
+import type { VideoAction } from "@/types/video.types";
 
-function useSaveVideoData() {
+function useSaveVideoData(dispatch: React.Dispatch<VideoAction>) {
 	const queryClient = useQueryClient();
 	const router = useRouter();
 
@@ -15,10 +16,31 @@ function useSaveVideoData() {
 		onSuccess: (data) => {
 			queryClient.invalidateQueries({ queryKey: ["videoData"] });
 
-			console.log(data);
+			dispatch({
+				type: "SAVE_VIDEO_SUCCESS",
+				payload: {
+					video: data,
+					message: "Video saved successfully",
+					success: true,
+				},
+			});
 		},
 		onError: (error: TypedError) => {
 			console.log(error);
+			if (error.response) {
+				dispatch({
+					type: "SAVE_VIDEO_FAIL",
+					payload: { message: error.response.data.message, success: false },
+				});
+			} else {
+				dispatch({
+					type: "SAVE_VIDEO_FAIL",
+					payload: {
+						message: "Video Saving Failed. Please try again.",
+						success: false,
+					},
+				});
+			}
 		},
 	});
 }
