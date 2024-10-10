45c315a462e74ce468b6c6ff89607547e3858f54
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-10-10 16:16:31 +0100

    feat: add helper function to extract video ID from YouTube URL

---
+++ frontend/src/helpers/extractVideoId.ts
@@ -0,0 +1,14 @@
+/**
+ * Extracts the video ID from a given YouTube video URL.
+ *
+ * @param videoUrl - The URL of the YouTube video.
+ * @returns The video ID if present, otherwise `null`.
+ */
+function extractVideoId(videoUrl: string) {
+  const urlObject = new URL(videoUrl);
+  const videoId = urlObject.searchParams.get('v');
+
+  return videoId;
+}
+
+export default extractVideoId;
