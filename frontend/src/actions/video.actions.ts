27de9b865dab22f1d15b7dcc5ff82a69b3af7523
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-10-10 16:19:12 +0100

    feat: add saveVideoData function to handle saving video data

---
+++ frontend/src/actions/video.actions.ts
@@ -0,0 +1,22 @@
+import axiosInstance from '@/lib/axios.lib';
+
+import extractVideoId from '@/helpers/extractVideoId';
+import type { Video } from '@/types/video.types';
+
+/**
+ * Saves video data by extracting the video ID from the provided URL
+ * and sending it to the server.
+ *
+ * @param {string} videoUrl - The URL of the video to be saved.
+ * @returns {Promise<Video | null>} - A promise that resolves to the saved
+ * video data or null if the video ID could not be extracted.
+ */
+export async function saveVideoData(videoUrl: string): Promise<Video | null> {
+  const videoId = extractVideoId(videoUrl);
+
+  if (!videoId) return null;
+
+  const response = await axiosInstance.post('/videos', { videoId });
+
+  return response.data;
+}
