801844351b2aee1ed0f99a3c9d4b2dfbc68c9f7d
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-10-10 16:53:33 +0100

    refactor: add types for the VideoProvider

--- frontend/src/types/video.types.ts
+++ frontend/src/types/video.types.ts
@@ -1,3 +1,5 @@
+import type { ReactNode } from 'react';
+
 export type Video = {
   id: string;
   youtubeId: string;
@@ -44,3 +46,26 @@ type Statistics = {
 type Player = {
   embedHtml: string;
 };
+
+export type VideoProviderProps = {
+  children: ReactNode;
+};
+
+export type VideoState = {
+  video: Video | null;
+  message: string | null;
+  success: boolean;
+};
+
+export type VideoContextType = {
+  state: VideoState;
+  saveVideo: (videoUrl: string) => void;
+  isLoading: boolean;
+};
+
+export type VideoAction =
+  | {
+      type: 'SAVE_VIDEO_SUCCESS';
+      payload: { message: string; video: Video; success: true };
+    }
+  | { type: 'SAVE_VIDEO_FAIL'; payload: { message: string; success: false } };
