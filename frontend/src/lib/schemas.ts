9315cc738262cd2f26be98a60c42e6293a46ef7b
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-10-11 15:43:42 +0100

    feat(schemas): add saveNoteFormSchema validation

--- frontend/src/lib/schemas.ts
+++ frontend/src/lib/schemas.ts
@@ -19,10 +19,19 @@ export const videoFormSchema = z.object({
   videoUrl: z
     .string()
     .url({
-      message: "Invalid youtube URL. Please provide a valid youtube video URL.",
+      message: 'Invalid youtube URL. Please provide a valid youtube video URL.',
     })
-    .includes("youtube.com/watch?v=", {
-      message: "Invalid youtube URL. Please provide a valid youtube video URL.",
+    .includes('youtube.com/watch?v=', {
+      message: 'Invalid youtube URL. Please provide a valid youtube video URL.',
     }),
 });
 
+export const saveNoteFormSchema = z.object({
+  noteTitle: z
+    .string({
+      message: 'Note title is required. Please enter a title.',
+    })
+    .min(3, {
+      message: 'Note title must be at least 3 characters long.',
+    }),
+});
