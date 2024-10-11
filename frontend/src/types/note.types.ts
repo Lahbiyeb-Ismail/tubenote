9441906e2420666b52b095e794c1ad2b266d48c5
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-10-11 17:02:41 +0100

    feat(types): add videoTitle property to Note type

--- frontend/src/types/note.types.ts
+++ frontend/src/types/note.types.ts
@@ -5,3 +5,11 @@ import type { saveNoteFormSchema, videoFormSchema } from '@/lib/schemas';
 export type VideoUrl = z.infer<typeof videoFormSchema>;
 
 export type NoteTitle = z.infer<typeof saveNoteFormSchema>;
+
+export type Note = {
+  title: string;
+  content: string;
+  videoId?: string;
+  thumbnail?: string;
+  videoTitle?: string;
+};
