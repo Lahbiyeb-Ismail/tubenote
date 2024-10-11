faeddc8b52f5deeb4c9f1b8971aae6f11f775c2f
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-10-11 17:28:43 +0100

    feat(types): add NoteProviderProps, NoteState, NoteContextType, and NoteAction types

--- frontend/src/types/note.types.ts
+++ frontend/src/types/note.types.ts
@@ -1,6 +1,7 @@
 import type { z } from 'zod';
 
 import type { saveNoteFormSchema, videoFormSchema } from '@/lib/schemas';
+import type { ReactNode } from 'react';
 
 export type VideoUrl = z.infer<typeof videoFormSchema>;
 
@@ -13,3 +14,26 @@ export type Note = {
   thumbnail?: string;
   videoTitle?: string;
 };
+
+export type NoteProviderProps = {
+  children: ReactNode;
+};
+
+export type NoteState = {
+  note: Note | null;
+  message: string | null;
+  success: boolean;
+};
+
+export type NoteContextType = {
+  state: NoteState;
+  createNote: (note: Note) => void;
+  isLoading: boolean;
+};
+
+export type NoteAction =
+  | {
+      type: 'CREATE_NOTE_SUCCESS';
+      payload: { message: string; note: Note; success: true };
+    }
+  | { type: 'CREATE_NOTE_FAIL'; payload: { message: string; success: false } };
