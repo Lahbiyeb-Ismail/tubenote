fad5b45689421611095694ea08dc9906386ee7a0
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-10-12 14:05:03 +0100

    refactor(actions): add CreateNoteResponse type definition and update createNote function signature

--- frontend/src/actions/note.actions.ts
+++ frontend/src/actions/note.actions.ts
@@ -1,7 +1,13 @@
 import axiosInstance from '@/lib/axios.lib';
-import type { Note } from '@/types/note.types';
+import type { CreateNoteResponse, Note } from '@/types/note.types';
 
-export async function createNote(note: Note) {
+/**
+ * Creates a new note by sending a POST request to the '/notes' endpoint.
+ *
+ * @param {Note} note - The note object to be created.
+ * @returns {Promise<CreateNoteResponse>} - A promise that resolves to the response data.
+ */
+export async function createNote(note: Note): Promise<CreateNoteResponse> {
   const response = await axiosInstance.post('/notes', note);
 
   return response.data;
