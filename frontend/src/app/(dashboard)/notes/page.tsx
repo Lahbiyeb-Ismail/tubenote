3fb032330458d12898e18e145f22a663d1a964bd
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-10-12 16:43:26 +0100

    refactor(ui): update NotesPage component to display notes and add note form

--- frontend/src/app/(dashboard)/notes/page.tsx
+++ frontend/src/app/(dashboard)/notes/page.tsx
@@ -1,14 +1,27 @@
+"use client";
+
 import AddNoteForm from "@/components/dashboards/AddNoteForm";
 import Header from "@/components/dashboards/Header";
+import NotesList from "@/components/note/NotesList";
+import { useNote } from "@/context/useNote";
 
 function NotesPage() {
+	const { notes, isNotesLoading } = useNote();
+
 	return (
 		<div className="min-h-screen flex-1 bg-gray-100">
 			<Header title="Your Video Notes" />
 			<main className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-				<div className="flex justify-end">
-					<AddNoteForm />
-				</div>
+				{!notes && !isNotesLoading ? (
+					<p className="text-center">No notes found.</p>
+				) : (
+					<>
+						<div className="flex justify-end">
+							<AddNoteForm />
+						</div>
+						<NotesList notes={notes} />
+					</>
+				)}
 			</main>
 		</div>
 	);
