5d7ca6cc434a2bd05b4e3236e5ba6d813c07f98b
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-10-12 13:52:36 +0100

    feat(api): add getUserNotes endpoint to retrieve user-specific notes

--- backend/src/controllers/note.controller.ts
+++ backend/src/controllers/note.controller.ts
@@ -74,3 +74,55 @@ export async function createNote(req: PayloadRequest, res: Response) {
       .json({ message: 'Error creating note.', error });
   }
 }
+
+/**
+ * Retrieves the notes for a specific user based on the user ID provided in the request payload.
+ *
+ * @param req - The request object containing the payload with the user ID.
+ * @param res - The response object used to send back the appropriate HTTP response.
+ *
+ * @remarks
+ * - If the user ID is not present in the request payload, the function responds
+ * with a 401 Unauthorized status.
+ * - If the user is not found in the database, the function responds with a
+ * 404 Not Found status.
+ * - If the notes are successfully retrieved, the function responds with a
+ * 200 OK status and the notes data.
+ * - If an error occurs during the process, the function responds with a
+ * 500 Internal Server Error status.
+ *
+ * @returns A promise that resolves to void.
+ */
+export async function getUserNotes(req: PayloadRequest, res: Response) {
+  const userID = req.payload?.userID;
+
+  if (!userID) {
+    res
+      .status(httpStatus.UNAUTHORIZED)
+      .json({ message: 'Unauthorized access. Please try again.' });
+    return;
+  }
+
+  try {
+    const user = await prismaClient.user.findUnique({
+      where: { id: userID },
+    });
+
+    if (!user) {
+      res
+        .status(httpStatus.NOT_FOUND)
+        .json({ message: 'Unauthorized access. Please try again.' });
+      return;
+    }
+
+    const notes = await prismaClient.note.findMany({
+      where: { userId: user.id },
+    });
+
+    res.status(httpStatus.OK).json({ notes });
+  } catch (error) {
+    res
+      .status(httpStatus.INTERNAL_SERVER_ERROR)
+      .json({ message: 'Error creating note.', error });
+  }
+}
