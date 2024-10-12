94f586ff22c407ff4117c1ccc50bdf9ba1475cca
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-10-12 16:40:58 +0100

    feat(config): add remotePatterns for image URLs in next.config.mjs
    This commit adds the remotePatterns configuration option to the images object in next.config.
    The remotePatterns array contains a list of remote image URLs that are
    allowed to be used in the application.
    This is useful for allowing images from trusted sources like GitHub, Google, and YouTube.

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'github.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.youtube.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ytimg.com',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
