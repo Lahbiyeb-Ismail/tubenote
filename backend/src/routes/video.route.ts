a7bda5533799614884aa4736483724fa08d4deec
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-10-14 17:59:47 +0100

    feat(video): add getUserVideos route handler

--- backend/src/routes/video.route.ts
+++ backend/src/routes/video.route.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
-import { getVideoData } from '../controllers/video.controller';
+import { getUserVideos, getVideoData } from '../controllers/video.controller';
 import isAuthenticated from '../middlewares/isAuthenticated';
 
 const router = Router();
 
 router.route('/').post(isAuthenticated, getVideoData);
+
+router.route('/').get(isAuthenticated, getUserVideos);
 
 export default router;
