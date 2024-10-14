ab618f782d418ceca8d0ca195014c948422a5918
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-10-14 16:49:18 +0100

    refactor(hooks): remove unused properties from useGetUserNotes

--- frontend/src/hooks/useGetUserNotes.tsx
+++ frontend/src/hooks/useGetUserNotes.tsx
@@ -1,12 +1,18 @@
 "use client";
 
 import { getUserNotes } from "@/actions/note.actions";
+import { useAuth } from "@/context/useAuth";
 import { useQuery } from "@tanstack/react-query";
 
 function useGetUserNotes() {
+	const {
+		state: { accessToken },
+	} = useAuth();
+
 	return useQuery({
 		queryKey: ["notes"],
 		queryFn: () => getUserNotes(),
+		enabled: !!accessToken,
 	});
 }
 
