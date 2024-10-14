1d525c08c893936888ac7d93228e8b035ba2c56b
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-10-14 16:51:55 +0100

    refactor(notes): update NotesPage to use useGetUserNotes hook

--- frontend/src/app/(dashboard)/notes/page.tsx
+++ frontend/src/app/(dashboard)/notes/page.tsx
@@ -4,22 +4,25 @@ import AddNoteForm from "@/components/dashboards/AddNoteForm";
 import Header from "@/components/dashboards/Header";
 import NotesList from "@/components/note/NotesList";
 import { useNote } from "@/context/useNote";
+import useGetUserNotes from "@/hooks/useGetUserNotes";
 
 function NotesPage() {
-	const { notes, isNotesLoading } = useNote();
+	// const { notes, isNotesLoading } = useNote();
+
+	const { data, isLoading } = useGetUserNotes();
 
 	return (
 		<div className="min-h-screen flex-1 bg-gray-100">
 			<Header title="Your Video Notes" />
 			<main className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-				{!notes && !isNotesLoading ? (
+				{!data && !isLoading ? (
 					<p className="text-center">No notes found.</p>
 				) : (
 					<>
 						<div className="flex justify-end">
 							<AddNoteForm />
 						</div>
-						<NotesList notes={notes} />
+						<NotesList notes={data} />
 					</>
 				)}
 			</main>
