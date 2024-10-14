fc21d5215dad91eb671ca1d12bd5badcac761145
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-10-14 17:59:04 +0100

    feat(video): add getUserVideos controller function

--- backend/src/controllers/video.controller.ts
+++ backend/src/controllers/video.controller.ts
@@ -63,3 +63,53 @@ export async function getVideoData(req: PayloadRequest, res: Response) {
       .json({ message: 'Error creating video', error });
   }
 }
+
+/**
+ * Retrieves videos associated with the authenticated user.
+ *
+ * @param req - The request object containing the payload with user information.
+ * @param res - The response object used to send back the appropriate HTTP response.
+ *
+ * @remarks
+ * This function checks if the user is authenticated by verifying the presence of `userID` in the request payload.
+ * If the user is not authenticated, it responds with a 401 Unauthorized status.
+ * If the user is authenticated, it attempts to fetch the user from the database.
+ * If the user is not found, it responds with a 404 Not Found status.
+ * If the user is found, it fetches all videos associated with the user and responds with a 200 OK status along with the videos.
+ * In case of any errors during the process, it responds with a 500 Internal Server Error status.
+ *
+ * @throws {Error} If there is an issue with the database query or any other unexpected error.
+ */
+export async function getUserVideos(req: PayloadRequest, res: Response) {
+  const userID = req.payload?.userID;
+
+  if (!userID) {
+    res
+      .status(httpStatus.UNAUTHORIZED)
+      .json({ message: 'Unauthorized access. Please try again.' });
+    return;
+  }
+
+  try {
+    const user = await prismaClient.user.findUnique({
+      where: { id: userID },
+    });
+
+    if (!user) {
+      res
+        .status(httpStatus.NOT_FOUND)
+        .json({ message: 'Unauthorized access. Please try again.' });
+      return;
+    }
+
+    const videos = await prismaClient.video.findMany({
+      where: { userId: user.id },
+    });
+
+    res.status(httpStatus.OK).json({ videos });
+  } catch (error) {
+    res
+      .status(httpStatus.INTERNAL_SERVER_ERROR)
+      .json({ message: 'Error creating note.', error });
+  }
+}
