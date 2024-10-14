f823ba58f41cce25044b0a662159faadb4bdced4
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-10-14 14:29:35 +0100

    feat(ui): add DropDownNavbar component

---
+++ frontend/src/components/Navbar/DropDownNavbar.tsx
@@ -0,0 +1,61 @@
+import { LogOut, Settings, User } from "lucide-react";
+
+import { useAuth } from "@/context/useAuth";
+
+import UserAvatar from "../global/UserAvatar";
+
+import { Button } from "../ui/button";
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+
+function DropDownNavbar() {
+	const { state } = useAuth();
+
+	const { user } = state;
+
+	return (
+		<div className="flex items-center">
+			<DropdownMenu>
+				<DropdownMenuTrigger asChild>
+					<Button variant="ghost" className="relative h-8 w-8 rounded-full">
+						<UserAvatar />
+					</Button>
+				</DropdownMenuTrigger>
+				<DropdownMenuContent className="w-56 mt-2" align="end" forceMount>
+					<DropdownMenuLabel className="font-normal">
+						<div className="flex flex-col space-y-1">
+							<p className="text-sm font-medium leading-none">
+								{user?.username}
+							</p>
+							<p className="text-xs leading-none text-muted-foreground">
+								{user?.email}
+							</p>
+						</div>
+					</DropdownMenuLabel>
+					<DropdownMenuSeparator />
+					<DropdownMenuItem className="flex items-center cursor-pointer">
+						<User className="mr-2 h-4 w-4" />
+						<span>Profile</span>
+					</DropdownMenuItem>
+					<DropdownMenuItem className="flex items-center cursor-pointer">
+						<Settings className="mr-2 h-4 w-4" />
+						<span>Settings</span>
+					</DropdownMenuItem>
+					<DropdownMenuSeparator />
+					<DropdownMenuItem className="flex items-center cursor-pointer text-red-600 focus:text-red-600 focus:bg-red-50">
+						<LogOut className="mr-2 h-4 w-4" />
+						<span>Log out</span>
+					</DropdownMenuItem>
+				</DropdownMenuContent>
+			</DropdownMenu>
+		</div>
+	);
+}
+
+export default DropDownNavbar;
