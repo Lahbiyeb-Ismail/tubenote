c372dc75972002cebaffc372bfd9dc5718d06152
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-10-15 16:38:45 +0100

    refactor(NotesList): update layout in NotesList component based on grid layout state

--- frontend/src/components/note/NotesList.tsx
+++ frontend/src/components/note/NotesList.tsx
@@ -1,14 +1,22 @@
+"use client";
+
+import React from "react";
 import NoteCard from "./NoteCard";
 import type { INote } from "@/types/note.types";
+import { useLayout } from "@/context/useLayout";
 
 type NotesListProps = {
 	notes?: INote[];
 };
 
 function NotesList({ notes }: NotesListProps) {
+	const { isGridLayout } = useLayout();
+
 	return (
 		<div className="px-4 py-6 sm:px-0">
-			<div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+			<div
+				className={`${isGridLayout ? "grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3" : "space-y-4"}`}
+			>
 				{notes?.map((note) => (
 					<NoteCard key={note.id} note={note} />
 				))}
