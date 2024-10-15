b9dfd78064b6399f1e238881bb3acd895b40f866
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-10-15 18:15:59 +0100

    refactor: update VideoPlayer component to set videoCurrentTime state when the video is played

--- frontend/src/components/video/VideoPlayer.tsx
+++ frontend/src/components/video/VideoPlayer.tsx
@@ -1,19 +1,53 @@
 "use client";
 
-import React from "react";
-import YouTube from "react-youtube";
+import { useVideo } from "@/context/useVideo";
+import React, { useRef, useState } from "react";
+import YouTube, { type YouTubeProps } from "react-youtube";
 
 type VideoPlayerProps = {
 	videoId?: string;
 };
 
 function VideoPlayer({ videoId }: VideoPlayerProps) {
+	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+	const playerRef = useRef<any | null>(null);
+	const { setVideoCurrentTime } = useVideo();
+
+	const onPlayerReady: YouTubeProps["onReady"] = (event) => {
+		playerRef.current = event.target;
+	};
+
+	const onStateChange: YouTubeProps["onStateChange"] = (event) => {
+		// Check if the player is playing (state 1)
+		if (event.data === 1) {
+			const time = playerRef.current?.getCurrentTime() || 0;
+			setVideoCurrentTime(time);
+		}
+	};
+
+	const onPause: YouTubeProps["onPause"] = () => {
+		const time = playerRef.current?.getCurrentTime() || 0;
+		setVideoCurrentTime(time);
+	};
+
+	const opts = {
+		height: "100%",
+		width: "100%",
+		playerVars: {
+			// https://developers.google.com/youtube/player_parameters
+			autoplay: 1,
+		},
+	};
+
 	return (
 		<div className="h-full">
 			<YouTube
 				videoId={videoId}
 				style={{ height: "100%" }}
-				opts={{ width: "100%", height: "100%" }}
+				opts={opts}
+				onReady={onPlayerReady}
+				onStateChange={onStateChange}
+				onPause={onPause}
 			/>
 		</div>
 	);
