443985c75016272d62304c74dd83974405ebc765
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-10-15 13:12:28 +0100

    feat: add responsive sidebar with toggle functionality
    - Implemented a Sidebar component with toggle functionality to open and close.
    - Added `usePathname` hook to manage the current path.

--- frontend/src/components/dashboards/Sidebar.tsx
+++ frontend/src/components/dashboards/Sidebar.tsx
@@ -1,34 +1,35 @@
 "use client";
 
 import { usePathname } from "next/navigation";
-import { useState } from "react";
 
 import UserProfile from "../global/UserProfile";
 import SidebarLogo from "./SidebarLogo";
-import SidebarNav from "./SidebarMenu";
+import SidebarNav from "./SidebarNav";
 
 import { Button } from "@/components/ui/button";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 
-function Sidebar() {
+type SidebarProps = {
+	toggleSidebar: () => void;
+	isSidebarOpen: boolean;
+};
+
+function Sidebar({ toggleSidebar, isSidebarOpen }: SidebarProps) {
 	const pathname = usePathname();
-
-	const [isOpen, setIsOpen] = useState(true);
-
-	const toggleSidebar = () => setIsOpen(!isOpen);
 
 	return (
 		<div
-			className={`fixed left-0 top-0 flex h-full flex-col pb-6 pt-16 bg-white p-2 shadow-lg transition-all duration-300 ${isOpen ? "w-64" : "w-20"}`}
+			className={`fixed left-0 top-0 flex h-full flex-col pb-6 pt-16 bg-white p-2 shadow-lg
+				transition-all duration-300 ${isSidebarOpen ? "w-64" : "w-20"}`}
 		>
 			<div className="flex items-center justify-between mb-6">
-				<SidebarLogo isOpen={isOpen} />
+				<SidebarLogo isOpen={isSidebarOpen} />
 				<Button variant="ghost" size="icon" onClick={toggleSidebar}>
-					{isOpen ? <ChevronLeft /> : <ChevronRight />}
+					{isSidebarOpen ? <ChevronLeft /> : <ChevronRight />}
 				</Button>
 			</div>
-			<SidebarNav isOpen={isOpen} pathname={pathname} />
-			<UserProfile isOpen={isOpen} />
+			<SidebarNav isOpen={isSidebarOpen} pathname={pathname} />
+			<UserProfile isOpen={isSidebarOpen} />
 		</div>
 	);
 }
