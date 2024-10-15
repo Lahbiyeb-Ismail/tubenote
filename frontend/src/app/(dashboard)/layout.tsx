464618c3116fd311c22f7d1e8f8e8be8dd1ba147
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-10-15 13:06:43 +0100

    feat: add sidebar toggle functionality to dashboard layout
    - Implemented `useState` to manage sidebar open/close state.
    - Added `toggleSidebar` function to handle sidebar state changes.
    - Updated layout to conditionally adjust main content margin based
    on sidebar state.

--- frontend/src/app/(dashboard)/layout.tsx
+++ frontend/src/app/(dashboard)/layout.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useState } from "react";
 import Sidebar from "@/components/dashboards/Sidebar";
 import withAuth from "@/HOC/withAuth";
 
@@ -8,10 +9,19 @@ type LayoutProps = {
 };
 
 function Layout({ children }: LayoutProps) {
+	const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+	const toggleSidebar = () => setIsSidebarOpen(!isSidebarOpen);
+
 	return (
-		<div className="flex bg-gray-100">
-			<Sidebar />
-			<div className="ml-64 flex-1 bg-gray-100 p-4">{children}</div>
+		<div className="flex bg-gray-100 overflow-hidden">
+			<Sidebar toggleSidebar={toggleSidebar} isSidebarOpen={isSidebarOpen} />
+			<div
+				className={`flex-1 bg-gray-100 p-4 overflow-auto transition-all duration-300
+					${isSidebarOpen ? "ml-64" : "ml-20"}`}
+			>
+				{children}
+			</div>
 		</div>
 	);
 }
