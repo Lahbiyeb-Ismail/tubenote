7bc3ebe15927ab69acc08d26b2f0614bae80be87
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-10-15 17:08:47 +0100

    refactor(SidebarNav): update SidebarNav component to be responsive

--- frontend/src/components/dashboards/SidebarNav.tsx
+++ frontend/src/components/dashboards/SidebarNav.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-
 import { sidebarMenuLinks } from "@/utils/constants";
 
 type SidebarNavProps = {
@@ -10,24 +9,38 @@ type SidebarNavProps = {
 function SidebarNav({ pathname, isOpen }: SidebarNavProps) {
 	return (
 		<nav
-			className={`flex-grow transition-all duration-300 ${isOpen ? "px-4" : "px-0"}`}
+			className={`flex-grow transition-all duration-300 ${
+				isOpen ? "px-4" : "px-0"
+			}`}
 		>
 			<ul className="space-y-2">
 				{sidebarMenuLinks.map((item) => (
 					<li
 						key={item.name}
-						className={`rounded-lg p-2 transition duration-150 ease-in-out
-							${item.href === pathname ? "bg-blue-100 text-blue-600" : "text-gray-600 hover:bg-gray-100"}`}
+						className={`rounded-lg p-2 transition duration-150 ease-in-out ${
+							item.href === pathname
+								? "bg-blue-100 text-blue-600"
+								: "text-gray-600 hover:bg-gray-100"
+						}`}
 					>
-						<Link href={item.href} className="flex items-center">
-							<item.icon
-								className={`transition-all duration-300 ${isOpen ? "mr-3" : "mr-0"}`}
-							/>
-							<span
-								className={`transition-all duration-300 ${isOpen ? "block" : "hidden"}`}
+						<Link
+							href={item.href}
+							className={`flex items-center ${
+								isOpen ? "" : "justify-center"
+							} h-full`}
+						>
+							<div
+								className={`flex items-center justify-center ${
+									isOpen ? "mr-3" : "w-full h-full"
+								}`}
 							>
-								{item.name}
-							</span>
+								<item.icon
+									className={`transition-all duration-300 ${
+										isOpen ? "" : "w-6 h-6"
+									}`}
+								/>
+							</div>
+							{isOpen && <span>{item.name}</span>}
 						</Link>
 					</li>
 				))}
