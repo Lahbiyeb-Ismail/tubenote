a20fb32df22fda215c557b80e61adf3bb0ff9af1
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-10-15 14:30:06 +0100

    feat(EditorPage): add responsive resizing for resizable panel group

--- frontend/src/app/editor/[videoId]/page.tsx
+++ frontend/src/app/editor/[videoId]/page.tsx
@@ -8,18 +8,35 @@ import {
 import TextEditor from "@/components/editor/TextEditor";
 import VideoPlayer from "@/components/video/VideoPlayer";
 import { useVideo } from "@/context/useVideo";
+import { useEffect, useState } from "react";
 
 function EditorPage() {
 	const {
 		state: { video },
 	} = useVideo();
 
+	const [direction, setDirection] = useState<"horizontal" | "vertical">(
+		"horizontal",
+	);
+
+	useEffect(() => {
+		function changeDirection() {
+			if (window.innerWidth < 768) {
+				setDirection("vertical");
+			} else {
+				setDirection("horizontal");
+			}
+		}
+
+		changeDirection();
+
+		window.addEventListener("resize", changeDirection);
+		return () => window.removeEventListener("resize", changeDirection);
+	}, []);
+
 	return (
 		<section className="height_viewport">
-			<ResizablePanelGroup
-				direction="horizontal"
-				className="flex w-full border"
-			>
+			<ResizablePanelGroup direction={direction} className="flex w-full border">
 				<ResizablePanel defaultSize={35} className="p-2">
 					<TextEditor />
 				</ResizablePanel>
