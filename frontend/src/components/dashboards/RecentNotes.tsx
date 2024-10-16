7af682bbf8538473ba1ff739fb24f0e15f57cadb
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-10-16 16:02:36 +0100

    refactor: update RecentNotes component by adding ViewAllNotes button

--- frontend/src/components/dashboards/RecentNotes.tsx
+++ frontend/src/components/dashboards/RecentNotes.tsx
@@ -1,5 +1,7 @@
+import Link from "next/link";
 import NoteCard from "../note/NoteCard";
 import useGetRecentNotes from "@/hooks/useGetRecentNotes";
+import { Button } from "../ui/button";
 
 function RecentNotes() {
 	const { data: recentNotes } = useGetRecentNotes();
@@ -11,6 +13,11 @@ function RecentNotes() {
 				{recentNotes?.map((note) => (
 					<NoteCard key={note.id} note={note} />
 				))}
+			</div>
+			<div className="mt-6 text-center">
+				<Link href="/notes">
+					<Button variant="outline">View All Notes</Button>
+				</Link>
 			</div>
 		</div>
 	);
