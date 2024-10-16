c57ce98b7b7c8a14d282bb714a9f28b87b33aa60
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-10-16 14:58:38 +0100

    refactor: update dashboard page layout and components
    - Update the layout of the dashboard page to use a grid system.
    - Replace the RecentUserActions component with RecentNotes and RecentlyUpdatedNotes
    components.
    - Remove the RecentUserActions component.

--- frontend/src/app/(dashboard)/dashboard/page.tsx
+++ frontend/src/app/(dashboard)/dashboard/page.tsx
@@ -1,13 +1,19 @@
+"use client";
+
 import UserStats from "@/components/dashboards/UserStats";
-import RecentUserActions from "@/components/dashboards/RecentUserActions";
+import RecentNotes from "@/components/dashboards/RecentNotes";
 import Header from "@/components/dashboards/Header";
+import RecentlyUpdatedNotes from "@/components/dashboards/RecentlyUpdatedNotes";
 
 function DashboardPage() {
 	return (
-		<div className="min-h-screen flex-grow p-8">
-			<Header title="Overview" />
-			<UserStats />
-			<RecentUserActions />
+		<div className="min-h-screen flex-grow p-4 md:p-8">
+			<Header title="Dashboard" />
+			<div className="mt-8 grid gap-6 md:grid-cols-2">
+				<UserStats />
+				<RecentNotes />
+				<RecentlyUpdatedNotes />
+			</div>
 		</div>
 	);
 }
