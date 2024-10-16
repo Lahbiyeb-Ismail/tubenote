fd763c2d362fc464c0e611d65dc3a025de78c378
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-10-16 16:48:01 +0100

    refactor: update DashboardPage to use RecentNoteList && remove RecentNotes, RecentlyUpdatedNotes

--- frontend/src/app/(dashboard)/dashboard/page.tsx
+++ frontend/src/app/(dashboard)/dashboard/page.tsx
@@ -1,18 +1,30 @@
 "use client";
 
-import UserStats from "@/components/dashboards/UserStats";
-import RecentNotes from "@/components/dashboards/RecentNotes";
 import Header from "@/components/dashboards/Header";
-import RecentlyUpdatedNotes from "@/components/dashboards/RecentlyUpdatedNotes";
+import RecentNoteList from "@/components/dashboards/RecentNotesList";
+
+import useGetRecentNotes from "@/hooks/useGetRecentNotes";
+import useGetRecentlyUpdatedNotes from "@/hooks/useGetRecentlyUpdatedNotes";
 
 function DashboardPage() {
+	const { data: recentNotes } = useGetRecentNotes();
+	const { data: recentlyUpdatedNotes } = useGetRecentlyUpdatedNotes();
+
 	return (
 		<div className="min-h-screen flex-grow p-4 md:p-8">
 			<Header title="Dashboard" />
 			<div className="mt-8 grid gap-6 md:grid-cols-2">
-				<UserStats />
-				<RecentNotes />
-				<RecentlyUpdatedNotes />
+				{/* <UserStats /> */}
+				<RecentNoteList
+					title="Recent Notes"
+					notes={recentNotes}
+					emptyMessage=""
+				/>
+				<RecentNoteList
+					title="Recently Updated Notes"
+					notes={recentlyUpdatedNotes}
+					emptyMessage=""
+				/>
 			</div>
 		</div>
 	);
