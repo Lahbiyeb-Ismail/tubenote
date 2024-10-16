e7a8105a5ec76418dbcf17b448bf715a3caeed29
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-10-16 13:21:38 +0100

    refactor: lazy load LoggedInButtons and LoggedOutButtons components in NavbarButtons

--- frontend/src/components/Navbar/NavbarButtons.tsx
+++ frontend/src/components/Navbar/NavbarButtons.tsx
@@ -1,9 +1,14 @@
 "use client";
 
+import dynamic from "next/dynamic";
 import { useAuth } from "@/context/useAuth";
 
-import LoggedInButtons from "./LoggedInButtons";
-import LoggedOutButtons from "./LoggedOutButtons";
+const LoggedInButtons = dynamic(() => import("./LoggedInButtons"), {
+	ssr: false,
+});
+const LoggedOutButtons = dynamic(() => import("./LoggedOutButtons"), {
+	ssr: false,
+});
 
 function NavbarButtons() {
 	const {
