c590b37e23bb0860e2920180e869335a0884fddd
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-10-17 17:03:56 +0100

    refactor(hooks): add loading, success and error toasts in useLogout hook

--- frontend/src/hooks/useLogout.tsx
+++ frontend/src/hooks/useLogout.tsx
@@ -5,6 +5,7 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { logoutUser } from "@/actions/auth.actions";
 import type { TypedError } from "@/types";
 import type { AuthAction } from "@/types/auth.types";
+import toast from "react-hot-toast";
 
 function useLogout(dispatch: React.Dispatch<AuthAction>) {
 	const queryClient = useQueryClient();
@@ -12,19 +13,28 @@ function useLogout(dispatch: React.Dispatch<AuthAction>) {
 
 	return useMutation({
 		mutationFn: logoutUser,
+		onMutate: () => {
+			toast.loading("Logging out...", { id: "loadingToast" });
+		},
 		onSuccess: () => {
+			toast.dismiss("loadingToast");
+
+			toast.success("Logged out successfully.");
 			queryClient.invalidateQueries({ queryKey: ["user"] });
 
 			localStorage.clear();
 			dispatch({ type: "LOGOUT_SUCCESS" });
 		},
 		onError(error: TypedError) {
+			toast.dismiss("loadingToast");
 			if (error.response) {
+				toast.error(error.response.data.message);
 				dispatch({
 					type: "REQUEST_FAIL",
 					payload: { errorMessage: error.response.data.message },
 				});
 			} else {
+				toast.error("Logout failed. Please try again.");
 				dispatch({
 					type: "REQUEST_FAIL",
 					payload: { errorMessage: "Logout failed. Please try again." },
