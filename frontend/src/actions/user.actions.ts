0f70b74eba9c5fad7c678c2d2ede33aa450066f0
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-10-17 15:46:23 +0100

    refactor(actions): update updateCurrentUser function return type

--- frontend/src/actions/user.actions.ts
+++ frontend/src/actions/user.actions.ts
@@ -7,8 +7,10 @@ import type { User } from '@/types/auth.types';
  * @param {User} user - The user object containing updated information.
  * @returns {Promise<User>} A promise that resolves to the updated user object.
  */
-export async function updateCurrentUser(user: User): Promise<User> {
-  const response = await axiosInstance.patch<User>(
+export async function updateCurrentUser(
+  user: User
+): Promise<{ user: User; message: string }> {
+  const response = await axiosInstance.patch(
     '/users/update-current',
     user
   );
