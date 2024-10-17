7363524ba4542922e7050b2466e4c7d9c1f35df3
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-10-17 14:28:23 +0100

    feat(schemas): update validation messages for username and email

--- frontend/src/lib/schemas.ts
+++ frontend/src/lib/schemas.ts
@@ -40,10 +40,12 @@ export const updateProfileSchema = z.object({
   username: z
     .string()
     .min(3, {
-      message: 'Password must be at least 8 characters.',
+      message: 'Password must be at least 3 characters.',
     })
     .max(20, { message: 'Username must be less than 20 characters.' }),
-  email: z.string().email(),
+  email: z
+    .string()
+    .email({ message: 'Invalid email address. Please try another one.' }),
 });
 
 export const updatePasswordSchema = z
