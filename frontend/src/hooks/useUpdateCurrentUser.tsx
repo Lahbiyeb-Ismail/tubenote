45cd31e61c9db7d0ad0a8d69a77cc631effeded1
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-10-17 17:16:06 +0100

    refactor(hooks): add loading, success, and error toasts in useUpdateCurrentUser hook

--- frontend/src/hooks/useUpdateCurrentUser.tsx
+++ frontend/src/hooks/useUpdateCurrentUser.tsx
@@ -2,13 +2,21 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { updateCurrentUser } from "@/actions/user.actions";
 import type { TypedError } from "@/types";
 import type { UserAction } from "@/types/user.types";
+import toast from "react-hot-toast";
 
 function useUpdateCurrentUser(dispatch: React.Dispatch<UserAction>) {
 	const queryClient = useQueryClient();
 
 	return useMutation({
 		mutationFn: updateCurrentUser,
+		onMutate: () => {
+			toast.loading("Updating user...", { id: "loadingToast" });
+		},
 		onSuccess: (data) => {
+			toast.dismiss("loadingToast");
+
+			toast.success("User updated successfully.");
+
 			queryClient.invalidateQueries({ queryKey: ["user"] });
 
 			dispatch({
@@ -21,12 +29,15 @@ function useUpdateCurrentUser(dispatch: React.Dispatch<UserAction>) {
 			window.location.reload();
 		},
 		onError(error: TypedError) {
+			toast.dismiss("loadingToast");
 			if (error.response) {
+				toast.error(error.response.data.message);
 				dispatch({
 					type: "UPDATE_USER_FAILD",
 					payload: { message: error.response.data.message, user: null },
 				});
 			} else {
+				toast.error("User update faild. Please try again.");
 				dispatch({
 					type: "UPDATE_USER_FAILD",
 					payload: { message: "User update faild.", user: null },
