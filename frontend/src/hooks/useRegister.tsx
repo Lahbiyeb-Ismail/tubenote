3e91290e725151c1ba39db193b3e7c9ac6843f2c
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-10-17 17:06:39 +0100

    refactor(hooks): add loading, success, and error toasts in useRegister hook

--- frontend/src/hooks/useRegister.tsx
+++ frontend/src/hooks/useRegister.tsx
@@ -7,6 +7,7 @@ import { registerUser } from "@/actions/auth.actions";
 
 import type { TypedError } from "@/types";
 import type { AuthAction } from "@/types/auth.types";
+import toast from "react-hot-toast";
 
 function useRegister(dispatch: React.Dispatch<AuthAction>) {
 	const queryClient = useQueryClient();
@@ -14,18 +15,31 @@ function useRegister(dispatch: React.Dispatch<AuthAction>) {
 
 	return useMutation({
 		mutationFn: registerUser,
+		onMutate: () => {
+			toast.loading("Registering...", { id: "loadingToast" });
+		},
 		onSuccess: () => {
+			toast.dismiss("loadingToast");
+
+			toast.success("Registered successfully. Please login to continue.");
+
 			queryClient.invalidateQueries({ queryKey: ["user"] });
 
 			router.push("/login");
 		},
 		onError: (error: TypedError) => {
+			toast.dismiss("loadingToast");
+
 			if (error.response) {
+				toast.error(error.response.data.message);
+
 				dispatch({
 					type: "REQUEST_FAIL",
 					payload: { errorMessage: error.response.data.message },
 				});
 			} else {
+				toast.error("Failed Registration. Please try again.");
+
 				dispatch({
 					type: "REQUEST_FAIL",
 					payload: { errorMessage: "Failed Registration. Please try again." },
