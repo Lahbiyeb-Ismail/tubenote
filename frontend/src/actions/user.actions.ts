53b6be36f61c6157ae75eb8e4a183d6f2eaf70a3
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-10-17 16:28:57 +0100

    refactor(actions): add the updatePassword action for updating the current user's password

--- frontend/src/actions/user.actions.ts
+++ frontend/src/actions/user.actions.ts
@@ -10,10 +10,33 @@ import type { User } from '@/types/auth.types';
 export async function updateCurrentUser(
   user: User
 ): Promise<{ user: User; message: string }> {
-  const response = await axiosInstance.patch(
-    '/users/update-current',
-    user
-  );
+  const response = await axiosInstance.patch('/users/update-current', user);
+
+  return response.data;
+}
+
+type UpdatePassword = {
+  currentPassword: string;
+  newPassword: string;
+};
+
+/**
+ * Updates the user's password.
+ *
+ * @param {Object} params - The parameters for updating the password.
+ * @param {string} params.currentPassword - The user's current password.
+ * @param {string} params.newPassword - The new password to set.
+ * @returns {Promise<{ message: string; user: User }>} A promise that resolves
+ * to an object containing a message and the updated user information.
+ */
+export async function updatePassword({
+  currentPassword,
+  newPassword,
+}: UpdatePassword): Promise<{ message: string; user: User }> {
+  const response = await axiosInstance.patch('/users/update-password', {
+    currentPassword,
+    newPassword,
+  });
 
   return response.data;
 }
