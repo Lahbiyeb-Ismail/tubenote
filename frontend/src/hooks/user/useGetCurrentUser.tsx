2eb0eb836bccd431e731a9331b722b3439fd0764
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-10-23 18:36:03 +0100

    refactor(hooks): update useGetCurrentUser to include access token in query key
    - Enable query based on the accessToken availability
    - Refetch the data after evry 5 minutes

--- frontend/src/hooks/user/useGetCurrentUser.tsx
+++ frontend/src/hooks/user/useGetCurrentUser.tsx
@@ -1,11 +1,25 @@
+"use client";
+
 import { useQuery } from "@tanstack/react-query";
+import { useMemo } from "react";
 
 import { getCurrentUser } from "@/actions/user.actions";
 
 function useGetCurrentUser() {
+	const accessToken = useMemo(() => {
+		if (typeof window !== "undefined") {
+			return localStorage.getItem("accessToken");
+		}
+		return null;
+	}, []);
+
 	return useQuery({
-		queryKey: ["current-user"],
+		queryKey: ["current-user", accessToken],
 		queryFn: getCurrentUser,
+		// Enable the query only if the access token is available.
+		enabled: !!accessToken,
+		// The data is considered fresh for 5 minutes, after which it will be refetched.
+		staleTime: 5 * 60 * 1000,
 	});
 }
 
