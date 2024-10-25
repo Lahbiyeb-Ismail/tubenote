152c29fe9bcaf8c768ed129d7d004a1d6b7c57a0
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-10-25 18:36:49 +0100

    refactor(types): add LoginCredentials type to auth.type.ts

--- backend/src/types/auth.type.ts
+++ backend/src/types/auth.type.ts
@@ -1,5 +1,7 @@
 import type { z } from 'zod';
 
-import type { registrationSchema } from '../schemas/auth.schema';
+import type { loginSchema, registrationSchema } from '../schemas/auth.schema';
 
 export type RegisterCredentiels = z.infer<typeof registrationSchema.body>;
+
+export type LoginCredentials = z.infer<typeof loginSchema.body>;
