6e49468b33720fdd30a5ee5d6ab86aaf6e92ac6f
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-10-25 18:36:23 +0100

    refactor(types): add more strictly typed Express.Request type

--- backend/src/types/index.ts
+++ backend/src/types/index.ts
@@ -1,11 +1,23 @@
 import type { Request } from 'express';
-
+import type { DeepPartial } from 'utility-types';
 
 export type Payload = {
   userID: string;
   iat: number;
   exp: number;
-}
+};
 export interface PayloadRequest extends Request {
-  payload?: Payload
+  payload?: Payload;
 }
+
+// More strictly typed Express.Request type
+// https://stackoverflow.com/questions/34508081/how-to-add-typescript-definitions-to-express-req-res
+export type TypedRequest<
+  ReqBody = Record<string, unknown>,
+  QueryString = Record<string, unknown>,
+> = Request<
+  Record<string, unknown>,
+  Record<string, unknown>,
+  DeepPartial<ReqBody>,
+  DeepPartial<QueryString>
+>;
