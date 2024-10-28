2bdeae9519001fbd05d1dfe61508af8a781ff715
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-10-28 17:41:39 +0100

    feat(context): add useModal context hook
    - This commit adds a new file, useModal.tsx, which contains the useModal context hook.
    - This hook provides a modal context for managing the state of modals in the application.
    - It includes a ModalProvider component that wraps the children components and
    a useModal hook for accessing the modal context.
    
    - The useModal hook throws an error if it is used outside of a ModalProvider
    component, ensuring that it is used correctly.

---
+++ frontend/src/context/useModal.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { createContext, useContext, useState } from "react";
+
+type ModalContextType = {
+	isModalOpen: boolean;
+	setIsModalOpen: (isOpen: boolean) => void;
+};
+
+const ModalContext = createContext<ModalContextType | undefined>(undefined);
+
+export function ModalProvider({ children }: { children: React.ReactNode }) {
+	const [isModalOpen, setIsModalOpen] = useState(false);
+
+	return (
+		<ModalContext.Provider value={{ isModalOpen, setIsModalOpen }}>
+			{children}
+		</ModalContext.Provider>
+	);
+}
+
+const useModal = () => {
+	const context = useContext(ModalContext);
+
+	if (context === undefined) {
+		throw new Error("useModal must be used within a ModalProvider");
+	}
+	return context;
+};
+
+export default useModal;
