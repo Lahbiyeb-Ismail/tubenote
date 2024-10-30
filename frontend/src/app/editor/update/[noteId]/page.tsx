811da587f5402751369a2306fe0ed1d67dccaf41
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-10-30 15:52:33 +0100

    refactor(frontend): update EditorPage component to include note content, title, and video ID

--- frontend/src/app/editor/update/[noteId]/page.tsx
+++ frontend/src/app/editor/update/[noteId]/page.tsx
@@ -1,39 +1,22 @@
 "use client";
 
-import {
-	ResizableHandle,
-	ResizablePanel,
-	ResizablePanelGroup,
-} from "@/components/ui/resizable";
-import TextEditor from "@/components/editor/TextEditor";
-import VideoPlayer from "@/components/video/VideoPlayer";
 import { useNote } from "@/context/useNote";
 
-function EditorPage() {
+import EditorPage from "@/components/editor/EditorPage";
+
+function Editor() {
 	const {
 		state: { note },
 	} = useNote();
 
 	return (
-		<section className="height_viewport">
-			<ResizablePanelGroup
-				direction="horizontal"
-				className="flex w-full border"
-			>
-				<ResizablePanel defaultSize={35} className="p-2">
-					<TextEditor
-						initialNoteContent={note?.content}
-						noteTitle={note?.title}
-						action="update"
-					/>
-				</ResizablePanel>
-				<ResizableHandle withHandle />
-				<ResizablePanel defaultSize={65} className="p-2">
-					<VideoPlayer videoId={note?.youtubeId} />
-				</ResizablePanel>
-			</ResizablePanelGroup>
-		</section>
+		<EditorPage
+			action="update"
+			initialNoteContent={note?.content}
+			noteTitle={note?.title}
+			videoId={note?.youtubeId}
+		/>
 	);
 }
 
-export default EditorPage;
+export default Editor;
