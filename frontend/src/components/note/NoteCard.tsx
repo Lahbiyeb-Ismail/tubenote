05ded50a0dd1c7a2486164cf10aaf451bd0324e9
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-10-31 17:52:07 +0100

    refactor(frontend): replace ConfirmationModal with useModal in NoteCard component

--- frontend/src/components/note/NoteCard.tsx
+++ frontend/src/components/note/NoteCard.tsx
@@ -16,7 +16,7 @@ import { Button } from "@/components/ui/button";
 
 import { useNote } from "@/context/useNote";
 import { useLayout } from "@/context/useLayout";
-import ConfirmationModal from "../global/ConfirmationModal";
+import { useModal } from "@/context/useModal";
 
 type NoteCardProps = {
 	note: INote;
@@ -25,110 +25,96 @@ type NoteCardProps = {
 function NoteCard({ note }: NoteCardProps) {
 	const { deleteNote, isLoading, getNote } = useNote();
 	const { isGridLayout } = useLayout();
-	const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+	const { openModal } = useModal();
 
 	const handleDeleteClick = () => {
-		setIsDeleteModalOpen(true);
-	};
-
-	const handleConfirmDelete = () => {
-		deleteNote(note.id);
-		setIsDeleteModalOpen(false);
+		openModal({
+			title: "Confirm Deletion",
+			description:
+				"Are you sure you want to delete this note? This action cannot be undone.",
+			confirmText: "Delete",
+			cancelText: "Cancel",
+			action: "delete",
+			onConfirm: () => deleteNote(note.id),
+		});
 	};
 
 	return (
-		<>
-			<Card
-				className={`overflow-hidden bg-white shadow-md transition-all duration-300 hover:shadow-lg ${
-					isGridLayout ? "hover:scale-105" : ""
-				}`}
-			>
-				<div className={`${isGridLayout ? "" : "flex"}`}>
-					<div
-						className={`relative ${
-							isGridLayout ? "h-48 w-full" : "w-40 flex-shrink-0"
-						}`}
-					>
-						<Image
-							src={note.thumbnail || "/images/placeholder.jpg"}
-							alt={note.title}
-							fill
-							className="object-cover"
-						/>
-					</div>
-					<div
-						className={` flex-grow ${
-							isGridLayout ? "" : "flex flex-row justify-between"
-						}`}
-					>
-						<CardHeader
-							className={`${isGridLayout ? "space-y-2" : "space-y-0"}`}
-						>
-							<div className="flex justify-between items-start">
-								<CardTitle
-									className={`font-bold ${
-										isGridLayout
-											? "text-xl line-clamp-2"
-											: "text-lg line-clamp-1"
-									}`}
-								>
-									{note.videoTitle}
-								</CardTitle>
-							</div>
-							<CardDescription className="text-sm text-muted-foreground line-clamp-1">
-								Note Title: {note.title}
-							</CardDescription>
-						</CardHeader>
-						<CardFooter
-							className={`flex justify-between ${
-								isGridLayout ? "flex-row p-4 pt-0" : "flex-col p-4 space-y-1"
-							}`}
-						>
-							<Button
-								variant="outline"
-								size={!isGridLayout ? "icon" : "sm"}
-								className={`text-blue-600 hover:text-blue-700 hover:bg-blue-50 disabled:opacity-50 disabled:cursor-not-allowed ${
-									!isGridLayout ? "w-10 h-10 p-0" : ""
+		<Card
+			className={`overflow-hidden bg-white shadow-md transition-all duration-300 hover:shadow-lg ${
+				isGridLayout ? "hover:scale-105" : ""
+			}`}
+		>
+			<div className={`${isGridLayout ? "" : "flex"}`}>
+				<div
+					className={`relative ${
+						isGridLayout ? "h-48 w-full" : "w-40 flex-shrink-0"
+					}`}
+				>
+					<Image
+						src={note.thumbnail || "/images/placeholder.jpg"}
+						alt={note.title}
+						fill
+						className="object-cover"
+					/>
+				</div>
+				<div
+					className={` flex-grow ${
+						isGridLayout ? "" : "flex flex-row justify-between"
+					}`}
+				>
+					<CardHeader className={`${isGridLayout ? "space-y-2" : "space-y-0"}`}>
+						<div className="flex justify-between items-start">
+							<CardTitle
+								className={`font-bold ${
+									isGridLayout ? "text-xl line-clamp-2" : "text-lg line-clamp-1"
 								}`}
-								onClick={() => getNote(note.id)}
-								disabled={isLoading}
 							>
-								<PencilIcon
-									className={`h-4 w-4 ${!isGridLayout ? "m-0" : "mr-2"}`}
-								/>
-								{isGridLayout && "Edit"}
-							</Button>
-							<Button
-								variant="outline"
-								size={!isGridLayout ? "icon" : "sm"}
-								className={`text-red-700 hover:text-red-800 hover:bg-red-50 disabled:opacity-50
+								{note.videoTitle}
+							</CardTitle>
+						</div>
+						<CardDescription className="text-sm text-muted-foreground line-clamp-1">
+							Note Title: {note.title}
+						</CardDescription>
+					</CardHeader>
+					<CardFooter
+						className={`flex justify-between ${
+							isGridLayout ? "flex-row p-4 pt-0" : "flex-col p-4 space-y-1"
+						}`}
+					>
+						<Button
+							variant="outline"
+							size={!isGridLayout ? "icon" : "sm"}
+							className={`text-blue-600 hover:text-blue-700 hover:bg-blue-50 disabled:opacity-50 disabled:cursor-not-allowed ${
+								!isGridLayout ? "w-10 h-10 p-0" : ""
+							}`}
+							onClick={() => getNote(note.id)}
+							disabled={isLoading}
+						>
+							<PencilIcon
+								className={`h-4 w-4 ${!isGridLayout ? "m-0" : "mr-2"}`}
+							/>
+							{isGridLayout && "Edit"}
+						</Button>
+						<Button
+							variant="outline"
+							size={!isGridLayout ? "icon" : "sm"}
+							className={`text-red-700 hover:text-red-800 hover:bg-red-50 disabled:opacity-50
                   disabled:cursor-not-allowed ${
 										!isGridLayout ? "w-10 h-10 p-0" : ""
 									}`}
-								onClick={handleDeleteClick}
-								disabled={isLoading}
-							>
-								<Trash2Icon
-									className={`h-4 w-4 ${!isGridLayout ? "m-0" : "mr-2"}`}
-								/>
-								{isGridLayout && "Delete"}
-							</Button>
-						</CardFooter>
-					</div>
+							onClick={handleDeleteClick}
+							disabled={isLoading}
+						>
+							<Trash2Icon
+								className={`h-4 w-4 ${!isGridLayout ? "m-0" : "mr-2"}`}
+							/>
+							{isGridLayout && "Delete"}
+						</Button>
+					</CardFooter>
 				</div>
-			</Card>
-
-			<ConfirmationModal
-				isOpen={isDeleteModalOpen}
-				onClose={() => setIsDeleteModalOpen(false)}
-				onConfirm={handleConfirmDelete}
-				title="Confirm Deletion"
-				message="Are you sure you want to delete this note? This action cannot be undone."
-				action="delete"
-				confirmText="Delete"
-				cancelText="Cancel"
-			/>
-		</>
+			</div>
+		</Card>
 	);
 }
 
