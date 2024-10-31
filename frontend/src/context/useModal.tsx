03f9471832a31b3cac33ad8f0a7db041bb80ae56
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-10-31 17:47:11 +0100

    refactor(frontend): enhance ModalContext to manage modal state and content

--- frontend/src/context/useModal.tsx
+++ frontend/src/context/useModal.tsx
@@ -1,31 +1,63 @@
 "use client";
 
-import { createContext, useContext, useState } from "react";
+import React, {
+	createContext,
+	useState,
+	useContext,
+	type ReactNode,
+} from "react";
 
-type ModalContextType = {
-	isModalOpen: boolean;
-	setIsModalOpen: (isOpen: boolean) => void;
+type ModalContent = {
+	title: string;
+	description: string;
+	confirmText: string;
+	cancelText: string;
+	noteTitle?: string;
+	noteContent?: string;
+	action: "delete" | "create" | "update";
+	onConfirm?: () => void;
 };
+
+type OpenModalContent = ModalContent;
+
+interface ModalContextType {
+	isOpen: boolean;
+	modalContent: ModalContent | null;
+	openModal: (content: OpenModalContent) => void;
+	closeModal: () => void;
+}
 
 const ModalContext = createContext<ModalContextType | undefined>(undefined);
 
-export function ModalProvider({ children }: { children: React.ReactNode }) {
-	const [isModalOpen, setIsModalOpen] = useState(false);
+export function ModalProvider({ children }: { children: ReactNode }) {
+	const [isOpen, setIsOpen] = useState(false);
+	const [modalContent, setModalContent] = useState<ModalContent | null>(null);
+
+	const openModal = (content: OpenModalContent) => {
+		setModalContent(content);
+		setIsOpen(true);
+	};
+
+	const closeModal = () => {
+		setIsOpen(false);
+		setModalContent(null);
+	};
 
 	return (
-		<ModalContext.Provider value={{ isModalOpen, setIsModalOpen }}>
+		<ModalContext.Provider
+			value={{ isOpen, modalContent, openModal, closeModal }}
+		>
 			{children}
 		</ModalContext.Provider>
 	);
 }
 
-const useModal = () => {
+export function useModal() {
 	const context = useContext(ModalContext);
 
 	if (context === undefined) {
 		throw new Error("useModal must be used within a ModalProvider");
 	}
-	return context;
-};
 
-export default useModal;
+	return context;
+}
