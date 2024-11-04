670b9fdc32acf3dbbc1ef93e415b90c33fc13970
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-11-04 14:40:35 +0100

    refactor(frontend): enhance SaveNoteForm layout and integrate cancel button functionality

--- frontend/src/components/editor/SaveNoteForm.tsx
+++ frontend/src/components/editor/SaveNoteForm.tsx
@@ -7,17 +7,22 @@ import { saveNoteFormSchema } from "@/lib/schemas";
 import type { NoteTitle } from "@/types/note.types";
 import { useVideo } from "@/context/useVideo";
 import { useNote } from "@/context/useNote";
+import { Button } from "../ui/button";
 
 type SaveNoteFormProps = {
 	noteTitle?: string;
 	noteContent: string;
+	cancelText: string;
 	action: "update" | "create";
+	closeModal: () => void;
 };
 
 function SaveNoteForm({
 	noteTitle: title,
 	noteContent,
 	action,
+	cancelText,
+	closeModal,
 }: SaveNoteFormProps) {
 	const {
 		register,
@@ -66,7 +71,7 @@ function SaveNoteForm({
 	return (
 		<form
 			onSubmit={handleSubmit(handleNoteSave)}
-			className="mb-1 flex items-center justify-center gap-4"
+			className="mb-1 flex flex-col items-center justify-center gap-4"
 		>
 			<input
 				type="text"
@@ -75,13 +80,18 @@ function SaveNoteForm({
 				{...register("noteTitle")}
 			/>
 
-			<button
-				type="submit"
-				className="inline-flex items-center justify-center whitespace-nowrap rounded-md border-2 bg-[#282828] px-4 py-2 text-center text-sm font-medium text-white transition-all hover:border-[#282828] hover:bg-white hover:text-[#282828] focus:ring-[#282828] focus-visible:outline-none focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-50 dark:focus:ring-[#282828]"
-				disabled={isLoading}
-			>
-				{isLoading ? "Saving..." : "Save"}
-			</button>
+			<div className="flex items-center justify-between w-full">
+				<Button variant="outline" onClick={closeModal}>
+					{cancelText}
+				</Button>
+				<button
+					type="submit"
+					className="inline-flex items-center justify-center whitespace-nowrap rounded-md border-2 bg-[#282828] px-4 py-2 text-center text-sm font-medium text-white transition-all hover:border-[#282828] hover:bg-white hover:text-[#282828] focus:ring-[#282828] focus-visible:outline-none focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-50 dark:focus:ring-[#282828]"
+					disabled={isLoading}
+				>
+					{isLoading ? "Saving..." : "Save"}
+				</button>
+			</div>
 		</form>
 	);
 }
