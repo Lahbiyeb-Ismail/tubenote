23e7b42cbcf04b6b48306b0e559e9825ad627904
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-11-05 15:10:18 +0100

    feat(frontend): wrap GoogleAuthButton in a Link component for OAuth redirection

--- frontend/src/components/auth/GoogleAuthButton.tsx
+++ frontend/src/components/auth/GoogleAuthButton.tsx
@@ -1,18 +1,22 @@
 import Image from "next/image";
 import { Button } from "../ui/button";
+import Link from "next/link";
+import getGoogleOAuthUrl from "@/helpers/getGoogleUrl";
 
 function GoogleAuthButton() {
 	return (
-		<Button variant="outline" className="w-full">
-			<Image
-				className="mr-2 h-4 w-4"
-				src="/images/google.png"
-				alt="google icon"
-				width={512}
-				height={512}
-			/>
-			Sign in with Google
-		</Button>
+		<Link href={getGoogleOAuthUrl()}>
+			<Button variant="outline" className="w-full">
+				<Image
+					className="mr-2 h-4 w-4"
+					src="/images/google.png"
+					alt="google icon"
+					width={512}
+					height={512}
+				/>
+				Sign in with Google
+			</Button>
+		</Link>
 	);
 }
 
