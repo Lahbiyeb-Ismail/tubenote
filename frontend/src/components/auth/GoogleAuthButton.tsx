6440a3a056bbf83baa95b2fcca1a30d957d9d9b0
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-11-06 16:50:25 +0100

    feat(auth): update GoogleAuthButton to use direct redirect for Google authentication

--- frontend/src/components/auth/GoogleAuthButton.tsx
+++ frontend/src/components/auth/GoogleAuthButton.tsx
@@ -1,22 +1,23 @@
 import Image from "next/image";
 import { Button } from "../ui/button";
-import Link from "next/link";
-import getGoogleOAuthUrl from "@/helpers/getGoogleUrl";
+import { GOOGLE_REDIRECT_URI } from "@/utils/constants";
 
 function GoogleAuthButton() {
+	const handleGoogleAuth = () => {
+		window.location.href = `${GOOGLE_REDIRECT_URI}`;
+	};
+
 	return (
-		<Link href={getGoogleOAuthUrl()}>
-			<Button variant="outline" className="w-full">
-				<Image
-					className="mr-2 h-4 w-4"
-					src="/images/google.png"
-					alt="google icon"
-					width={512}
-					height={512}
-				/>
-				Sign in with Google
-			</Button>
-		</Link>
+		<Button variant="outline" className="w-full" onClick={handleGoogleAuth}>
+			<Image
+				className="mr-2 h-4 w-4"
+				src="/images/google.png"
+				alt="google icon"
+				width={512}
+				height={512}
+			/>
+			Sign in with Google
+		</Button>
 	);
 }
 
