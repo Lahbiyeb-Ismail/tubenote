a34124080226fbe01f6916942fdef0d04bc9ec00
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-11-06 16:51:14 +0100

    feat(auth): add AuthCallback component to handle authentication token processing && user redirection

---
+++ frontend/src/app/(auth)/auth/callback/page.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import useGetCurrentUser from "@/hooks/user/useGetCurrentUser";
+
+export default function AuthCallback() {
+	const router = useRouter();
+	const searchParams = useSearchParams();
+	const { refetch: refetchCurrentUser } = useGetCurrentUser();
+	const [status, setStatus] = useState<"loading" | "success" | "error">(
+		"loading",
+	);
+
+	useEffect(() => {
+		async function handleAccessToken() {
+			const accessToken = searchParams.get("access_token") as string;
+			const parsedAccessToken = JSON.parse(decodeURIComponent(accessToken));
+
+			if (!parsedAccessToken) {
+				setStatus("error");
+				setTimeout(() => router.push("/"), 2000);
+				return;
+			}
+
+			try {
+				localStorage.setItem("accessToken", parsedAccessToken);
+
+				await refetchCurrentUser();
+				setStatus("success");
+				router.push("/");
+			} catch (error) {
+				console.error("Authentication error:", error);
+				setStatus("error");
+				setTimeout(() => router.push("/"), 2000);
+			}
+		}
+
+		handleAccessToken();
+	}, [searchParams, router, refetchCurrentUser]);
+
+	return (
+		<div className="flex min-h-screen items-center justify-center">
+			<div className="rounded-lg bg-white p-8 shadow-lg">
+				{status === "loading" && "Processing authentication..."}
+				{status === "error" && "Authentication failed. Please try again."}
+				{status === "success" && "Authentication successful. Redirecting..."}
+			</div>
+		</div>
+	);
+}
