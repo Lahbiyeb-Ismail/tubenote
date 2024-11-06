60199733356a85d22f90759b23979fbd99fd3092
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-11-06 16:42:49 +0100

    feat(auth): integrate Google authentication with passport and add callback route

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import passport from '../lib/passportAuth';
+
 import {
   handleRegister,
   handleLogin,
@@ -18,6 +20,12 @@ router.route('/login').post(validateRequestBody(loginSchema), handleLogin);
 
 router.route('/logout').post(handleLogout);
 
-router.route('/google').get(handleGoogleLogin);
+router
+  .route('/google')
+  .get(passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+router
+  .route('/google/callback')
+  .get(passport.authenticate('google', { session: false }), handleGoogleLogin);
 
 export default router;
