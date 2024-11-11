215d1534cdbed8f002b09683add23f7b1924b0c5
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-11-11 17:30:11 +0100

    feat(email): integrate Nodemailer for email sending configuration

---
+++ backend/src/config/nodemailer.config.ts
@@ -0,0 +1,14 @@
+import nodemailer, { type Transporter } from 'nodemailer';
+import envConfig from './envConfig';
+
+const transporter: Transporter = nodemailer.createTransport({
+  host: envConfig.email.smtp.host,
+  port: +envConfig.email.smtp.port,
+  secure: true, // use TLS
+  auth: {
+    user: envConfig.email.smtp.auth.user,
+    pass: envConfig.email.smtp.auth.password,
+  },
+});
+
+export default transporter;
