4f778db66289b772fc73dbaabcae9663c549100d
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-11-12 16:35:39 +0100

    feat(layout): add Navbar component to Home page for improved navigation

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,13 @@
-import Footer from "@/sections/Footer";
+import Navbar from "@/components/Navbar/Navbar";
+
 import Hero from "@/sections/Hero";
 import HowItWorks from "@/sections/HowItWorks";
+import Footer from "@/sections/Footer";
 
 export default function Home() {
 	return (
 		<main>
+			<Navbar />
 			<Hero />
 			<HowItWorks />
 			<Footer />
