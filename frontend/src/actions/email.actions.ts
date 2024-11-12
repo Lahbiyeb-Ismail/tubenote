37c39dd6de5afab59cbb2357f8f12f95269023f4
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-11-12 14:51:04 +0100

    feat(email): add action to send email verification request

---
+++ frontend/src/actions/email.actions.ts
@@ -0,0 +1,15 @@
+import axiosInstance from '@/lib/axios.lib';
+
+/**
+ * Sends an email verification request to the server.
+ *
+ * @param email - The email address to which the verification email will be sent.
+ * @returns A promise that resolves to an object containing a message.
+ */
+export async function sendEmailVerification(
+  email: string
+): Promise<{ message: string }> {
+  const res = await axiosInstance.post('/send-verification-email', { email });
+
+  return res.data;
+}
