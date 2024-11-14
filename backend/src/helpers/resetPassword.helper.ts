11b98352df026a3011aa3c521731fbb6f3cb2d78
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-11-14 17:26:22 +0100

    feat(resetPassword): add helper function to create reset password email templates

---
+++ backend/src/helpers/resetPassword.helper.ts
@@ -0,0 +1,30 @@
+import fs from 'node:fs';
+import path from 'node:path';
+
+import Handlebars from 'handlebars';
+
+import envConfig from '../config/envConfig';
+
+export function createResetPasswordEmail(token: string) {
+  const resetLink = `${envConfig.server.url}/api/v1/reset-password/${token}`;
+
+  // Read the email templates
+  const htmlTemplate = fs.readFileSync(
+    path.join(__dirname, '../templates/html', 'reset-password-template.html'),
+    'utf-8'
+  );
+  const textTemplate = fs.readFileSync(
+    path.join(__dirname, '../templates/txt', 'reset-password-template.txt'),
+    'utf-8'
+  );
+
+  // Compile the templates
+  const compiledHtmlTemplate = Handlebars.compile(htmlTemplate);
+  const compiledTextTemplate = Handlebars.compile(textTemplate);
+
+  // Render the email with the reset link
+  const htmlContent = compiledHtmlTemplate({ resetLink });
+  const textContent = compiledTextTemplate({ resetLink });
+
+  return { htmlContent, textContent };
+}
