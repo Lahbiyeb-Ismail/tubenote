b05c8f32ec674be43116e3f388854f059073afb2
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-11-14 15:22:20 +0100

    refactor(NavbarButtons): simplify dynamic imports for LoggedIn and LoggedOut buttons

--- frontend/src/components/Navbar/NavbarButtons.tsx
+++ frontend/src/components/Navbar/NavbarButtons.tsx
@@ -1,25 +1,16 @@
 "use client";
 
-import dynamic from "next/dynamic";
 import useGetCurrentUser from "@/hooks/user/useGetCurrentUser";
 
-const LoggedInButtons = dynamic(() => import("./LoggedInButtons"), {
-	ssr: false,
-});
-const LoggedOutButtons = dynamic(() => import("./LoggedOutButtons"), {
-	ssr: false,
-});
+import LoggedInButtons from "./LoggedInButtons";
+import LoggedOutButtons from "./LoggedOutButtons";
 
 function NavbarButtons() {
 	const { data: currentUser, isLoading } = useGetCurrentUser();
 
 	return (
 		<>
-			{currentUser && !isLoading ? (
-				<LoggedInButtons />
-			) : (
-				<LoggedOutButtons />
-			)}
+			{currentUser && !isLoading ? <LoggedInButtons /> : <LoggedOutButtons />}
 		</>
 	);
 }
