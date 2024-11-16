108a77206b240547b9da3b258015ffe3bdbf8b2f
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-11-16 17:05:57 +0100

    feat(services): add updateUser function to modify user data

--- backend/src/services/user.services.ts
+++ backend/src/services/user.services.ts
@@ -1,4 +1,5 @@
 import prismaClient from '../lib/prisma';
+import type { User } from '@prisma/client';
 
 /**
  * Verifies the email of a user by updating the `emailVerified` field to `true`.
@@ -11,4 +12,26 @@ export async function verifyUserEmail(userId: string) {
     where: { id: userId },
     data: { emailVerified: true },
   });
+}
+
+/**
+ * Updates a user with the given data.
+ *
+ * @param userId - The ID of the user to update.
+ * @param data - The data to update the user with.
+ * @returns A promise that resolves to the updated user.
+ */
+
+type UpdateUserInput = Partial<Omit<User, 'id' | 'createdAt' | 'updatedAt'>>;
+
+export async function updateUser(
+  userId: string,
+  data: UpdateUserInput
+): Promise<User> {
+  const updatedUser = await prismaClient.user.update({
+    where: { id: userId },
+    data,
+  });
+
+  return updatedUser;
 }
