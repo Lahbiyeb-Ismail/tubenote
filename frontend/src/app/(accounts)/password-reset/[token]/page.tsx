858664252a12b2c451687ef2dfc2346d5c231543
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-11-16 18:04:38 +0100

    feat(pages): integrate ResetPasswordForm into ResetPasswordPage

--- frontend/src/app/(accounts)/password-reset/[token]/page.tsx
+++ frontend/src/app/(accounts)/password-reset/[token]/page.tsx
@@ -1,5 +1,7 @@
+import ResetPasswordForm from "@/components/forms/ResetPasswordForm";
+
 function ResetPasswordPage({ params }: { params: { token: string } }) {
-	return <h1>{params.token}</h1>;
+	return <ResetPasswordForm token={params.token} />;
 }
 
 export default ResetPasswordPage;
