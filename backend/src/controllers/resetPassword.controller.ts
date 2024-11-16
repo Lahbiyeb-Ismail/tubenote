ada9676c605b2c69c53692d094222050c16f360b
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-11-16 17:09:21 +0100

    feat(controllers): implement handleResetPassword function for password reset process

--- backend/src/controllers/resetPassword.controller.ts
+++ backend/src/controllers/resetPassword.controller.ts
@@ -2,14 +2,20 @@ import type { Response } from 'express';
 import httpStatus from 'http-status';
 
 import type { TypedRequest } from '../types';
-import type { ForgotPasswordBody } from '../types/resetPassword.type';
+import type {
+  ForgotPasswordBody,
+  ResetPasswordBody,
+} from '../types/resetPassword.type';
 import { getUser } from '../helpers/auth.helper';
 import {
   createResetPasswordToken,
-  getResetPasswordToken,
+  deleteResetPasswordToken,
+  findResetPasswordToken,
 } from '../services/resetPassword.services';
 import { sendEmail } from '../utils/sendEmail';
 import { createResetPasswordEmail } from '../helpers/resetPassword.helper';
+import { hashPassword } from '../services/auth.services';
+import { updateUser } from '../services/user.services';
 
 /**
  * Handles the password reset process for a user.
@@ -43,7 +49,9 @@ export async function handleForgotPassword(
     return;
   }
 
-  const isResetTokenAlreadySent = await getResetPasswordToken(user.id);
+  const isResetTokenAlreadySent = await findResetPasswordToken({
+    userId: user.id,
+  });
 
   if (isResetTokenAlreadySent) {
     res.status(httpStatus.BAD_REQUEST).json({
@@ -66,4 +74,59 @@ export async function handleForgotPassword(
   res
     .status(httpStatus.OK)
     .json({ message: 'Password reset link sent to your email.' });
+}
+
+/**
+ * Handles the password reset process.
+ *
+ * @param req - The request object containing the reset token and new password.
+ * @param res - The response object used to send back the appropriate HTTP response.
+ *
+ * The function performs the following steps:
+ * 1. Validates the presence of the reset token and new password in the request.
+ * 2. Checks if the provided reset token is valid and not expired.
+ * 3. Hashes the new password.
+ * 4. Updates the user's password in the database.
+ * 5. Deletes the used reset token from the database.
+ * 6. Sends a success response if the password reset is successful.
+ *
+ * @throws Will send a 400 Bad Request response if the token or password is missing or invalid.
+ */
+export async function handleResetPassword(
+  req: TypedRequest<ResetPasswordBody>,
+  res: Response
+) {
+  const { token } = req.params;
+  const { password } = req.body;
+
+  if (!token) {
+    res.status(httpStatus.BAD_REQUEST).json({ message: 'Token is required.' });
+    return;
+  }
+
+  if (!password) {
+    res
+      .status(httpStatus.BAD_REQUEST)
+      .json({ message: 'Password is required.' });
+    return;
+  }
+
+  const resetToken = await findResetPasswordToken({ token });
+
+  if (!resetToken) {
+    res
+      .status(httpStatus.BAD_REQUEST)
+      .json({ message: 'Invalid or expired token.' });
+    return;
+  }
+
+  const hashedPassword = await hashPassword(password);
+
+  await updateUser(resetToken.userId, {
+    password: hashedPassword,
+  });
+
+  await deleteResetPasswordToken(resetToken.userId);
+
+  res.status(httpStatus.OK).json({ message: 'Password reset successful.' });
 }
