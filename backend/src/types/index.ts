f46a26deaea36306bfb555cc738f7d9f09a846ca
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-11-18 16:15:18 +0100

    feat(types): extend Express Request interface to include resetToken property

--- backend/src/types/index.ts
+++ backend/src/types/index.ts
@@ -10,6 +10,21 @@ export interface PayloadRequest extends Request {
   payload?: Payload;
 }
 
+import type { ResetPasswordToken } from '@prisma/client';
+
+/**
+ * Extends the Express Request interface to include a `resetToken` property.
+ *
+ * @property {ResetPasswordToken} resetToken - The token used for resetting the password.
+ */
+declare global {
+  namespace Express {
+    interface Request {
+      resetToken: ResetPasswordToken;
+    }
+  }
+}
+
 // More strictly typed Express.Request type
 // https://stackoverflow.com/questions/34508081/how-to-add-typescript-definitions-to-express-req-res
 export type TypedRequest<
