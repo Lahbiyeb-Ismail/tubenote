13e8bc7cce4114e97f1bf38f8260dc0c51faddcb
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-11-18 15:40:52 +0100

    feat(hooks): add custom hook to verify password reset token

---
+++ frontend/src/hooks/password-reset/useVerifyResetToken.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { verifyPasswordResetToken } from "@/actions/password.actions";
+import { useQuery } from "@tanstack/react-query";
+
+function useVerifyResetToken(token: string) {
+	return useQuery({
+		queryKey: ["reset-token", token],
+		queryFn: () => verifyPasswordResetToken(token),
+		// Enable the query only if the token is available.
+		enabled: !!token,
+		// Prevent refetching by setting a very long stale time
+		staleTime: Number.POSITIVE_INFINITY,
+		// Disable automatic refetching
+		refetchOnWindowFocus: false,
+		refetchOnReconnect: false,
+		// Don't retry on failure
+		retry: false,
+	});
+}
+
+export default useVerifyResetToken;
