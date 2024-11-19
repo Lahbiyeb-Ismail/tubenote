c3d7bda199d88f1cff67b7c063cdf5533a5bc336
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-11-19 15:08:18 +0100

    feat(SidebarNav): replace list items with SidebarLink component for improved navigation

--- frontend/src/components/dashboards/SidebarNav.tsx
+++ frontend/src/components/dashboards/SidebarNav.tsx
@@ -1,5 +1,5 @@
-import Link from "next/link";
 import { sidebarMenuLinks } from "@/utils/constants";
+import SidebarLink from "./SidebarLink";
 
 type SidebarNavProps = {
 	pathname: string;
@@ -15,34 +15,12 @@ function SidebarNav({ pathname, isOpen }: SidebarNavProps) {
 		>
 			<ul className="space-y-2">
 				{sidebarMenuLinks.map((item) => (
-					<li
+					<SidebarLink
 						key={item.name}
-						className={`rounded-lg p-2 transition duration-150 ease-in-out ${
-							item.href === pathname
-								? "bg-blue-100 text-blue-600"
-								: "text-gray-600 hover:bg-gray-100"
-						}`}
-					>
-						<Link
-							href={item.href}
-							className={`flex items-center ${
-								isOpen ? "" : "justify-center"
-							} h-full`}
-						>
-							<div
-								className={`flex items-center justify-center ${
-									isOpen ? "mr-3" : "w-full h-full"
-								}`}
-							>
-								<item.icon
-									className={`transition-all duration-300 ${
-										isOpen ? "" : "w-6 h-6"
-									}`}
-								/>
-							</div>
-							{isOpen && <span>{item.name}</span>}
-						</Link>
-					</li>
+						item={item}
+						pathname={pathname}
+						isOpen={isOpen}
+					/>
 				))}
 			</ul>
 		</nav>
