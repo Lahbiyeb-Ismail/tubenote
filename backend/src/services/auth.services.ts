54a3d10ba742fa26504bf9903218ee5bfa089f6f
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-11-19 16:03:53 +0100

    feat(auth): add createNewUser function to handle user registration with password hashing

--- backend/src/services/auth.services.ts
+++ backend/src/services/auth.services.ts
@@ -1,4 +1,7 @@
 import bcrypt from 'bcryptjs';
+import type { Prisma, User } from '@prisma/client';
+
+import prismaClient from '../lib/prisma';
 
 /**
  * Hashes a given password using bcrypt.
@@ -10,4 +13,26 @@ export async function hashPassword(password: string): Promise<string> {
   const salt = await bcrypt.genSalt(10);
 
   return bcrypt.hash(password, salt);
+}
+
+/**
+ * Creates a new user in the database with the provided user data.
+ * The user's password is hashed before being stored.
+ *
+ * @param {Prisma.UserCreateInput} userData - The data for the new user, including the password.
+ * @returns {Promise<Prisma.User>} The newly created user.
+ */
+export async function createNewUser(
+  userData: Prisma.UserCreateInput
+): Promise<User> {
+  const hashedpassword = await hashPassword(userData.password);
+
+  const newUser = await prismaClient.user.create({
+    data: {
+      ...userData,
+      password: hashedpassword,
+    },
+  });
+
+  return newUser;
 }
