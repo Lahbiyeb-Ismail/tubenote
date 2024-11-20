5c9e6e2d1a602a8134143d2927994fea8aaa4264
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-11-20 16:49:53 +0100

    feat(note): add fetchUserNotes function to retrieve notes for a specific user

---
+++ backend/src/services/note.services.ts
@@ -0,0 +1,18 @@
+import type { Note } from '@prisma/client';
+import prismaClient from '../lib/prisma';
+
+/**
+ * Fetches all notes associated with a specific user.
+ *
+ * @param userId - The unique identifier of the user whose notes are to be fetched.
+ * @returns A promise that resolves to an array of `Note` objects belonging to the specified user.
+ */
+export async function fetchUserNotes(userId: string): Promise<Note[]> {
+  const notes = await prismaClient.note.findMany({
+    where: {
+      userId,
+    },
+  });
+
+  return notes;
+}
