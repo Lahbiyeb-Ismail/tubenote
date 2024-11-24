7d840d43a01c88a831d897d49b126632c1789675
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-11-24 15:08:17 +0100

    refactor(axios): simplify AxiosInstance setup by using interceptors for request && response handling

--- frontend/src/lib/axios.lib.ts
+++ frontend/src/lib/axios.lib.ts
@@ -1,29 +1,17 @@
 import axios from 'axios';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL;
-
-const getAccessToken = (): string | null => {
-  if (typeof window !== 'undefined') {
-    return localStorage.getItem('accessToken');
-  }
-  return null;
-};
+import { API_URL } from '@/utils/constants';
+import {
+  setupRequestInterceptor,
+  setupResponseInterceptor,
+} from '@/interceptors/auth.interceptors';
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
   withCredentials: true,
-  headers: {
-    'Content-Type': 'application/json',
-  },
 });
 
-// Add an interceptor to dynamically set the Authorization header
-axiosInstance.interceptors.request.use((config) => {
-  const token = getAccessToken();
-  if (token) {
-    config.headers.Authorization = `Bearer ${JSON.parse(token)}`;
-  }
-  return config;
-});
+setupRequestInterceptor(axiosInstance);
+setupResponseInterceptor(axiosInstance);
 
 export default axiosInstance;
