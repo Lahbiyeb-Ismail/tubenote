d47907806b7487e3a3ccfe6e9aae5e46b1cfcfcb
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-11-25 17:58:52 +0100

    feat(notes): integrate pagination into NotesPage for improved user navigation

--- frontend/src/app/(dashboard)/notes/page.tsx
+++ frontend/src/app/(dashboard)/notes/page.tsx
@@ -7,15 +7,22 @@ import Header from "@/components/dashboards/Header";
 import NotesList from "@/components/note/NotesList";
 import Laoder from "@/components/global/Loader";
 import NoDataFound from "@/components/dashboards/NoDataFound";
+import { useState } from "react";
+import Pagination from "@/components/global/Pagination";
 
 function NotesPage() {
-	const { data, isLoading } = useGetUserNotes();
+	const [currentPage, setCurrentPage] = useState(1);
+	const { data, isLoading } = useGetUserNotes({ page: currentPage, limit: 8 });
+
+	const handlePageChange = (newPage: number) => {
+		setCurrentPage(newPage);
+	};
 
 	if (isLoading) return <Laoder />;
 
 	return (
 		<>
-			{!data || data.length === 0 ? (
+			{!data || data.notes.length === 0 ? (
 				<NoDataFound title="You don't have any notes yet." />
 			) : (
 				<div className="min-h-screen flex-1 bg-gray-100">
@@ -24,7 +31,12 @@ function NotesPage() {
 						<div className="flex justify-end">
 							<AddNoteForm />
 						</div>
-						<NotesList notes={data} />
+						<NotesList notes={data.notes} />
+						<Pagination
+							currentPage={currentPage}
+							totalPages={data.pagination.totalPages}
+							onPageChange={handlePageChange}
+						/>
 					</main>
 				</div>
 			)}
