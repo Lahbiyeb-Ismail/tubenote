af019d8b8894f0b95c48d7afe86e97581ef38969
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-11-28 14:34:00 +0100

    feat(CardContent): make card title clickable to navigate to note details

--- frontend/src/components/global/CardContent.tsx
+++ frontend/src/components/global/CardContent.tsx
@@ -1,14 +1,17 @@
 import { CardHeader, CardDescription, CardTitle } from "@/components/ui/card";
+import Link from "next/link";
 
 interface CardContentProps {
 	cardTitle: string;
 	cardDescription: string;
+	noteId: string;
 	isGridLayout: boolean;
 }
 
 function CardContent({
 	cardTitle,
 	cardDescription,
+	noteId,
 	isGridLayout,
 }: CardContentProps) {
 	return (
@@ -16,11 +19,11 @@ function CardContent({
 			className={`${isGridLayout ? "space-y-2" : "space-y-0"} px-6 py-0`}
 		>
 			<CardTitle
-				className={`font-bold ${
+				className={`font-bold hover:underline transition-all ${
 					isGridLayout ? "text-xl line-clamp-2" : "text-lg line-clamp-1"
 				}`}
 			>
-				{cardTitle}
+				<Link href={`/notes/${noteId}`}>{cardTitle}</Link>
 			</CardTitle>
 			<CardDescription className="text-sm text-muted-foreground line-clamp-1">
 				{cardDescription}
