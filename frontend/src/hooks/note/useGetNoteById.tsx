bcec061b19633d317a7931f6758f3c3856b674cb
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-11-28 15:22:27 +0100

    refactor(useGetNoteById): switch from mutation to query for note retrieval

--- frontend/src/hooks/note/useGetNoteById.tsx
+++ frontend/src/hooks/note/useGetNoteById.tsx
@@ -1,46 +1,14 @@
-import type React from "react";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
 
 import { getNoteById } from "@/actions/note.actions";
-import type { TypedError } from "@/types";
-import type { NoteAction } from "@/types/note.types";
-import { useRouter } from "next/navigation";
 
-function useGetNoteById(dispatch: React.Dispatch<NoteAction>) {
-	const queryClient = useQueryClient();
-	const router = useRouter();
-
-	return useMutation({
-		mutationFn: getNoteById,
-		onSuccess: (data) => {
-			queryClient.invalidateQueries({ queryKey: ["notes"] });
-
-			dispatch({
-				type: "GET_NOTE_SUCCESS",
-				payload: {
-					note: data,
-					success: true,
-				},
-			});
-
-			router.push(`/editor/update/${data.id}`);
-		},
-		onError: (error: TypedError) => {
-			if (error.response) {
-				dispatch({
-					type: "GET_NOTE_FAIL",
-					payload: { message: error.response.data.message, success: false },
-				});
-			} else {
-				dispatch({
-					type: "GET_NOTE_FAIL",
-					payload: {
-						message: "Geting Note Failed. Please try again.",
-						success: false,
-					},
-				});
-			}
-		},
+function useGetNoteById(noteId: string) {
+	return useQuery({
+		queryKey: ["note", noteId],
+		queryFn: () => getNoteById(noteId),
+		enabled: !!noteId,
 	});
 }
 
