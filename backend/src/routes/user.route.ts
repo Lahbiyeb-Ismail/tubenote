7c48244ad4a269adf387f10c7475a6306cd102c9
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-12-01 14:22:03 +0100

    refactor(route): updating user routes by unify authentication middleware

--- backend/src/routes/user.route.ts
+++ backend/src/routes/user.route.ts
@@ -5,28 +5,27 @@ import {
   updateCurrentUser,
   updateUserPassword,
 } from '../controllers/user.controller';
+
 import isAuthenticated from '../middlewares/isAuthenticated';
 import validateRequestBody from '../middlewares/validateRequestBody';
+
 import { updatePasswordSchema, updateUserSchema } from '../schemas/user.schema';
 
 const router = Router();
 
-router.route('/me').get(isAuthenticated, getCurrentUser);
+// - isAuthenticated: Ensures the user is authenticated before accessing any user routes.
+router.use(isAuthenticated);
 
+// - GET /me: Get the current user's information
+// - PATCH /me: Update the current user's information (requires request body validation)
 router
-  .route('/update-current')
-  .patch(
-    isAuthenticated,
-    validateRequestBody(updateUserSchema),
-    updateCurrentUser
-  );
+  .route('/me')
+  .get(getCurrentUser)
+  .patch(validateRequestBody(updateUserSchema), updateCurrentUser);
 
+// - PATCH /update-password: Update the current user's password (requires request body validation)
 router
   .route('/update-password')
-  .patch(
-    isAuthenticated,
-    validateRequestBody(updatePasswordSchema),
-    updateUserPassword
-  );
+  .patch(validateRequestBody(updatePasswordSchema), updateUserPassword);
 
 export default router;
