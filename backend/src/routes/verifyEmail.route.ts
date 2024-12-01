29b6fe59fb3aa12a9702f8741156ead65c5412ca
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-12-01 14:25:40 +0100

    refactor(route): enhance email verification routes with improved validation and structure

--- backend/src/routes/verifyEmail.route.ts
+++ backend/src/routes/verifyEmail.route.ts
@@ -1,17 +1,21 @@
 import { Router } from 'express';
-import validateRequestBody from '../middlewares/validateRequestBody';
-import validateRequestParams from '../middlewares/validateRequestParams';
-import {
-  sendVerifyEmailSchema,
-  verifyEmailSchema,
-} from '../schemas/verifyEmail.schema';
+
 import {
   sendVerificationEmailHandler,
   verifyEmailHandler,
 } from '../controllers/verifyEmail.controller';
 
+import validateRequestBody from '../middlewares/validateRequestBody';
+import validateRequestParams from '../middlewares/validateRequestParams';
+
+import {
+  sendVerifyEmailSchema,
+  verifyEmailSchema,
+} from '../schemas/verifyEmail.schema';
+
 const router = Router();
 
+// - POST /send-verification-email: Send a verification email to the user (requires request body validation)
 router
   .route('/send-verification-email')
   .post(
@@ -19,7 +23,7 @@ router
     sendVerificationEmailHandler
   );
 
-// /verify-email
+// - GET /verify-email/:token: Verify the user's email using the provided token (requires request params validation)
 router
   .route('/verify-email/:token')
   .get(validateRequestParams(verifyEmailSchema), verifyEmailHandler);
