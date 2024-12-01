d8501a91c65146e149c74a1acafaf2e5f2a856f9
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-12-01 14:15:35 +0100

    refactor(route): reorganize middleware usage && enhance request validation for password reset routes

--- backend/src/routes/resetPassword.route.ts
+++ backend/src/routes/resetPassword.route.ts
@@ -1,12 +1,6 @@
 import { Router } from 'express';
 
 import {
-  passwordResetBodySchema,
-  passwordResetParamsSchema,
-  forgotPasswordBodySchema,
-} from '../schemas/resetPassword.schema';
-
-import {
   handleForgotPassword,
   handleResetPassword,
   handleResetPasswordTokenVerification,
@@ -16,26 +10,36 @@ import validateRequestBody from '../middlewares/validateRequestBody';
 import validateRequestParams from '../middlewares/validateRequestParams';
 import { verifyPasswordResetToken } from '../middlewares/verifyPasswordResetToken';
 
+import {
+  passwordResetBodySchema,
+  passwordResetParamsSchema,
+  forgotPasswordBodySchema,
+} from '../schemas/resetPassword.schema';
+
 const router = Router();
 
+// - POST /forgot-password: Initiate the password reset process (requires request body validation).
 router
   .route('/forgot-password')
   .post(validateRequestBody(forgotPasswordBodySchema), handleForgotPassword);
 
+// Apply verifyPasswordResetToken middleware to all routes below
+router.use(verifyPasswordResetToken);
+
+// - GET /reset-password/:token/verify: Verify the password reset token (requires request params validation).
 router
   .route('/reset-password/:token/verify')
   .get(
     validateRequestParams(passwordResetParamsSchema),
-    verifyPasswordResetToken,
     handleResetPasswordTokenVerification
   );
 
+// - POST /reset-password/:token: Reset the password using a valid token (requires request params and body validation).
 router
   .route('/reset-password/:token')
   .post(
     validateRequestParams(passwordResetParamsSchema),
     validateRequestBody(passwordResetBodySchema),
-    verifyPasswordResetToken,
     handleResetPassword
   );
 
