739c39379095527db7c1ef03f2959a39a9a58702
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-12-01 14:09:32 +0100

    refactor(auth): add request body validation and enhance authentication routes

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -13,20 +13,26 @@ import { registrationSchema, loginSchema } from '../schemas/auth.schema';
 
 const router = Router();
 
+// - POST /register: Register a new user (requires request body validation).
 router
   .route('/register')
   .post(validateRequestBody(registrationSchema), handleRegister);
 
+// - POST /login: Authenticate a user (requires request body validation).
 router.route('/login').post(validateRequestBody(loginSchema), handleLogin);
 
+// - POST /logout: Log out the current user.
 router.route('/logout').post(handleLogout);
 
+// - POST /refresh: Refresh the user's access token.
 router.route('/refresh').post(handleRefreshToken);
 
+// - GET /google: Initiate Google OAuth authentication.
 router
   .route('/google')
   .get(passport.authenticate('google', { scope: ['profile', 'email'] }));
 
+// - GET /google/callback: Handle the Google OAuth callback.
 router
   .route('/google/callback')
   .get(passport.authenticate('google', { session: false }), handleGoogleLogin);
