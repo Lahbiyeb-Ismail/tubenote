0e544dab1e8ad2f996896f45a75457e1a4efc4db
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-12-02 14:40:25 +0100

    refactor(notes): replace INote type with Note type in NotesList component

--- frontend/src/components/note/NotesList.tsx
+++ frontend/src/components/note/NotesList.tsx
@@ -1,28 +1,30 @@
 "use client";
-
-import { useLayout } from "@/context/useLayout";
-import type { INote } from "@/types/note.types";
 import React from "react";
+
+import type { Note } from "@/types/note.types";
+
 import NoteCard from "./NoteCard";
 
+import { useLayout } from "@/context/useLayout";
+
 type NotesListProps = {
-  notes?: INote[];
+	notes?: Note[];
 };
 
 function NotesList({ notes }: NotesListProps) {
-  const { isGridLayout } = useLayout();
+	const { isGridLayout } = useLayout();
 
-  return (
-    <div className="md:px-4 py-6">
-      <div
-        className={`${isGridLayout ? "grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4" : "space-y-4"}`}
-      >
-        {notes?.map((note) => (
-          <NoteCard key={note.id} note={note} />
-        ))}
-      </div>
-    </div>
-  );
+	return (
+		<div className="md:px-4 py-6">
+			<div
+				className={`${isGridLayout ? "grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4" : "space-y-4"}`}
+			>
+				{notes?.map((note) => (
+					<NoteCard key={note.id} note={note} />
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default NotesList;
