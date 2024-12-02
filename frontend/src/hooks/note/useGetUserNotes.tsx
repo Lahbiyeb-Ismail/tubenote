794cb31f7d73cd4ac9b6623f5161b6102cb8e455
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-12-02 17:03:04 +0100

    refactor(note): update useGetUserNotes to use Note type and make parameters optional

--- frontend/src/hooks/note/useGetUserNotes.tsx
+++ frontend/src/hooks/note/useGetUserNotes.tsx
@@ -6,25 +6,25 @@ import { getUserNotes } from "@/actions/note.actions";
 import { getStorageValue } from "@/utils/localStorage";
 
 import type { Pagination } from "@/types";
-import type { INote } from "@/types/note.types";
+import type { Note } from "@/types/note.types";
 
 import { DEFAULT_PAGE, PAGE_LIMIT } from "@/utils/constants";
 
 function useGetUserNotes({
-  page = DEFAULT_PAGE,
-  limit = PAGE_LIMIT,
-}: { page: number; limit: number }): UseQueryResult<{
-  notes: INote[];
-  pagination: Pagination;
+	page = DEFAULT_PAGE,
+	limit = PAGE_LIMIT,
+}: { page?: number; limit?: number }): UseQueryResult<{
+	notes: Note[];
+	pagination: Pagination;
 }> {
-  const accessToken = getStorageValue<string>("accessToken");
+	const accessToken = getStorageValue<string>("accessToken");
 
-  return useQuery({
-    queryKey: ["notes", page, limit],
-    queryFn: () => getUserNotes({ page, limit }),
-    enabled: !!accessToken,
-    staleTime: 5 * 60 * 1000, // 5 minutes
-  });
+	return useQuery({
+		queryKey: ["notes", page, limit],
+		queryFn: () => getUserNotes({ page, limit }),
+		enabled: !!accessToken,
+		staleTime: 5 * 60 * 1000, // 5 minutes
+	});
 }
 
 export default useGetUserNotes;
