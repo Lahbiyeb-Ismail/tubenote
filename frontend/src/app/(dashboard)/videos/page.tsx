9df9995212d9c4c467b5e4da111efadddfd5506b
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-12-02 16:25:17 +0100

    feat(video): enhance VideosPage to handle loading, error states, and display video list

--- frontend/src/app/(dashboard)/videos/page.tsx
+++ frontend/src/app/(dashboard)/videos/page.tsx
@@ -6,29 +6,30 @@ import AddNoteForm from "@/components/dashboards/AddNoteForm";
 import Header from "@/components/dashboards/Header";
 import NoDataFound from "@/components/dashboards/NoDataFound";
 import Loader from "@/components/global/Loader";
+import VideosList from "@/components/video/VideosList";
 
 function VideosPage() {
-  const { data, isLoading } = useGetUserVideos();
+	const { data: videos, isLoading, isError } = useGetUserVideos();
 
-  if (isLoading) return <Loader />;
+	if (isError) return <div>Something went wrong</div>;
 
-  return (
-    <>
-      {!data ? (
-        <NoDataFound title="You don't have any videos yet." />
-      ) : (
-        <div className="min-h-screen flex-1 bg-gray-100">
-          <Header title="Your Video" />
-          <main className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-            <div className="flex justify-end">
-              <AddNoteForm />
-            </div>
-            {/* <NotesList notes={data} /> */}
-          </main>
-        </div>
-      )}
-    </>
-  );
+	if (isLoading) return <Loader />;
+
+	if (!videos || videos.length === 0) {
+		return <NoDataFound title="You don't have any videos yet." />;
+	}
+
+	return (
+		<div className="min-h-screen flex-1 bg-gray-100">
+			<Header title="Your Video" />
+			<main className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+				<div className="flex justify-end">
+					<AddNoteForm />
+				</div>
+				<VideosList videos={videos} />
+			</main>
+		</div>
+	);
 }
 
 export default VideosPage;
