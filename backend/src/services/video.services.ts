a8e944e9594c9138d92464fa7c78f952cd1e14ae
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-12-02 13:40:11 +0100

    refactor(video.services): update findVideo function to accept user ID and improve query parameters

--- backend/src/services/video.services.ts
+++ backend/src/services/video.services.ts
@@ -4,18 +4,30 @@ import prismaClient from '../lib/prisma';
 import handleAsyncOperation from '../utils/handleAsyncOperation';
 import { fetchYoutubeVideoDetails } from '../helpers/video.helper';
 
+type UserId = {
+  userId: string;
+};
+type VideoId = {
+  videoId: string;
+};
+
 /**
- * Finds a video by its YouTube ID.
+ * Finds a video based on the provided video ID and user ID.
  *
- * @param videoId - The YouTube ID of the video to find.
- * @returns A promise that resolves to the video if found, or null if not found.
+ * @param {Object} params - The parameters for finding the video.
+ * @param {string} params.videoId - The ID of the video to find.
+ * @param {string} params.userId - The ID of the user who owns the video.
+ * @returns {Promise<Video | null>} A promise that resolves to the found video or null if no video is found.
  * @throws Will throw an error if the operation fails.
  */
-export async function findVideo(videoId: string): Promise<Video | null> {
+export async function findVideo({
+  videoId,
+  userId,
+}: VideoId & UserId): Promise<Video | null> {
   return handleAsyncOperation(
     () =>
       prismaClient.video.findFirst({
-        where: { youtubeId: videoId },
+        where: { youtubeId: videoId, userId },
       }),
     { errorMessage: 'Faild to find video' }
   );
