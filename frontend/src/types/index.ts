6f812846c12725754405a1b08d8855f7ea474bdf
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-12-02 17:04:50 +0100

    refactor(types): update Pagination type for consistency and clarity

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -3,19 +3,19 @@ import type { AxiosError } from "axios";
 export type TypedError = AxiosError<{ message: string }>;
 
 export type User = {
-  id: string;
-  username: string;
-  email: string;
-  profilePicture: string | null;
-  emailVerified: boolean;
-  createAt: string;
-  updatedAt: string;
+	id: string;
+	username: string;
+	email: string;
+	profilePicture: string | null;
+	emailVerified: boolean;
+	createAt: string;
+	updatedAt: string;
 };
 
 export type Pagination = {
-  totalNotes: number;
-  totalPages: number;
-  currentPage: number;
-  hasNextPage: boolean;
-  hasPrevPage: boolean;
+	totalItems: number;
+	totalPages: number;
+	currentPage: number;
+	hasNextPage: boolean;
+	hasPrevPage: boolean;
 };
