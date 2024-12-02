9648f094d506d9fc5e819e4cf4d6482b9be92591
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-12-02 14:04:35 +0100

    feat(editor): integrate useGetVideoById hook for video data fetching with loading and error handling

--- frontend/src/app/editor/create/[videoId]/page.tsx
+++ frontend/src/app/editor/create/[videoId]/page.tsx
@@ -1,15 +1,28 @@
 "use client";
 
-import { useVideo } from "@/context/useVideo";
+import useGetVideoById from "@/hooks/video/useGetVideoById";
 
 import EditorPage from "@/components/editor/EditorPage";
+import Loader from "@/components/global/Loader";
 
-function Editor() {
-  const {
-    state: { video },
-  } = useVideo();
+function Editor({ params }: { params: { videoId: string } }) {
+	const { videoId } = params;
 
-  return <EditorPage videoId={video?.youtubeId as string} />;
+	const { data: video, isLoading, isError } = useGetVideoById(videoId);
+
+	if (isError) {
+		return <div>Error...</div>;
+	}
+
+	if (isLoading || !video) {
+		return (
+			<div className="min-h-screen flex items-center justify-center container max-w-4xl mx-auto px-4 py-8">
+				<Loader />
+			</div>
+		);
+	}
+
+	return <EditorPage videoId={video.youtubeId} />;
 }
 
 export default Editor;
