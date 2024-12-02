fbc6aff9db5342129323d511bf8506ce151297fe
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-12-02 13:17:56 +0100

    refactor: simplify layout structure by replacing ResizablePanelGroup with ResizablePanels component

--- frontend/src/components/editor/EditorPage.tsx
+++ frontend/src/components/editor/EditorPage.tsx
@@ -1,83 +1,56 @@
 "use client";
 
-import {
-  ResizableHandle,
-  ResizablePanel,
-  ResizablePanelGroup,
-} from "@/components/ui/resizable";
+import { Suspense } from "react";
+import dynamic from "next/dynamic";
+
 import VideoPlayer from "@/components/video/VideoPlayer";
-import { useEffect, useState } from "react";
+
+import ResizablePanels from "@/components/global/ResizablePanels";
+import Sidebar from "@/components/dashboards/Sidebar";
 
 import { useLayout } from "@/context/useLayout";
-import dynamic from "next/dynamic";
-import { Suspense } from "react";
-import Sidebar from "../dashboards/Sidebar";
 
 const AppMDXEditor = dynamic(() => import("./AppMDXEditor"), { ssr: false });
 
 type EditorPageProps = {
-  videoId: string;
-  initialNoteContent: string;
-  noteTitle: string;
-  noteId: string;
-  action?: "create" | "update";
+	videoId: string;
+	initialNoteContent?: string;
+	noteTitle?: string;
+	noteId?: string;
+	action?: "create" | "update";
 };
 
 function EditorPage({
-  noteId,
-  initialNoteContent,
-  noteTitle,
-  videoId,
-  action = "create",
+	noteId,
+	initialNoteContent,
+	noteTitle,
+	videoId,
+	action = "create",
 }: EditorPageProps) {
-  const [direction, setDirection] = useState<"horizontal" | "vertical">(
-    "horizontal"
-  );
-  const { isSidebarOpen } = useLayout();
+	const { isSidebarOpen } = useLayout();
 
-  useEffect(() => {
-    function changeDirection() {
-      if (window.innerWidth < 768) {
-        setDirection("vertical");
-      } else {
-        setDirection("horizontal");
-      }
-    }
-
-    changeDirection();
-
-    window.addEventListener("resize", changeDirection);
-    return () => window.removeEventListener("resize", changeDirection);
-  }, []);
-
-  return (
-    <div className="flex h-screen">
-      <Sidebar />
-      <div
-        className={`flex-grow transition-all duration-300 ${isSidebarOpen ? "ml-64" : "ml-20"}`}
-      >
-        <ResizablePanelGroup
-          direction={direction}
-          className="flex w-full h-full"
-        >
-          <ResizablePanel defaultSize={50} className="p-2 relative">
-            <Suspense fallback={null}>
-              <AppMDXEditor
-                action={action}
-                initialNoteContent={initialNoteContent}
-                noteTitle={noteTitle}
-                noteId={noteId}
-              />
-            </Suspense>
-          </ResizablePanel>
-          <ResizableHandle withHandle />
-          <ResizablePanel defaultSize={50} className="p-2">
-            <VideoPlayer videoId={videoId} />
-          </ResizablePanel>
-        </ResizablePanelGroup>
-      </div>
-    </div>
-  );
+	return (
+		<div className="flex h-screen">
+			<Sidebar />
+			<div
+				className={`flex-grow transition-all duration-300 ${isSidebarOpen ? "ml-64" : "ml-20"}`}
+			>
+				<ResizablePanels
+					leftSideContent={
+						<Suspense fallback={null}>
+							<AppMDXEditor
+								action={action}
+								initialNoteContent={initialNoteContent}
+								noteTitle={noteTitle}
+								noteId={noteId}
+							/>
+						</Suspense>
+					}
+					rightSideContent={<VideoPlayer videoId={videoId} />}
+				/>
+			</div>
+		</div>
+	);
 }
 
 export default EditorPage;
