35f59099f13421a7ba57f91c1a50a7cd0d047f2a
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-12-02 16:24:38 +0100

    feat(note): refactor NoteCard to use Card && CardFooter Wrapper for improved layout && functionality

--- frontend/src/components/note/NoteCard.tsx
+++ frontend/src/components/note/NoteCard.tsx
@@ -1,82 +1,79 @@
 "use client";
 
-import type { INote } from "@/types/note.types";
+import type { Note } from "@/types/note.types";
 
 import { useLayout } from "@/context/useLayout";
 import { useModal } from "@/context/useModal";
 import { useNote } from "@/context/useNote";
 
-import { Card } from "@/components/ui/card";
-
 import CardContent from "@/components/global/CardContent";
-import CardFooterComponent from "@/components/global/CardFooterComponent";
+import CardFooterWrapper from "@/components/global/CardFooterWrapper";
 import CardImage from "@/components/global/CardImage";
 import CardSettingsButton from "@/components/global/CardSettingsButton";
+import CardWrapper from "@/components/global/CardWrapper";
+
+import EditNoteButton from "@/components/note/EditNoteButton";
+import DeleteNoteButton from "@/components/note/DeleteNoteButton";
 
 type NoteCardProps = {
-  note: INote;
+	note: Note;
 };
 
 function NoteCard({ note }: NoteCardProps) {
-  const { deleteNote, isLoading } = useNote();
-  const { isGridLayout } = useLayout();
-  const { openModal } = useModal();
+	const { deleteNote, isLoading } = useNote();
+	const { isGridLayout } = useLayout();
+	const { openModal } = useModal();
 
-  const handleDeleteClick = () => {
-    openModal({
-      title: "Confirm Deletion",
-      description:
-        "Are you sure you want to delete this note? This action cannot be undone.",
-      confirmText: "Delete",
-      cancelText: "Cancel",
-      action: "delete",
-      onConfirm: () => deleteNote(note.id),
-      noteId: note.id,
-    });
-  };
+	const handleDeleteClick = () => {
+		openModal({
+			title: "Confirm Deletion",
+			description:
+				"Are you sure you want to delete this note? This action cannot be undone.",
+			confirmText: "Delete",
+			cancelText: "Cancel",
+			action: "delete",
+			onConfirm: () => deleteNote(note.id),
+			noteId: note.id,
+		});
+	};
 
-  return (
-    <Card
-      className={`overflow-hidden bg-white shadow-md transition-all duration-300 hover:shadow-lg ${
-        isGridLayout ? "hover:scale-105" : ""
-      }`}
-    >
-      <div className={`${isGridLayout ? "" : "flex"}`}>
-        <CardImage
-          src={note.thumbnail ?? "/images/placeholder.jpg"}
-          alt={note.title}
-          isGridLayout={isGridLayout}
-        />
-        <div
-          className={`flex-grow ${
-            isGridLayout ? "" : "flex flex-col justify-between"
-          }`}
-        >
-          <div className="flex justify-end p-2">
-            <CardSettingsButton
-              noteId={note.id}
-              onDelete={handleDeleteClick}
-              onExport={() => {
-                console.log("Export as PDF");
-              }}
-            />
-          </div>
-          <CardContent
-            cardTitle={note.videoTitle ?? ""}
-            cardDescription={`Note Title: ${note.title}`}
-            noteId={note.id}
-            isGridLayout={isGridLayout}
-          />
-          <CardFooterComponent
-            noteId={note.id}
-            onDelete={handleDeleteClick}
-            isGridLayout={isGridLayout}
-            isLoading={isLoading}
-          />
-        </div>
-      </div>
-    </Card>
-  );
+	return (
+		<CardWrapper>
+			<CardImage
+				src={note.thumbnail}
+				alt={note.title}
+				isGridLayout={isGridLayout}
+			/>
+			<div
+				className={`flex-grow ${
+					isGridLayout ? "" : "flex flex-col justify-between"
+				}`}
+			>
+				<div className="flex justify-end p-2">
+					<CardSettingsButton
+						noteId={note.id}
+						onDelete={handleDeleteClick}
+						onExport={() => {
+							console.log("Export as PDF");
+						}}
+					/>
+				</div>
+				<CardContent
+					cardTitle={note.videoTitle}
+					cardDescription={`Note Title: ${note.title}`}
+					href={`/notes/${note.id}`}
+					isGridLayout={isGridLayout}
+				/>
+				<CardFooterWrapper>
+					<EditNoteButton noteId={note.id} isLoading={isLoading} />
+					<DeleteNoteButton
+						isLoading={isLoading}
+						onDelete={handleDeleteClick}
+					/>
+				</CardFooterWrapper>
+			</div>
+		</CardWrapper>
+	);
 }
 
 export default NoteCard;
