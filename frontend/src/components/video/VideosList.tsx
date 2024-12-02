451c2123b8bc24db5e1dac54304f831e824297d7
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-12-02 16:23:09 +0100

    feat(video): add VideosList component for displaying a list of videos

---
+++ frontend/src/components/video/VideosList.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import type { Video } from "@/types/video.types";
+
+import { useLayout } from "@/context/useLayout";
+import VideoCard from "./VideoCard";
+
+type VideosListProps = {
+	videos: Video[];
+};
+
+function VideosList({ videos }: VideosListProps) {
+	const { isGridLayout } = useLayout();
+
+	console.log(videos);
+
+	return (
+		<div className="md:px-4 py-6">
+			<div
+				className={`${isGridLayout ? "grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4" : "space-y-4"}`}
+			>
+				{videos.map((video) => (
+					<VideoCard key={video.id} video={video} />
+				))}
+			</div>
+		</div>
+	);
+}
+
+export default VideosList;
