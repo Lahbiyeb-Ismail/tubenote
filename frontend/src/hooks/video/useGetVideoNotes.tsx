b314ff7507426afadf681163612c8534de6e7f9c
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-12-03 14:06:09 +0100

    feat(hooks): add custom hook useGetVideoNotes for fetching video notes with pagination

---
+++ frontend/src/hooks/video/useGetVideoNotes.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+
+import { getUserVideos, getVideoNotes } from "@/actions/video.actions";
+import { getStorageValue } from "@/utils/localStorage";
+
+import { DEFAULT_PAGE, PAGE_LIMIT } from "@/utils/constants";
+
+function useGetVideoNotes({
+	videoId,
+	page = DEFAULT_PAGE,
+	limit = PAGE_LIMIT,
+}: { videoId: string; page?: number; limit?: number }) {
+	const accessToken = getStorageValue<string>("accessToken");
+
+	return useQuery({
+		queryKey: ["videos", page, limit],
+		queryFn: () => getVideoNotes({ videoId, page, limit }),
+		enabled: !!accessToken,
+	});
+}
+
+export default useGetVideoNotes;
