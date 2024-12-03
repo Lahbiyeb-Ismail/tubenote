7d55c643e2a66a32aeaa2396745828b6c766f52e
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-12-03 14:05:36 +0100

    feat(actions): add getVideoNotes function for fetching notes with pagination support

--- frontend/src/actions/video.actions.ts
+++ frontend/src/actions/video.actions.ts
@@ -3,6 +3,13 @@ import axiosInstance from "@/lib/axios.lib";
 import extractVideoId from "@/helpers/extractVideoId";
 import type { Video } from "@/types/video.types";
 import type { Pagination } from "@/types";
+import type { Note } from "@/types/note.types";
+
+type VideoIdParam = { videoId: string };
+type LimitParam = { limit: number };
+type PageParam = { page: number };
+
+type GetVideoNotesProps = VideoIdParam & PageParam & LimitParam;
 
 /**
  * Saves video data by extracting the video ID from the provided URL
@@ -50,6 +57,36 @@ export async function getUserVideos({
  */
 export async function getVideoById(videoId: string): Promise<Video> {
 	const response = await axiosInstance.get(`/videos/${videoId}`);
+
+	return response.data;
+}
+
+/**
+ * Fetches the notes for a specific video.
+ *
+ * @param {Object} params - The parameters for fetching video notes.
+ * @param {string} params.videoId - The ID of the video.
+ * @param {number} params.page - The page number for pagination.
+ * @param {number} params.limit - The number of notes per page.
+ * @returns {Promise<Object>} A promise that resolves to an object containing the video, notes, and pagination information.
+ * @returns {Promise<Video>} video - The video object.
+ * @returns {Promise<Note[]>} notes - An array of notes.
+ * @returns {Promise<Pagination>} pagination - The pagination information.
+ */
+export async function getVideoNotes({
+	videoId,
+	page,
+	limit,
+}: GetVideoNotesProps): Promise<{
+	video: Video;
+	notes: Note[];
+	pagination: Pagination;
+}> {
+	const response = await axiosInstance.get<{
+		video: Video;
+		notes: Note[];
+		pagination: Pagination;
+	}>(`/videos/${videoId}/notes?page=${page}&limit=${limit}`);
 
 	return response.data;
 }
