8fb1a14b28ffb72971d6b4f4ac4bfce1ffec56b6
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-12-04 16:18:59 +0100

    feat(AddNoteForm): implement video ID extraction and navigation on note submission

--- frontend/src/components/dashboards/AddNoteForm.tsx
+++ frontend/src/components/dashboards/AddNoteForm.tsx
@@ -10,68 +10,73 @@ import { useNote } from "@/context/useNote";
 import { useVideo } from "@/context/useVideo";
 import { videoFormSchema } from "@/lib/schemas";
 import type { VideoUrl } from "@/types/note.types";
+import { useRouter } from "next/navigation";
+import extractVideoId from "@/helpers/extractVideoId";
 
 function AddNoteForm() {
-  const form = useForm<VideoUrl>({
-    resolver: zodResolver(videoFormSchema),
-    defaultValues: {
-      videoUrl: "",
-    },
-  });
+	const router = useRouter();
 
-  const { isLoading, saveVideo } = useVideo();
-  const { clearNoteState } = useNote();
+	const form = useForm<VideoUrl>({
+		resolver: zodResolver(videoFormSchema),
+		defaultValues: {
+			videoUrl: "",
+		},
+	});
 
-  const handleAddNote = async (formData: VideoUrl) => {
-    clearNoteState();
-    saveVideo(formData.videoUrl);
-  };
+	// const { isLoading, saveVideo } = useVideo();
+	// const { clearNoteState } = useNote();
 
-  return (
-    <Form {...form}>
-      <form
-        className="w-full max-w-md"
-        onSubmit={form.handleSubmit(handleAddNote)}
-      >
-        <div
-          className={`flex items-center justify-between border-b border-[#171215] py-2 ${
-            form.formState.errors.videoUrl
-              ? "border-red-500"
-              : "border-[#171215]"
-          }`}
-        >
-          <FormField
-            name="videoUrl"
-            render={({ field }) => (
-              <FormItem className="flex-grow mr-2">
-                <FormControl>
-                  <Input
-                    {...field}
-                    id="videoUrl"
-                    type="text"
-                    placeholder="Enter YouTube video URL"
-                    className={`w-full appearance-none bg-transparent px-2 py-1 leading-tight text-gray-700 focus:outline-none ${
-                      form.formState.errors.videoUrl
-                        ? "border-red-500 focus:border-red-500"
-                        : "border-transparent"
-                    }`}
-                  />
-                </FormControl>
-              </FormItem>
-            )}
-          />
+	const handleAddNote = async (formData: VideoUrl) => {
+		const videoId = extractVideoId(formData.videoUrl);
 
-          <button
-            className="flex-shrink-0 rounded border-4 border-[#171215] bg-[#171215] px-2 py-1 text-sm text-white hover:border-[#2c2326] hover:bg-[#2c2326]"
-            type="submit"
-            disabled={isLoading}
-          >
-            {isLoading ? "Saving Video" : "Take Note"}
-          </button>
-        </div>
-      </form>
-    </Form>
-  );
+		router.push(`/notes/add/${videoId}`);
+	};
+
+	return (
+		<Form {...form}>
+			<form
+				className="w-full max-w-md"
+				onSubmit={form.handleSubmit(handleAddNote)}
+			>
+				<div
+					className={`flex items-center justify-between border-b border-[#171215] py-2 ${
+						form.formState.errors.videoUrl
+							? "border-red-500"
+							: "border-[#171215]"
+					}`}
+				>
+					<FormField
+						name="videoUrl"
+						render={({ field }) => (
+							<FormItem className="flex-grow mr-2">
+								<FormControl>
+									<Input
+										{...field}
+										id="videoUrl"
+										type="text"
+										placeholder="Enter YouTube video URL"
+										className={`w-full appearance-none bg-transparent px-2 py-1 leading-tight text-gray-700 focus:outline-none ${
+											form.formState.errors.videoUrl
+												? "border-red-500 focus:border-red-500"
+												: "border-transparent"
+										}`}
+									/>
+								</FormControl>
+							</FormItem>
+						)}
+					/>
+
+					<button
+						className="flex-shrink-0 rounded border-4 border-[#171215] bg-[#171215] px-2 py-1 text-sm text-white hover:border-[#2c2326] hover:bg-[#2c2326]"
+						type="submit"
+						disabled={form.formState.isSubmitting}
+					>
+						Add New Note
+					</button>
+				</div>
+			</form>
+		</Form>
+	);
 }
 
 export default AddNoteForm;
