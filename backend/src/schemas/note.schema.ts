0cfae17127eb2c0ca1a894cac585c459672f4116
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-12-04 16:50:11 +0100

    refactor(note.schema): standardize formatting and indentation in schema definitions

--- backend/src/schemas/note.schema.ts
+++ backend/src/schemas/note.schema.ts
@@ -1,23 +1,23 @@
-import { z } from 'zod';
+import { z } from "zod";
 
 export const noteSchema = {
-  body: z.object({
-    title: z
-      .string()
-      .min(3, { message: 'Title must be at least 3 characters long.' }),
-    content: z
-      .string()
-      .min(10, { message: 'Content must be at least 10 characters long.' }),
-    videoTitle: z.string(),
-    thumbnail: z.string(),
-    videoId: z.string(),
-    youtubeId: z.string(),
-    timestamp: z.number(),
-  }),
+	body: z.object({
+		title: z
+			.string()
+			.min(3, { message: "Title must be at least 3 characters long." }),
+		content: z
+			.string()
+			.min(10, { message: "Content must be at least 10 characters long." }),
+		videoTitle: z.string(),
+		thumbnail: z.string(),
+		videoId: z.string(),
+		youtubeId: z.string(),
+		timestamp: z.number(),
+	}),
 };
 
 export const noteIdParamSchema = {
-  params: z.object({
-    noteId: z.string(),
-  }),
+	params: z.object({
+		noteId: z.string(),
+	}),
 };
