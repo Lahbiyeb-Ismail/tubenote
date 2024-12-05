65bf2ee9a56380fc1e04d09069226f3c95fbde0e
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-12-05 17:08:33 +0100

    refactor(schemas): simplify reset password schemas by removing unnecessary structure

--- backend/src/schemas/resetPassword.schema.ts
+++ backend/src/schemas/resetPassword.schema.ts
@@ -1,19 +1,13 @@
-import { z } from 'zod';
+import { z } from "zod";
 
-export const forgotPasswordBodySchema = {
-  body: z.object({
-    email: z.string().email({ message: 'Invalid email address.' }),
-  }),
-};
+export const forgotPasswordBodySchema = z.object({
+	email: z.string().email({ message: "Invalid email address." }),
+});
 
-export const passwordResetParamsSchema = {
-  params: z.object({
-    token: z.string().min(1),
-  }),
-};
+export const resetPasswordParamsSchema = z.object({
+	token: z.string().min(1),
+});
 
-export const passwordResetBodySchema = {
-  body: z.object({
-    password: z.string().min(8),
-  }),
-};
+export const resetPasswordBodySchema = z.object({
+	password: z.string().min(8),
+});
