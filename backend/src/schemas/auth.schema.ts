4d0ad26bc8cf2a46d7d97476538cf769bbb42d5c
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-12-05 15:24:49 +0100

    refactor(auth.schema): simplify schema definitions and improve formatting

--- backend/src/schemas/auth.schema.ts
+++ backend/src/schemas/auth.schema.ts
@@ -1,25 +1,21 @@
-import { z } from 'zod';
+import { z } from "zod";
 
-export const registrationSchema = {
-  body: z.object({
-    username: z
-      .string()
-      .min(3, { message: 'Username must be at least 3 characters long.' }),
-    email: z.string().email({ message: 'Invalid email address.' }),
-    password: z
-      .string()
-      .min(8, { message: 'Password must be at least 8 characters long.' }),
-    profilePicture: z.string().url().nullable().optional(),
-    googleId: z.string().nullable().optional(),
-    emailVerified: z.boolean().default(false),
-  }),
-};
+export const registrationSchema = z.object({
+	username: z
+		.string()
+		.min(3, { message: "Username must be at least 3 characters long." }),
+	email: z.string().email({ message: "Invalid email address." }),
+	password: z
+		.string()
+		.min(8, { message: "Password must be at least 8 characters long." }),
+	profilePicture: z.string().url().nullable().optional(),
+	googleId: z.string().nullable().optional(),
+	emailVerified: z.boolean().default(false),
+});
 
-export const loginSchema = {
-  body: z.object({
-    email: z.string().email({ message: 'Invalid email address.' }),
-    password: z
-      .string()
-      .min(8, { message: 'Password must be at least 8 characters long.' }),
-  }),
-};
+export const loginSchema = z.object({
+	email: z.string().email({ message: "Invalid email address." }),
+	password: z
+		.string()
+		.min(8, { message: "Password must be at least 8 characters long." }),
+});
