1389458c48a81301174e080e304b6700e1a65128
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-12-05 17:27:08 +0100

    refactor(routes): update verify email routes to use unified request validation middleware

--- backend/src/routes/verifyEmail.route.ts
+++ backend/src/routes/verifyEmail.route.ts
@@ -1,31 +1,30 @@
-import { Router } from 'express';
+import { Router } from "express";
 
 import {
-  sendVerificationEmailHandler,
-  verifyEmailHandler,
-} from '../controllers/verifyEmail.controller';
+	sendVerificationEmailHandler,
+	verifyEmailHandler,
+} from "../controllers/verifyEmail.controller";
 
-import validateRequestBody from '../middlewares/validateRequestBody';
-import validateRequestParams from '../middlewares/validateRequestParams';
+import validateRequest from "../middlewares/validateRequest";
 
 import {
-  sendVerifyEmailSchema,
-  verifyEmailSchema,
-} from '../schemas/verifyEmail.schema';
+	sendVerifyEmailBodySchema,
+	verifyEmailParamSchema,
+} from "../schemas/verifyEmail.schema";
 
 const router = Router();
 
 // - POST /send-verification-email: Send a verification email to the user (requires request body validation)
 router
-  .route('/send-verification-email')
-  .post(
-    validateRequestBody(sendVerifyEmailSchema),
-    sendVerificationEmailHandler
-  );
+	.route("/send-verification-email")
+	.post(
+		validateRequest({ body: sendVerifyEmailBodySchema }),
+		sendVerificationEmailHandler,
+	);
 
 // - GET /verify-email/:token: Verify the user's email using the provided token (requires request params validation)
 router
-  .route('/verify-email/:token')
-  .get(validateRequestParams(verifyEmailSchema), verifyEmailHandler);
+	.route("/verify-email/:token")
+	.get(validateRequest({ params: verifyEmailParamSchema }), verifyEmailHandler);
 
 export default router;
