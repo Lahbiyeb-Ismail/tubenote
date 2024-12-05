8bda40b50585315a528f39fcd2c64e30fecacdd3
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-12-05 15:30:54 +0100

    refactor(auth): replace validateRequestBody middleware with validateRequest for improved validation

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -1,40 +1,44 @@
-import { Router } from 'express';
-import passport from '../lib/passportAuth';
+import { Router } from "express";
+import passport from "../lib/passportAuth";
 
 import {
-  handleRegister,
-  handleLogin,
-  handleLogout,
-  handleGoogleLogin,
-  handleRefreshToken,
-} from '../controllers/auth.controller';
-import validateRequestBody from '../middlewares/validateRequestBody';
-import { registrationSchema, loginSchema } from '../schemas/auth.schema';
+	handleRegister,
+	handleLogin,
+	handleLogout,
+	handleGoogleLogin,
+	handleRefreshToken,
+} from "../controllers/auth.controller";
+
+import { registrationSchema, loginSchema } from "../schemas/auth.schema";
+
+import validateRequest from "../middlewares/validateRequest";
 
 const router = Router();
 
 // - POST /register: Register a new user (requires request body validation).
 router
-  .route('/register')
-  .post(validateRequestBody(registrationSchema), handleRegister);
+	.route("/register")
+	.post(validateRequest({ body: registrationSchema }), handleRegister);
 
 // - POST /login: Authenticate a user (requires request body validation).
-router.route('/login').post(validateRequestBody(loginSchema), handleLogin);
+router
+	.route("/login")
+	.post(validateRequest({ body: loginSchema }), handleLogin);
 
 // - POST /logout: Log out the current user.
-router.route('/logout').post(handleLogout);
+router.route("/logout").post(handleLogout);
 
 // - POST /refresh: Refresh the user's access token.
-router.route('/refresh').post(handleRefreshToken);
+router.route("/refresh").post(handleRefreshToken);
 
 // - GET /google: Initiate Google OAuth authentication.
 router
-  .route('/google')
-  .get(passport.authenticate('google', { scope: ['profile', 'email'] }));
+	.route("/google")
+	.get(passport.authenticate("google", { scope: ["profile", "email"] }));
 
 // - GET /google/callback: Handle the Google OAuth callback.
 router
-  .route('/google/callback')
-  .get(passport.authenticate('google', { session: false }), handleGoogleLogin);
+	.route("/google/callback")
+	.get(passport.authenticate("google", { session: false }), handleGoogleLogin);
 
 export default router;
