f1cfcb58ab75f707aab6e74ebc4e497d38be5251
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-12-06 22:03:40 +0100

    feat(asyncOperation): implement InternalServerError for improved error handling in async operations

--- backend/src/utils/handleAsyncOperation.ts
+++ backend/src/utils/handleAsyncOperation.ts
@@ -1,5 +1,5 @@
-import { Prisma } from '@prisma/client';
-import { BaseError } from '../errors/BaseError';
+import { Prisma } from "@prisma/client";
+import { InternalServerError } from "../errors";
 
 /**
  * Represents an asynchronous operation that returns a promise of type T.
@@ -9,7 +9,7 @@ import { BaseError } from '../errors/BaseError';
 type AsyncOperation<T> = () => Promise<T>;
 
 interface ErrorHandlerOptions {
-  errorMessage: string;
+	errorMessage: string;
 }
 
 /**
@@ -26,75 +26,60 @@ interface ErrorHandlerOptions {
  *   - `Unknown Error` for unknown errors.
  */
 async function handleAsyncOperation<T>(
-  operation: AsyncOperation<T>,
-  options: ErrorHandlerOptions
+	operation: AsyncOperation<T>,
+	options: ErrorHandlerOptions,
 ): Promise<T> {
-  const { errorMessage } = options;
+	const { errorMessage } = options;
 
-  try {
-    return await operation();
-  } catch (error) {
-    if (
-      error instanceof Prisma.PrismaClientKnownRequestError ||
-      error instanceof Prisma.PrismaClientValidationError ||
-      error instanceof Prisma.PrismaClientUnknownRequestError
-    ) {
-      // Log the detailed error for debugging
-      console.error('Prisma error details:', {
-        errorType: error.constructor.name,
-        errorCode: 'code' in error ? error.code : undefined,
-        errorMessage: error.message,
-        errorName: error.name,
-      });
+	try {
+		return await operation();
+	} catch (error) {
+		if (
+			error instanceof Prisma.PrismaClientKnownRequestError ||
+			error instanceof Prisma.PrismaClientValidationError ||
+			error instanceof Prisma.PrismaClientUnknownRequestError
+		) {
+			// Log the detailed error for debugging
+			console.error("Prisma error details:", {
+				errorType: error.constructor.name,
+				errorCode: "code" in error ? error.code : undefined,
+				errorMessage: error.message,
+				errorName: error.name,
+			});
 
-      // Throw a generic error for the client
-      throw new BaseError(
-        'Database Error',
-        500,
-        `${errorMessage}: A database error occurred.`,
-        true
-      );
-    }
+			throw new InternalServerError(
+				`${errorMessage}: A database error occurred.`,
+			);
+		}
 
-    if (
-      error instanceof Prisma.PrismaClientInitializationError ||
-      error instanceof Prisma.PrismaClientRustPanicError
-    ) {
-      // Log the critical error
-      console.error('Critical Prisma error:', error);
+		if (
+			error instanceof Prisma.PrismaClientInitializationError ||
+			error instanceof Prisma.PrismaClientRustPanicError
+		) {
+			// Log the critical error
+			console.error("Critical Prisma error:", error);
 
-      // Throw a generic error for the client
-      throw new BaseError(
-        'Critical Database Error',
-        500,
-        `${errorMessage}: A critical database error occurred.`,
-        false
-      );
-    }
+			throw new InternalServerError(
+				`${errorMessage}: A critical database error occurred.`,
+			);
+		}
 
-    if (error instanceof Error) {
-      // Log the unexpected error
-      console.error('Unexpected error:', error);
+		if (error instanceof Error) {
+			// Log the unexpected error
+			console.error("Unexpected error:", error);
 
-      // Throw a generic error for the client
-      throw new BaseError(
-        'Unexpected Error',
-        500,
-        `${errorMessage}: An unexpected error occurred.`,
-        false
-      );
-    }
-    // Log the unknown error
-    console.error('Unknown error:', error);
+			// Throw a generic error for the client
+			throw new InternalServerError(
+				`${errorMessage}: An unexpected error occurred.`,
+			);
+		}
+		// Log the unknown error
+		console.error("Unknown error:", error);
 
-    // Throw a generic error for the client
-    throw new BaseError(
-      'Unknown Error',
-      500,
-      `${errorMessage}: An unknown error occurred.`,
-      false
-    );
-  }
+		throw new InternalServerError(
+			`${errorMessage}: An unknown error occurred.`,
+		);
+	}
 }
 
 export default handleAsyncOperation;
