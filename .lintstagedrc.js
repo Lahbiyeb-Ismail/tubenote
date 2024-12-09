6b0d61da4afc0b0eec23b988f09c1ac60b75bb23
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-12-09 21:01:46 +0100

    feat: add lint-staged configuration for ESLint and Prettier with Biome integration

---
+++ .lintstagedrc.js
@@ -0,0 +1,17 @@
+const path = require("path");
+
+const buildBiomeCommand = (command) => (filenames) => {
+  const files = filenames
+    .filter((file) => !file.includes("/dist/") && !file.includes("\\dist\\"))
+    .map((f) => path.relative(process.cwd(), f))
+    .join(" ");
+  return files ? `biome ${command} ${files}` : "true";
+};
+
+const buildEslintCommand = buildBiomeCommand("check --apply-unsafe");
+const buildPrettierCommand = buildBiomeCommand("format --write");
+
+module.exports = {
+  "*.{js,jsx,ts,tsx}": [buildEslintCommand, buildPrettierCommand],
+  "*.{json,md}": [buildPrettierCommand],
+};
