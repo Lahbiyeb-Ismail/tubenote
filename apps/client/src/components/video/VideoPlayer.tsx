9abc799241f5a887219a3a4687f231d54792c4be
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-12-16 16:30:51 +0100

    refactor(video): integrate timestamp handling in VideoPlayer component

--- apps/client/src/components/video/VideoPlayer.tsx
+++ apps/client/src/components/video/VideoPlayer.tsx
@@ -1,49 +1,78 @@
 "use client";
 
-import { useNote } from "@/context/useNote";
-import { useVideo } from "@/context/useVideo";
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
 import YouTube, { type YouTubeProps } from "react-youtube";
 
+import { useVideo } from "@/context/useVideo";
+import type { Timestamp } from "@/types/note.types";
+
 type VideoPlayerProps = {
-  videoId?: string;
+  videoId: string;
+  timestamp?: Timestamp;
 };
 
-function VideoPlayer({ videoId }: VideoPlayerProps) {
+function VideoPlayer({ videoId, timestamp }: VideoPlayerProps) {
   const playerRef = useRef<any | null>(null);
-  const { setVideoCurrentTime } = useVideo();
-  const {
-    state: { note },
-  } = useNote();
+
+  const { setNoteTimestamp, noteTimestamp } = useVideo();
 
   const onPlayerReady: YouTubeProps["onReady"] = (event) => {
     playerRef.current = event.target;
 
-    if (playerRef.current && note) {
-      playerRef.current.seekTo(note.timestamp);
+    if (playerRef.current && timestamp) {
+      syncVideoWithNote(timestamp.start);
+      playerRef.current.playVideo();
     }
   };
 
-  const onStateChange: YouTubeProps["onStateChange"] = (event) => {
-    // Check if the player is playing (state 1)
-    if (event.data === 1) {
-      const time = playerRef.current?.getCurrentTime() || 0;
-      setVideoCurrentTime(time);
-    }
+  const onPlay: YouTubeProps["onPlay"] = () => {
+    setNoteTimestamp({
+      ...noteTimestamp,
+      start:
+        noteTimestamp.end - noteTimestamp.start < 0
+          ? 0
+          : Math.floor(noteTimestamp.end - noteTimestamp.start),
+    });
   };
 
   const onPause: YouTubeProps["onPause"] = () => {
-    const time = playerRef.current?.getCurrentTime() || 0;
-    setVideoCurrentTime(time);
+    setNoteTimestamp({
+      ...noteTimestamp,
+      end: Math.floor(playerRef.current?.getCurrentTime()) || 0,
+    });
   };
+
+  const syncVideoWithNote = (seconds: number) => {
+    if (playerRef.current) {
+      playerRef.current.seekTo(seconds);
+    }
+  };
+
+  // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
+  useEffect(() => {
+    if (timestamp) {
+      syncVideoWithNote(timestamp.start);
+    }
+  }, [timestamp]);
+
+  useEffect(() => {
+    const checkTime = () => {
+      if (playerRef.current && timestamp) {
+        const currentTime = playerRef.current.getCurrentTime();
+        if (currentTime >= timestamp.end) {
+          playerRef.current.pauseVideo();
+        }
+      }
+    };
+
+    const interval = setInterval(checkTime, 1000); // Check every second
+
+    return () => clearInterval(interval);
+  }, [timestamp]);
 
   const opts = {
     height: "100%",
     width: "100%",
-    playerVars: {
-      // https://developers.google.com/youtube/player_parameters
-      autoplay: 1,
-    },
   };
 
   return (
@@ -53,8 +82,8 @@ function VideoPlayer({ videoId }: VideoPlayerProps) {
         style={{ height: "100%" }}
         opts={opts}
         onReady={onPlayerReady}
-        onStateChange={onStateChange}
         onPause={onPause}
+        onPlay={onPlay}
       />
     </div>
   );
