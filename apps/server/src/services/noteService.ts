22a21d9311f2515e14aa0807da85d1741d603176
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-12-17 17:26:21 +0100

    feat(noteService): add updateNote method for updating existing notes

--- apps/server/src/services/noteService.ts
+++ apps/server/src/services/noteService.ts
@@ -1,12 +1,29 @@
 import type { Note, Prisma } from "@prisma/client";
 
-import noteDatabase from "../databases/noteDatabase";
+import noteDatabase, { type IUpdateNote } from "../databases/noteDatabase";
+import { NotFoundError } from "../errors";
 
 class NoteService {
   async addNewNote(noteData: Prisma.NoteCreateInput): Promise<Note> {
     const note = await noteDatabase.create(noteData);
 
     return note;
+  }
+
+  async updateNote({ noteId, userId, data }: IUpdateNote): Promise<Note> {
+    const note = await noteDatabase.find({ noteId, userId });
+
+    if (!note) {
+      throw new NotFoundError("Note not found.");
+    }
+
+    const updatedNote = await noteDatabase.update({
+      noteId,
+      userId,
+      data,
+    });
+
+    return updatedNote;
   }
 }
 
