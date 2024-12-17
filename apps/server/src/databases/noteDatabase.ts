57d35144014cd11753eadeabf54c9541b16873b9
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-12-17 17:25:40 +0100

    feat(noteDatabase): add find and update methods for note management

--- apps/server/src/databases/noteDatabase.ts
+++ apps/server/src/databases/noteDatabase.ts
@@ -3,7 +3,31 @@ import type { Note, Prisma } from "@prisma/client";
 import prismaClient from "../lib/prisma";
 import handleAsyncOperation from "../utils/handleAsyncOperation";
 
+interface IUserId {
+  userId: string;
+}
+interface INoteId {
+  noteId: string;
+}
+
+export interface IUpdateNote extends IUserId, INoteId {
+  data: Prisma.NoteUpdateInput;
+}
+
 class NoteDatabase {
+  async find({ noteId, userId }: IUserId & INoteId): Promise<Note | null> {
+    return handleAsyncOperation(
+      () =>
+        prismaClient.note.findFirst({
+          where: {
+            id: noteId,
+            userId,
+          },
+        }),
+      { errorMessage: "Failed to find note." }
+    );
+  }
+
   async create(noteData: Prisma.NoteCreateInput): Promise<Note> {
     return handleAsyncOperation(
       () =>
@@ -13,6 +37,20 @@ class NoteDatabase {
           },
         }),
       { errorMessage: "Failed to create note." }
+    );
+  }
+
+  async update({ userId, noteId, data }: IUpdateNote): Promise<Note> {
+    return handleAsyncOperation(
+      () =>
+        prismaClient.note.update({
+          where: {
+            id: noteId,
+            userId,
+          },
+          data,
+        }),
+      { errorMessage: "Failed to update note." }
     );
   }
 }
