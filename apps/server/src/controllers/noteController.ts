a97bfd0d2f864a08b7609105d63d713a0b4d1cf0
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-12-17 17:27:06 +0100

    feat(noteController): add updateNote method for updating existing notes

--- apps/server/src/controllers/noteController.ts
+++ apps/server/src/controllers/noteController.ts
@@ -2,7 +2,7 @@ import type { Response } from "express";
 import httpStatus from "http-status";
 
 import type { TypedRequest } from "../types";
-import type { NoteBody } from "../types/note.type";
+import type { NoteBody, NoteIdParam } from "../types/note.type";
 
 import noteService from "../services/noteService";
 
@@ -20,6 +20,25 @@ class NoteController {
     res
       .status(httpStatus.CREATED)
       .json({ message: "Note created successfully.", note });
+  }
+
+  async updateNote(
+    req: TypedRequest<NoteBody, NoteIdParam>,
+    res: Response
+  ): Promise<void> {
+    const userId = req.userId;
+    const { noteId } = req.params;
+    const noteData = req.body;
+
+    const updatedNote = await noteService.updateNote({
+      userId,
+      noteId,
+      data: noteData,
+    });
+
+    res
+      .status(httpStatus.OK)
+      .json({ message: "Note updated successfully.", note: updatedNote });
   }
 }
 
