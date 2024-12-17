f49210e08fb56c2c61283dd697441af250efbd29
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-12-17 15:07:35 +0100

    feat(authController): add login method to handle user authentication and token generation

--- apps/server/src/controllers/authController.ts
+++ apps/server/src/controllers/authController.ts
@@ -1,8 +1,10 @@
 import type { Response } from "express";
 import httpStatus from "http-status";
+import { refreshTokenCookieConfig } from "../config/cookie.config";
+import { REFRESH_TOKEN_NAME } from "../constants/auth";
 import authService from "../services/authService";
 import type { TypedRequest } from "../types";
-import type { RegisterCredentials } from "../types/auth.type";
+import type { LoginCredentials, RegisterCredentials } from "../types/auth.type";
 
 class AuthController {
   async register(req: TypedRequest<RegisterCredentials>, res: Response) {
@@ -13,6 +15,22 @@ class AuthController {
     res.status(httpStatus.CREATED).json({
       message: "A verification email has been sent to your email.",
       email: user.email,
+    });
+  }
+
+  async login(req: TypedRequest<LoginCredentials>, res: Response) {
+    const { email, password } = req.body;
+
+    const { accessToken, refreshToken } = await authService.loginUser(
+      email,
+      password
+    );
+
+    res.cookie(REFRESH_TOKEN_NAME, refreshToken, refreshTokenCookieConfig);
+
+    res.status(httpStatus.OK).json({
+      message: "Login successful",
+      accessToken,
     });
   }
 }
