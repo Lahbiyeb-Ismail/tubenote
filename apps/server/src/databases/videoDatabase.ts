10c8b8dab1c00829544c1ac35059a80c73d5e66a
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-12-18 16:53:03 +0100

    feat(videoDatabase): add methods for finding, creating, and connecting videos to users

--- apps/server/src/databases/videoDatabase.ts
+++ apps/server/src/databases/videoDatabase.ts
@@ -1,4 +1,4 @@
-import type { Video } from "@prisma/client";
+import type { Prisma, Video } from "@prisma/client";
 import prismaClient from "../lib/prisma";
 import handleAsyncOperation from "../utils/handleAsyncOperation";
 
@@ -11,6 +11,27 @@ export interface IFindMany {
 export interface IVideo extends Omit<Video, "userIds"> {}
 
 class VideoDatabase {
+  async find(params: Prisma.VideoWhereInput): Promise<IVideo | null> {
+    return handleAsyncOperation(
+      () =>
+        prismaClient.video.findFirst({
+          where: { ...params },
+          omit: { userIds: true },
+        }),
+      { errorMessage: "Faild to find video" }
+    );
+  }
+
+  async findUnique(videoId: string): Promise<Video | null> {
+    return handleAsyncOperation(
+      () =>
+        prismaClient.video.findUnique({
+          where: { youtubeId: videoId },
+        }),
+      { errorMessage: "Faild to find video" }
+    );
+  }
+
   async findMany({ userId, limit, skip }: IFindMany): Promise<IVideo[]> {
     return handleAsyncOperation(
       async () => {
@@ -36,6 +57,51 @@ class VideoDatabase {
           },
         }),
       { errorMessage: "Failed to count user videos." }
+    );
+  }
+
+  async create(videoData: any, userId: string): Promise<Video> {
+    return handleAsyncOperation(
+      async () => {
+        const { snippet, statistics, player } = videoData;
+        return await prismaClient.video.create({
+          data: {
+            youtubeId: videoData.id,
+            userIds: [userId],
+            snippet: {
+              title: snippet.title,
+              categoryId: snippet.categoryId,
+              channelId: snippet.channelId,
+              channelTitle: snippet.channelTitle,
+              description: snippet.description,
+              liveBroadcastContent: snippet.liveBroadcastContent,
+              publishedAt: snippet.publishedAt,
+              tags: snippet.tags || [],
+              thumbnails: snippet.thumbnails,
+            },
+            statistics,
+            player,
+          },
+        });
+      },
+      { errorMessage: "Failed to create video" }
+    );
+  }
+
+  async connectVideoToUser(videoId: string, userId: string): Promise<Video> {
+    return handleAsyncOperation(
+      () =>
+        prismaClient.video.update({
+          where: { id: videoId },
+          data: {
+            userIds: {
+              push: userId,
+            },
+          },
+        }),
+      {
+        errorMessage: "Failed to update video",
+      }
     );
   }
 }
