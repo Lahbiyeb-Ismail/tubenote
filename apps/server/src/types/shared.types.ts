8c6b269a7535ca06fce1d74b9248897b7bc88742
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-12-19 16:20:02 +0100

    feat(types): add shared types for user ID, video ID, note ID, and pagination

---
+++ apps/server/src/types/shared.types.ts
@@ -0,0 +1,15 @@
+export interface IUserId {
+  userId: string;
+}
+
+export interface IVideoId {
+  videoId: string;
+}
+export interface INoteId {
+  noteId: string;
+}
+
+export interface IPagination {
+  limit: number;
+  skip?: number;
+}
