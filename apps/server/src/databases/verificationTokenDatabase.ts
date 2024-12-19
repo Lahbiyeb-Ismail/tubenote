2ef560d69995ccaf2eeec8017cd2c39599dde82f
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-12-19 17:58:42 +0100

    feat(verificationTokenDatabase): add method to find email verification tokens

--- apps/server/src/databases/verificationTokenDatabase.ts
+++ apps/server/src/databases/verificationTokenDatabase.ts
@@ -1,8 +1,23 @@
 import { randomUUID } from "node:crypto";
+import type { EmailVerificationToken, Prisma } from "@prisma/client";
 import prismaClient from "../lib/prisma";
 import handleAsyncOperation from "../utils/handleAsyncOperation";
 
+export interface IFindToken {
+  where: Prisma.EmailVerificationTokenWhereInput;
+}
+
 class VerificationTokenDatabase {
+  async find({ where }: IFindToken): Promise<EmailVerificationToken | null> {
+    return handleAsyncOperation(
+      () =>
+        prismaClient.emailVerificationToken.findFirst({
+          where,
+        }),
+      { errorMessage: "Failed to get email verification token." }
+    );
+  }
+
   async create(userId: string) {
     const token = randomUUID();
     const expiresAt = new Date(Date.now() + 3600000); // Token expires in 1 hour
