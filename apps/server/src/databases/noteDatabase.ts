7b3cece44560c7599fdbd01220f0faaca48053aa
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-12-19 16:21:24 +0100

    refactor(noteDatabase): simplify note operations by using unified filter interfaces

--- apps/server/src/databases/noteDatabase.ts
+++ apps/server/src/databases/noteDatabase.ts
@@ -1,102 +1,69 @@
-import type { Note, Prisma } from "@prisma/client";
+import type { Note } from "@prisma/client";
 
 import prismaClient from "../lib/prisma";
+
 import handleAsyncOperation from "../utils/handleAsyncOperation";
 
-interface IUserId {
-  userId: string;
-}
-interface INoteId {
-  noteId: string;
-}
-
-interface Pagination {
-  limit: number;
-  skip?: number;
-}
-
-export interface OrderByParam {
-  orderBy?:
-    | Prisma.NoteOrderByWithRelationInput
-    | Prisma.NoteOrderByWithRelationInput[];
-}
-export interface IFindNote extends IUserId, INoteId {}
-
-export interface IUpdateNote extends IUserId, INoteId {
-  data: Prisma.NoteUpdateInput;
-}
-
-export interface IDeleteNote extends IUserId, INoteId {}
-
-export interface IFindMany extends Pagination, OrderByParam {
-  params: Prisma.NoteWhereInput;
-}
+import type {
+  ICreateNote,
+  IFindNotes,
+  INoteFilter,
+  INoteFilterUnique,
+  IUpdateNote,
+} from "../types/note.type";
 
 class NoteDatabase {
-  async find({ noteId, userId }: IFindNote): Promise<Note | null> {
+  async find({ where }: INoteFilterUnique): Promise<Note | null> {
     return handleAsyncOperation(
       () =>
-        prismaClient.note.findFirst({
-          where: {
-            id: noteId,
-            userId,
-          },
+        prismaClient.note.findUnique({
+          where,
         }),
       { errorMessage: "Failed to find note." }
     );
   }
 
-  async create(noteData: Prisma.NoteCreateInput): Promise<Note> {
+  async create({ data }: ICreateNote): Promise<Note> {
     return handleAsyncOperation(
       () =>
         prismaClient.note.create({
-          data: {
-            ...noteData,
-          },
+          data,
         }),
       { errorMessage: "Failed to create note." }
     );
   }
 
-  async update({ userId, noteId, data }: IUpdateNote): Promise<Note> {
+  async update({ where, data }: IUpdateNote): Promise<Note> {
     return handleAsyncOperation(
       () =>
         prismaClient.note.update({
-          where: {
-            id: noteId,
-            userId,
-          },
+          where,
           data,
         }),
       { errorMessage: "Failed to update note." }
     );
   }
 
-  async delete({ userId, noteId }: IDeleteNote): Promise<void> {
+  async delete({ where }: INoteFilterUnique): Promise<void> {
     handleAsyncOperation(
       () =>
         prismaClient.note.delete({
-          where: {
-            id: noteId,
-            userId,
-          },
+          where,
         }),
       { errorMessage: "Failed to delete note." }
     );
   }
 
   async findMany({
-    params,
+    where,
     limit,
     skip,
     orderBy = { createdAt: "desc" },
-  }: IFindMany): Promise<Note[]> {
+  }: IFindNotes): Promise<Note[]> {
     return handleAsyncOperation(
       () =>
         prismaClient.note.findMany({
-          where: {
-            ...params,
-          },
+          where,
           take: limit,
           skip,
           orderBy,
@@ -105,13 +72,11 @@ class NoteDatabase {
     );
   }
 
-  async count(param: Prisma.NoteWhereInput): Promise<number> {
+  async count({ where }: INoteFilter): Promise<number> {
     return handleAsyncOperation(
       () =>
         prismaClient.note.count({
-          where: {
-            ...param,
-          },
+          where,
         }),
       { errorMessage: "Failed to count notes." }
     );
