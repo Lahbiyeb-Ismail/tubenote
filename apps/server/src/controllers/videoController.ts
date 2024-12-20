f3827b773057561d1e51197b49960c05981356b6
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-12-20 16:15:32 +0100

    docs(videoController): add JSDoc comments for video-related methods

--- apps/server/src/controllers/videoController.ts
+++ apps/server/src/controllers/videoController.ts
@@ -2,11 +2,22 @@ import type { Response } from "express";
 import httpStatus from "http-status";
 
 import type { EmptyRecord, PaginationQuery, TypedRequest } from "../types";
-
-import videoService from "../services/videoService";
 import type { VideoIdParam } from "../types/video.type";
 
+import videoService from "../services/videoService";
+
+/**
+ * Controller for handling video-related operations.
+ */
 class VideoController {
+  /**
+   * Retrieves a paginated list of videos for a specific user.
+   *
+   * @param req - The request object containing user ID and pagination query parameters.
+   * @param res - The response object to send the result.
+   *
+   * @returns A JSON response with the list of videos and pagination details.
+   */
   async getUserVideos(
     req: TypedRequest<EmptyRecord, EmptyRecord, PaginationQuery>,
     res: Response
@@ -33,6 +44,14 @@ class VideoController {
     });
   }
 
+  /**
+   * Retrieves a specific video by its ID for a specific user.
+   *
+   * @param req - The request object containing user ID and video ID parameters.
+   * @param res - The response object to send the result.
+   *
+   * @returns A JSON response with the video details.
+   */
   async getVideoById(
     req: TypedRequest<EmptyRecord, VideoIdParam>,
     res: Response
@@ -45,5 +64,4 @@ class VideoController {
     res.status(httpStatus.OK).json(video);
   }
 }
-
 export default new VideoController();
