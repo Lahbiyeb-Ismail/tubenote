c9b09728c542f4a1acbd840c7e91c1576a998bb7
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-12-21 16:01:46 +0100

    refactor(videoService): move YouTube data fetching logic into VideoService class

--- apps/server/src/services/videoService.ts
+++ apps/server/src/services/videoService.ts
@@ -2,12 +2,26 @@ import type { Video } from "@prisma/client";
 
 import videoDB, { type IFindMany, type IVideo } from "../databases/videoDB";
 
-import { fetchYoutubeVideoData } from "../helpers/video.helper";
+import { YOUTUBE_API_KEY, YOUTUBE_API_URL } from "../constants";
 
 import { ERROR_MESSAGES } from "../constants/errorMessages";
-import { NotFoundError } from "../errors";
+import { BadRequestError, NotFoundError } from "../errors";
 
 class VideoService {
+  private async fetchYoutubeVideoData(videoId: string): Promise<any> {
+    const response = await fetch(
+      `${YOUTUBE_API_URL}/videos?id=${videoId}&key=${YOUTUBE_API_KEY}&part=snippet,statistics,player`
+    );
+
+    if (!response.ok) {
+      throw new BadRequestError(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    return data.items;
+  }
+
   async fetchUserVideos({ userId, limit, skip }: IFindMany): Promise<{
     videos: IVideo[];
     videosCount: number;
@@ -34,7 +48,7 @@ class VideoService {
     }
 
     if (!video) {
-      const youtubeVideoData = await fetchYoutubeVideoData(videoId);
+      const youtubeVideoData = await this.fetchYoutubeVideoData(videoId);
 
       if (!youtubeVideoData.length) {
         throw new NotFoundError(ERROR_MESSAGES.RESOURCE_NOT_FOUND);
