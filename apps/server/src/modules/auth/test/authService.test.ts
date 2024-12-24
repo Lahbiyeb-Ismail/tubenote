13d65a9774fdfd2ebaf1125f69438505f97b7666
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-12-24 15:39:52 +0100

    test(authService): add login service tests

--- apps/server/src/modules/auth/test/authService.test.ts
+++ apps/server/src/modules/auth/test/authService.test.ts
@@ -1,7 +1,13 @@
 import bcrypt from "bcryptjs";
 
 import { ERROR_MESSAGES } from "../../../constants/errorMessages";
-import { ConflictError } from "../../../errors";
+import {
+  ConflictError,
+  ForbiddenError,
+  NotFoundError,
+  UnauthorizedError,
+} from "../../../errors";
+import RefreshTokenService from "../../refreshToken/refreshTokenService";
 import UserDB from "../../user/userDB";
 import EmailVerificationService from "../../verifyEmailToken/verifyEmailService";
 import AuthService from "../authService";
@@ -9,6 +15,7 @@ import AuthService from "../authService";
 jest.mock("bcryptjs");
 jest.mock("../../user/userDB");
 jest.mock("../../verifyEmailToken/verifyEmailService");
+jest.mock("../../refreshToken/refreshTokenService");
 
 describe("Test AuthService methods", () => {
   describe("Register service tests", () => {
@@ -73,6 +80,98 @@ describe("Test AuthService methods", () => {
           password: "password123",
         })
       ).rejects.toThrow(new ConflictError(ERROR_MESSAGES.EMAIL_ALREADY_EXISTS));
+    });
+  });
+
+  describe("Login service tests", () => {
+    const mockUser = {
+      id: "1",
+      email: "test@example.com",
+      password: "hashedPassword",
+      isEmailVerified: true,
+    };
+
+    const mockLoginParams = {
+      email: "test@example.com",
+      password: "password123",
+    };
+
+    const mockTokens = {
+      accessToken: "mockAccessToken",
+      refreshToken: "mockRefreshToken",
+    };
+
+    beforeEach(() => {
+      jest.clearAllMocks();
+      jest.spyOn(AuthService, "comparePasswords").mockResolvedValue(true);
+      jest.spyOn(AuthService, "createJwtTokens").mockReturnValue(mockTokens);
+    });
+
+    it("should successfully login a user", async () => {
+      (UserDB.findByEmail as jest.Mock).mockResolvedValue(mockUser);
+      (RefreshTokenService.createToken as jest.Mock).mockResolvedValue(
+        undefined
+      );
+
+      const result = await AuthService.loginUser(mockLoginParams);
+
+      expect(result).toEqual(mockTokens);
+      expect(UserDB.findByEmail).toHaveBeenCalledWith(mockLoginParams.email);
+      expect(AuthService.comparePasswords).toHaveBeenCalledWith({
+        rawPassword: mockLoginParams.password,
+        hashedPassword: mockUser.password,
+      });
+      expect(AuthService.createJwtTokens).toHaveBeenCalledWith(mockUser.id);
+      expect(RefreshTokenService.createToken).toHaveBeenCalledWith(
+        mockUser.id,
+        mockTokens.refreshToken
+      );
+    });
+
+    it("should throw a NotFoundError if the user doesn't exist", async () => {
+      (UserDB.findByEmail as jest.Mock).mockResolvedValue(null);
+
+      await expect(AuthService.loginUser(mockLoginParams)).rejects.toThrow(
+        new NotFoundError(ERROR_MESSAGES.RESOURCE_NOT_FOUND)
+      );
+
+      expect(UserDB.findByEmail).toHaveBeenCalledWith(mockLoginParams.email);
+      expect(AuthService.comparePasswords).not.toHaveBeenCalled();
+      expect(AuthService.createJwtTokens).not.toHaveBeenCalled();
+      expect(RefreshTokenService.createToken).not.toHaveBeenCalled();
+    });
+
+    it("should throw a UnauthorizedError if the email is not verified", async () => {
+      const unverifiedUser = { ...mockUser, isEmailVerified: false };
+      (UserDB.findByEmail as jest.Mock).mockResolvedValue(unverifiedUser);
+
+      await expect(AuthService.loginUser(mockLoginParams)).rejects.toThrow(
+        new UnauthorizedError(ERROR_MESSAGES.EMAIL_NOT_VERIFIED)
+      );
+
+      expect(UserDB.findByEmail).toHaveBeenCalledWith(mockLoginParams.email);
+      expect(AuthService.comparePasswords).not.toHaveBeenCalled();
+      expect(AuthService.createJwtTokens).not.toHaveBeenCalled();
+      expect(RefreshTokenService.createToken).not.toHaveBeenCalled();
+    });
+
+    it("should throw a ForbiddenError if the password is incorrect", async () => {
+      (UserDB.findByEmail as jest.Mock).mockResolvedValue(mockUser);
+      jest.spyOn(AuthService, "comparePasswords").mockResolvedValue(false);
+
+      await expect(AuthService.loginUser(mockLoginParams)).rejects.toThrow(
+        new ForbiddenError(ERROR_MESSAGES.INVALID_CREDENTIALS)
+      );
+
+      expect(UserDB.findByEmail).toHaveBeenCalledWith(mockLoginParams.email);
+
+      expect(AuthService.comparePasswords).toHaveBeenCalledWith({
+        rawPassword: mockLoginParams.password,
+        hashedPassword: mockUser.password,
+      });
+
+      expect(AuthService.createJwtTokens).not.toHaveBeenCalled();
+      expect(RefreshTokenService.createToken).not.toHaveBeenCalled();
     });
   });
 });
