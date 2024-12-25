ea990f6c38ab543f633771a652ea158fef33edaf
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-12-25 17:31:00 +0100

    test(resetPasswordService): add unit tests for sendResetToken method

---
+++ apps/server/src/modules/resetPasswordToken/tests/resetPasswordService.test.ts
@@ -0,0 +1,112 @@
+import { ERROR_MESSAGES } from "../../../constants/errorMessages";
+import { ForbiddenError } from "../../../errors";
+import type { UserEntry } from "../../user/user.type";
+import UserService from "../../user/userService";
+import type { ResetTokenEntry } from "../resetPassword.type";
+import ResetPasswordDB from "../resetPasswordDB";
+import ResetPasswordService from "../resetPasswordService";
+
+jest.mock("../../user/userService");
+jest.mock("../resetPasswordDB");
+
+describe("ResetPasswordService tests", () => {
+  beforeAll(() => {
+    jest.clearAllMocks();
+  });
+
+  const mockEmail = "test@example.com";
+
+  const mockUser: UserEntry = {
+    id: "user123",
+    email: "test@example.com",
+    username: "testuser",
+    password: "hashedpassword",
+    isEmailVerified: true,
+    googleId: "",
+    profilePicture: "",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    videoIds: [],
+  };
+
+  const mockResetTokenEntry: ResetTokenEntry = {
+    id: "token123",
+    userId: "user123",
+    createdAt: new Date(),
+    expiresAt: new Date(),
+    token: "mockResetTokenValue",
+  };
+
+  describe("sendResetToken email method tests", () => {
+    beforeEach(() => {
+      jest.clearAllMocks();
+    });
+
+    it("should throw a ForbiddenError if the user is not found", async () => {
+      (UserService.getUserByEmail as jest.Mock).mockResolvedValue(null);
+
+      await expect(
+        ResetPasswordService.sendResetToken(mockEmail)
+      ).rejects.toThrow(new ForbiddenError(ERROR_MESSAGES.FORBIDDEN));
+
+      expect(UserService.getUserByEmail).toHaveBeenCalledWith(mockEmail);
+
+      expect(UserService.getUserByEmail).toHaveBeenCalledTimes(1);
+    });
+
+    it("should throw a ForbiddenError if the user's email is not verified", async () => {
+      (UserService.getUserByEmail as jest.Mock).mockResolvedValue({
+        ...mockUser,
+        isEmailVerified: false,
+      });
+
+      await expect(
+        ResetPasswordService.sendResetToken(mockEmail)
+      ).rejects.toThrow(new ForbiddenError(ERROR_MESSAGES.EMAIL_NOT_VERIFIED));
+
+      expect(UserService.getUserByEmail).toHaveBeenCalledWith(mockEmail);
+
+      expect(UserService.getUserByEmail).toHaveBeenCalledTimes(1);
+    });
+
+    it("should throw a ForbiddenError if the reset token is already sent", async () => {
+      (UserService.getUserByEmail as jest.Mock).mockResolvedValue(mockUser);
+
+      (ResetPasswordDB.findByUserId as jest.Mock).mockResolvedValue(
+        mockResetTokenEntry
+      );
+
+      await expect(
+        ResetPasswordService.sendResetToken(mockEmail)
+      ).rejects.toThrow(new ForbiddenError(ERROR_MESSAGES.RESET_LINK_SENT));
+
+      expect(UserService.getUserByEmail).toHaveBeenCalledWith(mockEmail);
+
+      expect(UserService.getUserByEmail).toHaveBeenCalledTimes(1);
+
+      expect(ResetPasswordDB.findByUserId).toHaveBeenCalledWith(mockUser.id);
+
+      expect(mockResetTokenEntry.userId).toBe(mockUser.id);
+    });
+
+    it("should send a reset token email", async () => {
+      (UserService.getUserByEmail as jest.Mock).mockResolvedValue(mockUser);
+
+      (ResetPasswordDB.findByUserId as jest.Mock).mockResolvedValue(null);
+
+      jest
+        .spyOn(ResetPasswordService, "createToken")
+        .mockResolvedValue(mockResetTokenEntry.token);
+
+      await ResetPasswordService.sendResetToken(mockEmail);
+
+      expect(UserService.getUserByEmail).toHaveBeenCalledWith(mockEmail);
+
+      expect(UserService.getUserByEmail).toHaveBeenCalledTimes(1);
+
+      expect(ResetPasswordDB.findByUserId).toHaveBeenCalledWith(mockUser.id);
+
+      expect(ResetPasswordDB.findByUserId).toHaveBeenCalledTimes(1);
+    });
+  });
+});
