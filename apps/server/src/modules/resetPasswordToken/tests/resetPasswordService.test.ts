e19cb81cffd5c6b038d3c82cf54048a25daf2842
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-12-27 13:13:23 +0100

    test(resetPasswordService): add tests for reset method to handle invalid and expired tokens

--- apps/server/src/modules/resetPasswordToken/tests/resetPasswordService.test.ts
+++ apps/server/src/modules/resetPasswordToken/tests/resetPasswordService.test.ts
@@ -1,13 +1,17 @@
 import { ERROR_MESSAGES } from "../../../constants/errorMessages";
 import { ForbiddenError } from "../../../errors";
+import AuthService from "../../auth/authService";
 import type { UserEntry } from "../../user/user.type";
+import UserDB from "../../user/userDB";
 import UserService from "../../user/userService";
 import type { ResetTokenEntry } from "../resetPassword.type";
 import ResetPasswordDB from "../resetPasswordDB";
 import ResetPasswordService from "../resetPasswordService";
 
-jest.mock("../../user/userService");
 jest.mock("../resetPasswordDB");
+jest.mock("../../user/userDB");
+jest.mock("../../user/userService");
+jest.mock("../../auth/authService");
 
 describe("ResetPasswordService tests", () => {
   beforeAll(() => {
@@ -124,6 +128,77 @@ describe("ResetPasswordService tests", () => {
 
       expect(result).toBe(mockResetTokenEntry.token);
       expect(typeof result).toBe("string");
+    });
+  });
+
+  describe("reset method tests", () => {
+    beforeEach(() => {
+      jest.clearAllMocks();
+    });
+
+    it("should throw a ForbiddenError if the reset token is invalid", async () => {
+      const inValidResetToken = "invalidToken";
+
+      (ResetPasswordDB.findByToken as jest.Mock).mockResolvedValue(null);
+
+      await expect(
+        ResetPasswordService.reset(inValidResetToken, "password123")
+      ).rejects.toThrow(new ForbiddenError(ERROR_MESSAGES.INVALID_TOKEN));
+
+      expect(ResetPasswordDB.findByToken).toHaveBeenCalledWith(
+        inValidResetToken
+      );
+    });
+
+    it("should throw a ForbiddenError if the reset token is expired", async () => {
+      (ResetPasswordDB.findByToken as jest.Mock).mockResolvedValue({
+        ...mockResetTokenEntry,
+        expiresAt: new Date(Date.now() - 1000),
+      });
+
+      await expect(
+        ResetPasswordService.reset(mockResetTokenEntry.token, "password123")
+      ).rejects.toThrow(new ForbiddenError(ERROR_MESSAGES.INVALID_TOKEN));
+
+      expect(ResetPasswordDB.findByToken).toHaveBeenCalledWith(
+        mockResetTokenEntry.token
+      );
+      expect(ResetPasswordDB.deleteMany).toHaveBeenCalledWith(
+        mockResetTokenEntry.userId
+      );
+    });
+
+    it("should reset the user's password", async () => {
+      const newPassword = "newpassword123";
+
+      jest
+        .spyOn(ResetPasswordService, "verfiyResetToken")
+        .mockResolvedValue(mockResetTokenEntry);
+
+      jest
+        .spyOn(AuthService, "hashPassword")
+        .mockResolvedValue("hashedPassword");
+
+      (UserDB.update as jest.Mock).mockResolvedValue(null);
+
+      await ResetPasswordService.reset(mockResetTokenEntry.token, newPassword);
+
+      expect(ResetPasswordService.verfiyResetToken).toHaveBeenCalledWith(
+        mockResetTokenEntry.token
+      );
+
+      expect(AuthService.hashPassword).toHaveBeenCalledWith(newPassword);
+
+      expect(AuthService.hashPassword).toHaveBeenCalledTimes(1);
+
+      expect(UserDB.update).toHaveBeenCalledWith({
+        userId: mockResetTokenEntry.userId,
+        data: { password: "hashedPassword" },
+      });
+
+      expect(ResetPasswordDB.deleteMany).toHaveBeenCalledWith(
+        mockResetTokenEntry.userId
+      );
     });
   });
 });
