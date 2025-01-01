af26578b9b291859c15ab20abcd165a180802129
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-01-01 16:51:08 +0100

    refactor(video): update fetchYoutubeVideoData to use youtubeId and enhance error handling

--- apps/server/src/modules/video/videoService.ts
+++ apps/server/src/modules/video/videoService.ts
@@ -7,14 +7,15 @@ import type {
   FindVideoParams,
   UserVideos,
   VideoEntry,
+  YoutubeVideoData,
 } from "./video.type";
 
 import VideoDB from "./videoDB";
 
 class VideoService {
-  private async fetchYoutubeVideoData(videoId: string): Promise<any> {
+  async fetchYoutubeVideoData(youtubeId: string): Promise<YoutubeVideoData> {
     const response = await fetch(
-      `${YOUTUBE_API_URL}/videos?id=${videoId}&key=${YOUTUBE_API_KEY}&part=snippet,statistics,player`
+      `${YOUTUBE_API_URL}/videos?id=${youtubeId}&key=${YOUTUBE_API_KEY}&part=snippet,statistics,player`
     );
 
     if (!response.ok) {
@@ -23,7 +24,11 @@ class VideoService {
 
     const data = await response.json();
 
-    return data.items;
+    if (!data.items.length) {
+      throw new NotFoundError(ERROR_MESSAGES.RESOURCE_NOT_FOUND);
+    }
+
+    return data.items[0];
   }
 
   async findUserVideos({
@@ -41,45 +46,55 @@ class VideoService {
     return { videos, videosCount, totalPages };
   }
 
-  async findVideoById({
-    videoId,
-    userId,
-  }: FindVideoParams): Promise<VideoEntry> {
-    const video = await VideoDB.findById(videoId);
-
-    if (video) {
-      return await this.linkVideoToUser(video, userId);
-    }
-
-    if (!video) {
-      const youtubeVideoData = await this.fetchYoutubeVideoData(videoId);
-
-      if (!youtubeVideoData.length) {
-        throw new NotFoundError(ERROR_MESSAGES.RESOURCE_NOT_FOUND);
-      }
-
-      const newVideo = await VideoDB.create({
-        videoData: youtubeVideoData[0],
-        userId,
-      });
-
-      return await this.linkVideoToUser(newVideo, userId);
-    }
+  async findVideoByYoutubeId(youtubeId: string): Promise<VideoEntry | null> {
+    const video = await VideoDB.findByYoutubeId(youtubeId);
 
     return video;
+  }
+
+  async createVideo({
+    userId,
+    youtubeId,
+  }: { userId: string; youtubeId: string }): Promise<VideoEntry> {
+    const youtubeVideoData = await this.fetchYoutubeVideoData(youtubeId);
+
+    const newVideo = await VideoDB.create({
+      userId,
+      videoData: youtubeVideoData,
+      youtubeId,
+    });
+
+    return newVideo;
   }
 
   async linkVideoToUser(
     video: VideoEntry,
     userId: string
   ): Promise<VideoEntry> {
-    if (video.userIds && video.userIds.includes(userId)) {
-      return video;
-    }
-
     const updatedVideo = await VideoDB.connectVideoToUser(video.id, userId);
 
     return updatedVideo;
+  }
+
+  async getVideoData({
+    userId,
+    youtubeId,
+  }: { youtubeId: string; userId: string }): Promise<VideoEntry> {
+    if (!youtubeId || !userId) {
+      throw new BadRequestError(ERROR_MESSAGES.BAD_REQUEST);
+    }
+
+    const video = await this.findVideoByYoutubeId(youtubeId);
+
+    if (video && video.userIds && video.userIds.includes(userId)) return video;
+
+    if (video && video.userIds && !video.userIds.includes(userId)) {
+      return await this.linkVideoToUser(video, userId);
+    }
+
+    const newVideo = await this.createVideo({ userId, youtubeId });
+
+    return await this.linkVideoToUser(newVideo, userId);
   }
 }
 
