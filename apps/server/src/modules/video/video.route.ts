be0fdcd24619854b5d628e727aa471abfed87d9e
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-01-07 17:13:59 +0100

    feat(video): integrate VideoController with VideoService for improved route handling

--- apps/server/src/modules/video/video.route.ts
+++ apps/server/src/modules/video/video.route.ts
@@ -1,12 +1,20 @@
 import { Router } from "express";
 
+import prismaClient from "../../lib/prisma";
+
+import { VideoController } from "./video.controller";
+import { VideoDatabase } from "./video.db";
+import { VideoService } from "./video.service";
+
 import isAuthenticated from "../../middlewares/isAuthenticated";
 import validateRequest from "../../middlewares/validateRequest";
 
-import VideoController from "./video.controller";
-
 import { idParamSchema } from "../../common/schemas/id-param.schema";
 import { paginationSchema } from "../../common/schemas/query-pagination.schema";
+
+const videoDB = new VideoDatabase(prismaClient);
+const videoService = new VideoService(videoDB);
+const videoController = new VideoController(videoService);
 
 const router = Router();
 
@@ -17,16 +25,14 @@ router.use(isAuthenticated);
 // - POST /: Create a new video (requires request body validation)
 router
   .route("/")
-  .get(
-    validateRequest({ query: paginationSchema }),
-    VideoController.getUserVideos
+  .get(validateRequest({ query: paginationSchema }), (req, res) =>
+    videoController.getUserVideos(req, res)
   );
 
 router
   .route("/:id")
-  .get(
-    validateRequest({ params: idParamSchema }),
-    VideoController.getVideoById
+  .get(validateRequest({ params: idParamSchema }), (req, res) =>
+    videoController.getVideoById(req, res)
   );
 
 export default router;
