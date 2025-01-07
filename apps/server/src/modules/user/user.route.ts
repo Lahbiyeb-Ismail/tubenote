ba3073daf08a289afffdf63b692239a813a70164
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-01-07 13:50:38 +0100

    refactor(user): instantiate UserController with UserService and services for improved dependency

--- apps/server/src/modules/user/user.route.ts
+++ apps/server/src/modules/user/user.route.ts
@@ -3,9 +3,18 @@ import { Router } from "express";
 import isAuthenticated from "../../middlewares/isAuthenticated";
 import validateRequest from "../../middlewares/validateRequest";
 
+import prismaClient from "../../lib/prisma";
+import { PasswordService } from "../password/password.service";
 import { updatePasswordSchema } from "./schema/update-password.schema";
 import { updateUserSchema } from "./schema/update-user.schema";
-import UserController from "./user.controller";
+import { UserController } from "./user.controller";
+import { UserDatabase } from "./user.db";
+import { UserService } from "./user.service";
+
+const userDB = new UserDatabase(prismaClient);
+const passwordService = new PasswordService();
+const userService = new UserService(userDB, passwordService);
+const userController = new UserController(userService);
 
 const router = Router();
 
@@ -16,18 +25,16 @@ router.use(isAuthenticated);
 // - PATCH /me: Update the current user's information (requires request body validation)
 router
   .route("/me")
-  .get(UserController.getCurrentUser)
-  .patch(
-    validateRequest({ body: updateUserSchema }),
-    UserController.updateCurrentUser
+  .get((req, res) => userController.getCurrentUser(req, res))
+  .patch(validateRequest({ body: updateUserSchema }), (req, res) =>
+    userController.updateCurrentUser(req, res)
   );
 
 // - PATCH /update-password: Update the current user's password (requires request body validation)
 router
   .route("/update-password")
-  .patch(
-    validateRequest({ body: updatePasswordSchema }),
-    UserController.updateUserPassword
+  .patch(validateRequest({ body: updatePasswordSchema }), (req, res) =>
+    userController.updateUserPassword(req, res)
   );
 
 export default router;
