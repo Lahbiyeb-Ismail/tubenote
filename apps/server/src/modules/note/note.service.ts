1cfb4cae18e6faa8557e0c90f0fa4cce9f3dff69
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-01-08 16:35:42 +0100

    refactor(note): update INoteService to use NoteDto instead of NoteEntry

--- apps/server/src/modules/note/note.service.ts
+++ apps/server/src/modules/note/note.service.ts
@@ -5,21 +5,22 @@ import type { FindManyDto } from "../../common/dtos/find-many.dto";
 import type { CreateNoteDto } from "./dtos/create-note.dto";
 import type { DeleteNoteDto } from "./dtos/delete-note.dto";
 import type { FindNoteDto } from "./dtos/find-note.dto";
+import type { NoteDto } from "./dtos/note.dto";
 import type { UpdateNoteDto } from "./dtos/update-note.dto";
 import type { INoteDatabase } from "./note.db";
-import type { NoteEntry, UserNotes } from "./note.type";
+import type { UserNotes } from "./note.type";
 
 export interface INoteService {
-  findNote(findNoteDto: FindNoteDto): Promise<NoteEntry>;
-  createNote(userId: string, createNoteDto: CreateNoteDto): Promise<NoteEntry>;
+  findNote(findNoteDto: FindNoteDto): Promise<NoteDto>;
+  createNote(userId: string, createNoteDto: CreateNoteDto): Promise<NoteDto>;
   updateNote(
     findNoteDto: FindNoteDto,
     updateNoteDto: UpdateNoteDto
-  ): Promise<NoteEntry>;
+  ): Promise<NoteDto>;
   deleteNote(deleteNoteDto: DeleteNoteDto): Promise<void>;
   fetchUserNotes(findManyDto: FindManyDto): Promise<UserNotes>;
-  fetchRecentNotes(findManyDto: FindManyDto): Promise<NoteEntry[]>;
-  fetchRecentlyUpdatedNotes(findManyDto: FindManyDto): Promise<NoteEntry[]>;
+  fetchRecentNotes(findManyDto: FindManyDto): Promise<NoteDto[]>;
+  fetchRecentlyUpdatedNotes(findManyDto: FindManyDto): Promise<NoteDto[]>;
   fetchNotesByVideoId(id: string, findManyDto: FindManyDto): Promise<UserNotes>;
 }
 
@@ -30,7 +31,7 @@ export class NoteService implements INoteService {
     this.noteDB = noteDB;
   }
 
-  async findNote(findNoteDto: FindNoteDto): Promise<NoteEntry> {
+  async findNote(findNoteDto: FindNoteDto): Promise<NoteDto> {
     const note = await this.noteDB.find(findNoteDto);
 
     if (!note) {
@@ -43,7 +44,7 @@ export class NoteService implements INoteService {
   async createNote(
     userId: string,
     createNoteDto: CreateNoteDto
-  ): Promise<NoteEntry> {
+  ): Promise<NoteDto> {
     const note = await this.noteDB.create(userId, createNoteDto);
 
     return note;
@@ -52,7 +53,7 @@ export class NoteService implements INoteService {
   async updateNote(
     findNoteDto: FindNoteDto,
     updateNoteDto: UpdateNoteDto
-  ): Promise<NoteEntry> {
+  ): Promise<NoteDto> {
     await this.findNote(findNoteDto);
 
     const updatedNote = await this.noteDB.update(findNoteDto, updateNoteDto);
@@ -77,7 +78,7 @@ export class NoteService implements INoteService {
     return { notes, notesCount, totalPages };
   }
 
-  async fetchRecentNotes(findManyDto: FindManyDto): Promise<NoteEntry[]> {
+  async fetchRecentNotes(findManyDto: FindManyDto): Promise<NoteDto[]> {
     const recentNotes = await this.noteDB.findMany(findManyDto);
 
     return recentNotes;
@@ -85,7 +86,7 @@ export class NoteService implements INoteService {
 
   async fetchRecentlyUpdatedNotes(
     findManyDto: FindManyDto
-  ): Promise<NoteEntry[]> {
+  ): Promise<NoteDto[]> {
     const recentlyUpdatedNotes = await this.noteDB.findMany(findManyDto);
 
     return recentlyUpdatedNotes;
