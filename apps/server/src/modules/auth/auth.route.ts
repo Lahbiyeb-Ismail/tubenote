15c0a368f037911f6790c05d82e5a1d45a6da055
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-01-11 16:26:12 +0100

    refactor(auth): simplify authentication route by configuring passport and using dependency injection

--- apps/server/src/modules/auth/auth.route.ts
+++ apps/server/src/modules/auth/auth.route.ts
@@ -1,61 +1,19 @@
 import { Router } from "express";
 import passport from "passport";
 
-import prismaClient from "../../lib/prisma";
-
-import { UserDatabase } from "../user/user.db";
-import { AuthController } from "./auth.controller";
-import { AuthService } from "./auth.service";
-
 import isAuthenticated from "../../middlewares/isAuthenticated";
 import validateRequest from "../../middlewares/validateRequest";
 
 import envConfig from "../../config/envConfig";
-import { EmailService } from "../../services/emailService";
-import { JwtService } from "../jwt/jwt.service";
-import { PasswordService } from "../password/password.service";
-import { RefreshTokenDatabase } from "../refreshToken/refresh-token.db";
-import { RefreshTokenService } from "../refreshToken/refresh-token.service";
-import { UserService } from "../user/user.service";
-import { VerificationTokenDatabase } from "../verifyEmailToken/verification-token.db";
+
+import { configurePassport } from "./config/passport.config";
+
 import { loginUserSchema } from "./schemas/login-user.schema";
 import { registerUserSchema } from "./schemas/register-user.schema";
-import {
-  GoogleAuthStrategy,
-  type GoogleConfig,
-} from "./strategies/google.strategy";
 
-const userDB = new UserDatabase(prismaClient);
-const refreshTokenDB = new RefreshTokenDatabase(prismaClient);
-const verificationTokenDB = new VerificationTokenDatabase(prismaClient);
+import { authController } from "./di-container";
 
-const jwtService = new JwtService();
-const passwordService = new PasswordService(userDB);
-const userService = new UserService(userDB, passwordService);
-const refreshTokenService = new RefreshTokenService(refreshTokenDB);
-const emailService = new EmailService(userDB, verificationTokenDB);
-
-const googleAuthConfig: GoogleConfig = {
-  clientID: envConfig.google.client_id,
-  clientSecret: envConfig.google.client_secret,
-  callbackURL: "/api/v1/auth/google/callback",
-};
-
-const authService = new AuthService(
-  jwtService,
-  userService,
-  passwordService,
-  refreshTokenService,
-  emailService
-);
-const authController = new AuthController(authService);
-
-const googleAuthStrategy = new GoogleAuthStrategy(
-  googleAuthConfig,
-  userService
-);
-
-passport.use(googleAuthStrategy.getStrategy());
+configurePassport();
 
 const router = Router();
 
