0e6ee34e15605608be5cf6f2eb4fde8940e0dc10
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-01-14 17:04:45 +0100

    feat(user): add user routes for fetching and updating current user information

---
+++ apps/server/src/modules/user/user.routes.ts
@@ -0,0 +1,31 @@
+import { Router } from "express";
+
+import isAuthenticated from "../../middlewares/isAuthenticated";
+import validateRequest from "../../middlewares/validateRequest";
+
+import { userController } from "../../di-container";
+
+import { updateUserSchema } from "./schema/update-user.schema";
+
+const router = Router();
+
+// - isAuthenticated: Ensures the user is authenticated before accessing any user routes.
+router.use(isAuthenticated);
+
+// - GET /me: Get the current user's information
+// - PATCH /me: Update the current user's information (requires request body validation)
+router
+  .route("/me")
+  .get((req, res) => userController.getCurrentUser(req, res))
+  .patch(validateRequest({ body: updateUserSchema }), (req, res) =>
+    userController.updateCurrentUser(req, res)
+  );
+
+// - PATCH /update-password: Update the current user's password (requires request body validation)
+// router
+//   .route("/update-password")
+//   .patch(validateRequest({ body: updatePasswordSchema }), (req, res) =>
+//     userController.updateUserPassword(req, res)
+//   );
+
+export default router;
