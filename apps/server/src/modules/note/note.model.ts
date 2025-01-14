d4ad485e400d0eb90722ee2020c4cdb71b60731b
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-01-14 15:18:33 +0100

    feat(note): add Note interface to define note structure

---
+++ apps/server/src/modules/note/note.model.ts
@@ -0,0 +1,13 @@
+export interface Note {
+  id: string;
+  userId: string;
+  videoId: string;
+  youtubeId: string;
+  title: string;
+  content: string;
+  videoTitle: string;
+  thumbnail: string;
+  timestamp: number;
+  createdAt: Date;
+  updatedAt: Date;
+}
