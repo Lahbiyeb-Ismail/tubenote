5fa01fd569007cc2319a4f3d32fd1819cf8acc6c
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-01-14 16:31:43 +0100

    feat(resetPassword): add interfaces for ResetPassword repository, service, and controller

---
+++ apps/server/src/modules/resetPasswordToken/reset-password.types.ts
@@ -0,0 +1,35 @@
+import type { Response } from "express";
+
+import type { EmptyRecord, TypedRequest } from "../../types";
+
+import type { ResetPasswordToken } from "./reset-password.model";
+
+import type { EmailBodyDto } from "../../common/dtos/email-body.dto";
+import type { TokenParamDto } from "../../common/dtos/token-param.dto";
+import type { PasswordBodyDto } from "./dtos/password-body.dto";
+export interface IResetPasswordRespository {
+  findByUserId(userId: string): Promise<ResetPasswordToken | null>;
+  findByToken(token: string): Promise<ResetPasswordToken | null>;
+  create(userId: string): Promise<string>;
+  deleteMany(userId: string): Promise<void>;
+}
+
+export interface IResetPasswordService {
+  sendResetToken(email: string): Promise<void>;
+  createToken(userId: string): Promise<string>;
+  reset(token: string, password: string): Promise<void>;
+  findResetToken(token: string): Promise<ResetPasswordToken | null>;
+  isResetTokenExpired(resetToken: ResetPasswordToken): Promise<boolean>;
+  verifyResetToken(token: string): Promise<ResetPasswordToken>;
+}
+export interface IResetPasswordController {
+  forgotPassword(req: TypedRequest<EmailBodyDto>, res: Response): Promise<void>;
+  resetPassword(
+    req: TypedRequest<PasswordBodyDto, TokenParamDto>,
+    res: Response
+  ): Promise<void>;
+  verifyResetToken(
+    req: TypedRequest<EmptyRecord, TokenParamDto>,
+    res: Response
+  ): Promise<void>;
+}
