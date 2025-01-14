45f7594424e6886b20c9401cc4ca69f9c6a43069
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-01-14 16:58:37 +0100

    feat(user): add interfaces for user repository, service, and controller

---
+++ apps/server/src/modules/user/user.types.ts
@@ -0,0 +1,32 @@
+import type { Response } from "express";
+
+import type { TypedRequest } from "../../types";
+
+import type { User } from "./user.model";
+
+import type { CreateUserDto } from "./dtos/create-user.dto";
+import type { UpdateUserDto } from "./dtos/update-user.dto";
+export interface IUserRepository {
+  create(createUserDto: CreateUserDto): Promise<User>;
+  findByEmail(email: string): Promise<User | null>;
+  findById(id: string): Promise<User | null>;
+  updateUser(id: string, updateUserDto: UpdateUserDto): Promise<User>;
+  updatePassword(userId: string, hashedPassword: string): Promise<User>;
+}
+
+export interface IUserService {
+  createUser(createUserDto: CreateUserDto): Promise<User>;
+  findOrCreateUser(createUserDto: CreateUserDto): Promise<User>;
+  getUserByEmail(email: string): Promise<User | null>;
+  getUserById(userId: string): Promise<User>;
+  updateUser(id: string, updateUserDto: UpdateUserDto): Promise<User>;
+  verifyUserEmail(id: string): Promise<User>;
+}
+
+export interface IUserController {
+  getCurrentUser(req: TypedRequest, res: Response): Promise<void>;
+  updateCurrentUser(
+    req: TypedRequest<UpdateUserDto>,
+    res: Response
+  ): Promise<void>;
+}
