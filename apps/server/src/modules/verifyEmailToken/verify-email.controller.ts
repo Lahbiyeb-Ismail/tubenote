93cdfe235796ff9467f6c739892493ca383ae709
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-01-14 17:29:50 +0100

    refactor(verifyEmail): simplify constructor and && private readonly service in VerifyEmailController

--- apps/server/src/modules/verifyEmailToken/verify-email.controller.ts
+++ apps/server/src/modules/verifyEmailToken/verify-email.controller.ts
@@ -1,27 +1,18 @@
 import type { Response } from "express";
 import httpStatus from "http-status";
 
-import type { EmailBodyDto } from "../../common/dtos/email-body.dto";
 import type { TokenParamDto } from "../../common/dtos/token-param.dto";
 import type { EmptyRecord, TypedRequest } from "../../types";
-import type { IVerifyEmailService } from "./verify-email.service";
-
-export interface IVerifyEmailController {
-  verifyEmail(
-    req: TypedRequest<EmptyRecord, TokenParamDto>,
-    res: Response
-  ): Promise<void>;
-}
+import type {
+  IVerifyEmailController,
+  IVerifyEmailService,
+} from "./verify-email.types";
 
 /**
  * Controller for handling email verification operations.
  */
 export class VerifyEmailController implements IVerifyEmailController {
-  private verifyEmailService: IVerifyEmailService;
-
-  constructor(verifyEmailService: IVerifyEmailService) {
-    this.verifyEmailService = verifyEmailService;
-  }
+  constructor(private readonly _verifyEmailService: IVerifyEmailService) {}
 
   /**
    * Verify the user's email using the token.
@@ -34,7 +25,7 @@ export class VerifyEmailController implements IVerifyEmailController {
   ): Promise<void> {
     const { token } = req.params;
 
-    await this.verifyEmailService.verifyUserEmail(token);
+    await this._verifyEmailService.verifyUserEmail(token);
 
     res.status(httpStatus.OK).json({ message: "Email verified successfully." });
   }
