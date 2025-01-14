9ff273a91c46c2a65ebaf1511af768bfb7bec4a3
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-01-14 15:17:21 +0100

    feat(note): define types and interfaces for note repository, service, and controller

---
+++ apps/server/src/modules/note/note.types.ts
@@ -0,0 +1,70 @@
+import type { Response } from "express";
+
+import type { Note } from "./note.model";
+
+import type { EmptyRecord, TypedRequest } from "../../types";
+
+import type { FindManyDto } from "../../common/dtos/find-many.dto";
+import type { IdParamDto } from "../../common/dtos/id-param.dto";
+import type { QueryPaginationDto } from "../../common/dtos/query-pagination.dto";
+
+import type { CreateNoteDto } from "./dtos/create-note.dto";
+import type { DeleteNoteDto } from "./dtos/delete-note.dto";
+import type { FindNoteDto } from "./dtos/find-note.dto";
+import type { UpdateNoteDto } from "./dtos/update-note.dto";
+
+export interface UserNotes {
+  notes: Note[];
+  notesCount: number;
+  totalPages: number;
+}
+
+export interface INoteRepository {
+  find(findNoteDto: FindNoteDto): Promise<Note | null>;
+  create(userId: string, createNoteDto: CreateNoteDto): Promise<Note>;
+  update(findNoteDto: FindNoteDto, updateNoteDto: UpdateNoteDto): Promise<Note>;
+  delete(deleteNoteDto: DeleteNoteDto): Promise<void>;
+  findMany(findManyDto: FindManyDto): Promise<Note[]>;
+  findManyByVideoId(id: string, findManyDto: FindManyDto): Promise<Note[]>;
+  count(userId: string): Promise<number>;
+}
+
+export interface INoteService {
+  findNote(findNoteDto: FindNoteDto): Promise<Note>;
+  createNote(userId: string, createNoteDto: CreateNoteDto): Promise<Note>;
+  updateNote(
+    findNoteDto: FindNoteDto,
+    updateNoteDto: UpdateNoteDto
+  ): Promise<Note>;
+  deleteNote(deleteNoteDto: DeleteNoteDto): Promise<void>;
+  fetchUserNotes(findManyDto: FindManyDto): Promise<UserNotes>;
+  fetchRecentNotes(findManyDto: FindManyDto): Promise<Note[]>;
+  fetchRecentlyUpdatedNotes(findManyDto: FindManyDto): Promise<Note[]>;
+  fetchNotesByVideoId(id: string, findManyDto: FindManyDto): Promise<UserNotes>;
+}
+
+export interface INoteController {
+  createNote(req: TypedRequest<CreateNoteDto>, res: Response): Promise<void>;
+  updateNote(
+    req: TypedRequest<UpdateNoteDto, IdParamDto>,
+    res: Response
+  ): Promise<void>;
+  deleteNote(
+    req: TypedRequest<EmptyRecord, IdParamDto>,
+    res: Response
+  ): Promise<void>;
+  getNoteById(
+    req: TypedRequest<EmptyRecord, IdParamDto>,
+    res: Response
+  ): Promise<void>;
+  getUserNotes(
+    req: TypedRequest<EmptyRecord, EmptyRecord, QueryPaginationDto>,
+    res: Response
+  ): Promise<void>;
+  getUserRecentNotes(req: TypedRequest, res: Response): Promise<void>;
+  getRecentlyUpatedNotes(req: TypedRequest, res: Response): Promise<void>;
+  getNotesByVideoId(
+    req: TypedRequest<EmptyRecord, IdParamDto, QueryPaginationDto>,
+    res: Response
+  ): Promise<void>;
+}
