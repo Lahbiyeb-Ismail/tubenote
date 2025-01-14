b96286070dba143b312f95637f1e1e1e53ede906
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-01-14 15:45:27 +0100

    feat(auth): add authentication routes for user registration, login, logout, and Google OAuth

---
+++ apps/server/src/modules/auth/auth.routes.ts
@@ -0,0 +1,53 @@
+import { Router } from "express";
+import passport from "passport";
+
+import envConfig from "../../config/envConfig";
+
+import isAuthenticated from "../../middlewares/isAuthenticated";
+import validateRequest from "../../middlewares/validateRequest";
+
+import { authController, googleAuthStrategy } from "../../di-container";
+
+import { loginUserSchema } from "./schemas/login-user.schema";
+import { registerUserSchema } from "./schemas/register-user.schema";
+
+passport.use(googleAuthStrategy.getStrategy());
+
+const router = Router();
+
+// - POST /register: Register a new user (requires request body validation).
+router
+  .route("/register")
+  .post(validateRequest({ body: registerUserSchema }), (req, res) =>
+    authController.register(req, res)
+  );
+
+// - POST /login: Authenticate a user (requires request body validation).
+router
+  .route("/login")
+  .post(validateRequest({ body: loginUserSchema }), (req, res) =>
+    authController.login(req, res)
+  );
+
+// - POST /logout: Log out the current user.
+router
+  .route("/logout")
+  .post(isAuthenticated, (req, res) => authController.logout(req, res));
+
+// - POST /refresh: Refresh the user's access token.
+router.route("/refresh").post((req, res) => authController.refresh(req, res));
+
+// - GET /google: Initiate Google OAuth authentication.
+router
+  .route("/google")
+  .get(passport.authenticate("google", { scope: ["profile", "email"] }));
+
+// - GET /google/callback: Handle the Google OAuth callback.
+router.route("/google/callback").get(
+  passport.authenticate("google", {
+    failureRedirect: `${envConfig.client.url}/login`,
+  }),
+  (req, res) => authController.loginWithGoogle(req, res)
+);
+
+export default router;
