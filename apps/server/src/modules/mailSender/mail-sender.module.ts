05d30bee83576921c5c5eaa7225cdea5b17a1550
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-01-15 16:01:13 +0100

    refactor(mailSender): integrate resetPasswordService into MailSenderService

--- apps/server/src/modules/mailSender/mail-sender.module.ts
+++ apps/server/src/modules/mailSender/mail-sender.module.ts
@@ -1,9 +1,12 @@
 import transporter from "../../config/nodemailer.config";
+import { resetPasswordService } from "../resetPasswordToken/reset-password.module";
 import { verifyEmailService } from "../verifyEmailToken/verify-email.module";
 import { MailSenderService } from "./mail-sender.service";
+
 import type { IMailSenderService } from "./mail-sender.types";
 
 export const mailSenderService: IMailSenderService = new MailSenderService(
   transporter,
-  verifyEmailService
+  verifyEmailService,
+  resetPasswordService
 );
