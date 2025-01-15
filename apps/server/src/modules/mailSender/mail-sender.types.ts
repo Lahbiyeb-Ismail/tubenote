e70f80bdc98dda13da0363a723bdb040199bd2e6
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-01-15 15:58:33 +0100

    refactor(mailSender): update sendResetPasswordEmail to accept only email parameter

--- apps/server/src/modules/mailSender/mail-sender.types.ts
+++ apps/server/src/modules/mailSender/mail-sender.types.ts
@@ -3,8 +3,5 @@ import type { SendMailDto } from "./dtos/send-mail.dto";
 export interface IMailSenderService {
   sendMail(sendMailDto: SendMailDto): Promise<void>;
   sendVerificationEmail(email: string): Promise<void>;
-  sendResetPasswordEmail({
-    email,
-    token,
-  }: { email: string; token: string }): Promise<void>;
+  sendResetPasswordEmail(email: string): Promise<void>;
 }
