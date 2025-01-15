36821414fc8254fd906c8b1c278c0d8d5dc447d6
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-01-15 17:58:48 +0100

    feat(auth): define types for local authentication service and controller

---
+++ apps/server/src/modules/auth/local-auth/local-auth.types.ts
@@ -0,0 +1,17 @@
+import type { Response } from "express";
+import type { TypedRequest } from "../../../types";
+
+import type { User } from "../../user/user.model";
+import type { LoginResponseDto } from "../dtos/login-response.dto";
+import type { LoginUserDto } from "../dtos/login-user.dto";
+import type { RegisterUserDto } from "../dtos/register-user.dto";
+
+export interface ILocalAuthService {
+  registerUser: (registerUserDto: RegisterUserDto) => Promise<User>;
+  loginUser: (loginUserDto: LoginUserDto) => Promise<LoginResponseDto>;
+}
+
+export interface ILocalAuthController {
+  register(req: TypedRequest<RegisterUserDto>, res: Response): Promise<void>;
+  login(req: TypedRequest<LoginUserDto>, res: Response): Promise<void>;
+}
