6467d5b61648e6bfde7e2c1b01dc88bdce50c32a
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-01-17 18:30:05 +0100

    refactor(password): remove obsolete DTOs and password service

--- apps/server/src/modules/password/dtos/compare-passwords.dto.ts
+++
@@ -1,4 +0,0 @@
-export interface ComparePasswordsDto {
-  password: string;
-  hashedPassword: string;
-}
--- apps/server/src/modules/password/dtos/reset-password.dto.ts
+++
@@ -1,4 +0,0 @@
-export interface ResetPasswordDto {
-  userId: string;
-  password: string;
-}
