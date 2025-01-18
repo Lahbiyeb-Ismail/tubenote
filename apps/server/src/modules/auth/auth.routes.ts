e29f2a2e03e4f0dbabf2e7b044115f2ea1c9e903
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-01-18 14:54:28 +0100

    feat(auth): add routes for password reset process including forgot password and token verification

--- apps/server/src/modules/auth/auth.routes.ts
+++ apps/server/src/modules/auth/auth.routes.ts
@@ -8,7 +8,11 @@ import validateRequest from "../../middlewares/validate-request.middleware";
 
 import { authController, googleAuthStrategy } from "./auth.module";
 
+import { emailBodySchema } from "../../common/schemas/email-body.schema";
+import { tokenParamSchema } from "../../common/schemas/token-param.schema";
 import { localAuthController } from "./local-auth/local-auth.module";
+import { resetPasswordController } from "./reset-password/reset-password.module";
+import { passwordBodySchema } from "./reset-password/schemas/password-body.schema";
 import { loginUserSchema } from "./schemas/login-user.schema";
 import { registerUserSchema } from "./schemas/register-user.schema";
 
@@ -49,6 +53,29 @@ router.route("/google/callback").get(
     failureRedirect: `${envConfig.client.url}/login`,
   }),
   (req, res) => authController.loginWithGoogle(req, res)
+);
+
+// - POST /forgot-password: Initiate the password reset process (requires request body validation).
+router
+  .route("/forgot-password")
+  .post(validateRequest({ body: emailBodySchema }), (req, res) =>
+    resetPasswordController.forgotPassword(req, res)
+  );
+
+// - GET /reset-password/:token/verify: Verify the password reset token (requires request params validation).
+router
+  .route("/reset-password/:token/verify")
+  .get(validateRequest({ params: tokenParamSchema }), (req, res) =>
+    resetPasswordController.verifyResetToken(req, res)
+  );
+
+// - POST /reset-password/:token: Reset the password using a valid token (requires request params and body validation).
+router.route("/reset-password/:token").post(
+  validateRequest({
+    params: tokenParamSchema,
+    body: passwordBodySchema,
+  }),
+  (req, res) => resetPasswordController.resetPassword(req, res)
 );
 
 export default router;
