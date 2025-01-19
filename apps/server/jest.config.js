64f6fbf3817cad7a78a412d1e655ce7aec32c988
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-01-19 16:16:55 +0100

    chore(jest): add module name mappings for path aliases

--- apps/server/jest.config.js
+++ apps/server/jest.config.js
@@ -4,4 +4,13 @@ module.exports = {
   roots: ["<rootDir>/src", "<rootDir>/tests"],
   testMatch: ["**/__tests__/**/*.ts", "**/?(*.)+(spec|test).ts"],
   moduleFileExtensions: ["ts", "js", "json", "node"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+    "^@config/(.*)$": "<rootDir>/src/config/$1",
+    "^@common/(.*)$": "<rootDir>/src/common/$1",
+    "^@middlewares/(.*)$": "<rootDir>/src/middlewares/$1",
+    "^@modules/(.*)$": "<rootDir>/src/modules/$1",
+    "^@utils/(.*)$": "<rootDir>/src/utils/$1",
+    "^@constants/(.*)$": "<rootDir>/src/constants/$1",
+  },
 };
