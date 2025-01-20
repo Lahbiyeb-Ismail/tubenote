c0e5e912fdaa60b891c91c6a22e0b168531ec51e
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-01-20 13:19:27 +0100

    feat(jwt): implement JWT service with token signing and verification

---
+++ apps/server/src/modules/auth/core/jwt/jwt.types.ts
@@ -0,0 +1,11 @@
+import type { JwtPayload } from "@/types";
+
+import type { LoginResponseDto } from "@modules/auth/dtos/login-response.dto";
+import type { SignTokenDto } from "./dtos/sign-token.dto";
+import type { VerifyTokenDto } from "./dtos/verify-token.dto";
+
+export interface IJwtService {
+  verify(verifyTokenDto: VerifyTokenDto): Promise<JwtPayload | null>;
+  sign(signTokenDto: SignTokenDto): string;
+  generateAuthTokens(userId: string): LoginResponseDto;
+}
