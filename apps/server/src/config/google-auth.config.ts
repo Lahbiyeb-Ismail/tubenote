2be0400cc5dd9588fdb6d822786fe0d201af8a86
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-01-20 16:16:11 +0100

    refactor(config): remove Google authentication configuration file from the global config dir

--- apps/server/src/config/google-auth.config.ts
+++
@@ -1,9 +0,0 @@
-import envConfig from "./env.config";
-
-import type { GoogleConfig } from "@/modules/auth/core/strategies/google.strategy";
-
-export const googleAuthConfig: GoogleConfig = {
-  clientID: envConfig.google.client_id,
-  clientSecret: envConfig.google.client_secret,
-  callbackURL: "/api/v1/auth/google/callback",
-};
