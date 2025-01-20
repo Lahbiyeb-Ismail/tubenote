b9de30f090e08a4de2f4ea41577066feb99e5003
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-01-20 16:14:43 +0100

    refactor(auth): add Google authentication configuration

---
+++ apps/server/src/modules/auth/config/google-auth.config.ts
@@ -0,0 +1,9 @@
+import envConfig from "@config/env.config";
+
+import type { GoogleConfig } from "@/modules/auth/core/strategies/google.strategy";
+
+export const googleAuthConfig: GoogleConfig = {
+  clientID: envConfig.google.client_id,
+  clientSecret: envConfig.google.client_secret,
+  callbackURL: envConfig.google.google_callback_url,
+};
