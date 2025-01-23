4b8a2c33c1579d94fd9b7f8cddcfb4dc1a8df65d
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-01-23 16:43:53 +0100

    feat(local-auth): integrate email verification service in user registration

--- apps/server/src/modules/auth/features/local-auth/local-auth.module.ts
+++ apps/server/src/modules/auth/features/local-auth/local-auth.module.ts
@@ -4,12 +4,15 @@ import { LocalAuthService } from "./local-auth.service";
 import { jwtService } from "@modules/auth/core/services/jwt/jwt.module";
 import { passwordHasherService } from "@modules/auth/core/services/password-hasher/password-hasher.module";
 import { refreshTokenService } from "@modules/auth/features/refresh-token/refresh-token.module";
+import { verifyEmailService } from "@modules/auth/features/verify-email/verify-email.module";
+
 import { mailSenderService } from "@modules/mailSender/mail-sender.module";
 import { userService } from "@modules/user/user.module";
 
 const localAuthService = new LocalAuthService(
   jwtService,
   userService,
+  verifyEmailService,
   passwordHasherService,
   refreshTokenService,
   mailSenderService
