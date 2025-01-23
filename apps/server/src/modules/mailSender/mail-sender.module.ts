31e65dbe26b7f87c29a21215f2d6361508bdd2b7
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-01-23 16:48:29 +0100

    refactor(mail-sender): simplify service by removing unused services and updating method signatures

--- apps/server/src/modules/mailSender/mail-sender.module.ts
+++ apps/server/src/modules/mailSender/mail-sender.module.ts
@@ -2,13 +2,4 @@ import transporter from "@config/node-mailer.config";
 
 import { MailSenderService } from "./mail-sender.service";
 
-import { resetPasswordService } from "@modules/auth/features/reset-password/reset-password.module";
-import { verifyEmailService } from "@modules/auth/features/verify-email/verify-email.module";
-
-import type { IMailSenderService } from "./mail-sender.types";
-
-export const mailSenderService: IMailSenderService = new MailSenderService(
-  transporter,
-  verifyEmailService,
-  resetPasswordService
-);
+export const mailSenderService = new MailSenderService(transporter);
