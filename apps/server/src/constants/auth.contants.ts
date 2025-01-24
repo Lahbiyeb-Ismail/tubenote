893ca83956b47a7ae8e6f05e2f1d14f3e7c3e51c
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-01-24 13:52:40 +0100

    refactor(constants): add constants for verify email and reset password token secrets and expiration

--- apps/server/src/constants/auth.contants.ts
+++ apps/server/src/constants/auth.contants.ts
@@ -29,3 +29,31 @@ export const REFRESH_TOKEN_SECRET = envConfig.jwt.refresh_token.secret;
  * This value is retrieved from the environment configuration.
  */
 export const REFRESH_TOKEN_EXPIRE = envConfig.jwt.refresh_token.expire;
+
+/**
+ * Secret key used for signing and verifying verify email tokens.
+ * This value is retrieved from the environment configuration.
+ */
+export const VERIFY_EMAIL_TOKEN_SECRET =
+  envConfig.jwt.verify_email_token.secret;
+
+/**
+ * The expiration time for the verify email token.
+ * This value is retrieved from the environment configuration.
+ */
+export const VERIFY_EMAIL_TOKEN_EXPIRES_IN =
+  envConfig.jwt.verify_email_token.expires_in;
+
+/**
+ * Secret key used for signing and verifying reset password tokens.
+ * This value is retrieved from the environment configuration.
+ */
+export const RESET_PASSWORD_TOKEN_SECRET =
+  envConfig.jwt.reset_password_token.secret;
+
+/**
+ * The expiration time for the reset password token.
+ * This value is retrieved from the environment configuration.
+ */
+export const RESET_PASSWORD_TOKEN_EXPIRES_IN =
+  envConfig.jwt.reset_password_token.expires_in;
