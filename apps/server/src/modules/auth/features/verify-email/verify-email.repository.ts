2f0697381040bfff7c28c6cc3254a8b92597eef1
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-01-24 15:16:03 +0100

    refactor(verifyemail): update saveToken method to accept SaveTokenDto and include expiresAt check

--- apps/server/src/modules/auth/features/verify-email/verify-email.repository.ts
+++ apps/server/src/modules/auth/features/verify-email/verify-email.repository.ts
@@ -3,6 +3,7 @@ import type { PrismaClient } from "@prisma/client";
 import handleAsyncOperation from "@/utils/handle-async-operation";
 
 import { ERROR_MESSAGES } from "@/constants/error-messages.contants";
+import type { SaveTokenDto } from "./dtos/save-token.dto";
 import type { VerifyEmailToken } from "./verify-email.model";
 import type { IVerifyEmailRepository } from "./verify-email.types";
 
@@ -27,19 +28,19 @@ export class VerifyEmailRepository implements IVerifyEmailRepository {
         this._db.emailVerificationToken.findFirst({
           where: {
             token,
+            expiresAt: { gte: new Date() },
           },
         }),
       { errorMessage: ERROR_MESSAGES.FAILD_TO_FIND }
     );
   }
 
-  async saveToken(userId: string, token: string): Promise<VerifyEmailToken> {
+  async saveToken(saveTokenDto: SaveTokenDto): Promise<VerifyEmailToken> {
     return handleAsyncOperation(
       () =>
         this._db.emailVerificationToken.create({
           data: {
-            token,
-            userId,
+            ...saveTokenDto,
           },
         }),
       { errorMessage: ERROR_MESSAGES.FAILD_TO_CREATE }
