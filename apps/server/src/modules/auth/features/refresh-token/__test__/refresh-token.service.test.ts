35f490373d01f23c8b0e4bdf2939fc4208033b76
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-01-25 14:06:26 +0100

    test(refresh): update refresh token service tests to improve error handling && token management

--- apps/server/src/modules/auth/features/refresh-token/__test__/refresh-token.service.test.ts
+++ apps/server/src/modules/auth/features/refresh-token/__test__/refresh-token.service.test.ts
@@ -1,25 +1,34 @@
-import { ForbiddenError, NotFoundError, UnauthorizedError } from "@/errors";
+import {
+  BadRequestError,
+  DatabaseError,
+  ForbiddenError,
+  UnauthorizedError,
+} from "@/errors";
 
+import { REFRESH_TOKEN_EXPIRES_IN } from "@constants/auth.contants";
 import { ERROR_MESSAGES } from "@constants/error-messages.contants";
+import { stringToDate } from "@utils/convert-string-to-date";
 
 import { RefreshTokenService } from "../refresh-token.service";
 
 import type { IJwtService } from "@/modules/auth/core/services/jwt/jwt.types";
+import type { LoginResponseDto } from "@/modules/auth/dtos/login-response.dto";
 import type { RefreshDto } from "@/modules/auth/dtos/refresh.dto";
 import type { JwtPayload } from "@/types";
-import type { CreateTokenDto } from "../dtos/create-token.dto";
 import type { RefreshToken } from "../refresh-token.model";
 import {
   type IRefreshTokenRepository,
   type IRefreshTokenService,
 } from "../refresh-token.types";
 
+jest.mock("@utils/convert-string-to-date");
+
 describe("RefreshTokenService", () => {
   let refreshTokenService: IRefreshTokenService;
 
   const mockRefreshTokenRepository: jest.Mocked<IRefreshTokenRepository> = {
-    create: jest.fn(),
-    find: jest.fn(),
+    saveToken: jest.fn(),
+    findValidToken: jest.fn(),
     delete: jest.fn(),
     deleteAll: jest.fn(),
   };
@@ -35,12 +44,19 @@ describe("RefreshTokenService", () => {
   const mockTokenId = "token-id-001";
   const mockAccessToken = "test-access-token";
   const mockNewRefreshToken = "new-refresh-token";
+  const expiresIn = stringToDate(REFRESH_TOKEN_EXPIRES_IN);
+
+  const mockLoginResponse: LoginResponseDto = {
+    accessToken: mockAccessToken,
+    refreshToken: mockNewRefreshToken,
+  };
 
   const mockRefreshToken: RefreshToken = {
     id: mockTokenId,
     userId: mockUserId,
     token: mockToken,
-    createdAt: new Date(),
+    createdAt: new Date(Date.now()),
+    expiresAt: expiresIn,
   };
 
   beforeEach(() => {
@@ -52,218 +68,106 @@ describe("RefreshTokenService", () => {
     jest.clearAllMocks();
   });
 
-  describe("RefreshTokenService - createToken", () => {
-    const createTokenDto: CreateTokenDto = {
-      userId: mockUserId,
-      token: mockToken,
-    };
-
-    it("should create a refresh token", async () => {
-      mockRefreshTokenRepository.create.mockResolvedValue(mockRefreshToken);
-
-      const result = await refreshTokenService.createToken(createTokenDto);
-
-      expect(result).toEqual(mockRefreshToken);
-
-      expect(mockRefreshTokenRepository.create).toHaveBeenCalledWith(
-        createTokenDto
-      );
-    });
-
-    it("should handle unexpected errors from refresh Token repository create method", async () => {
-      const error = new Error("creation of token failed");
-
-      mockRefreshTokenRepository.create.mockRejectedValue(error);
-
-      await expect(
-        refreshTokenService.createToken(createTokenDto)
-      ).rejects.toThrow(error);
-    });
-  });
-
-  describe("RefreshTokenService - findToken", () => {
-    it("should find a refreshToken by token value", async () => {
-      mockRefreshTokenRepository.find.mockResolvedValue(mockRefreshToken);
-
-      const result = await refreshTokenService.findToken(mockToken);
-
-      expect(result).toEqual(mockRefreshToken);
-      expect(mockRefreshTokenRepository.find).toHaveBeenCalledWith(mockToken);
-    });
-
-    it("should return null when token is not found", async () => {
-      mockRefreshTokenRepository.find.mockResolvedValue(null);
-
-      const result = await refreshTokenService.findToken(mockToken);
-
-      expect(result).toBeNull();
-      expect(mockRefreshTokenRepository.find).toHaveBeenCalledWith(mockToken);
-    });
-
-    it("should handle unexpected errors from refresh Token repository find method", async () => {
-      const error = new Error("finding token failed");
-
-      mockRefreshTokenRepository.find.mockRejectedValue(error);
-
-      await expect(refreshTokenService.findToken(mockToken)).rejects.toThrow(
-        error
-      );
-    });
-  });
-
-  describe("RefreshTokenService - deleteToken", () => {
-    it("should delete a token if it exists", async () => {
-      mockRefreshTokenRepository.find.mockResolvedValue(mockRefreshToken);
-
-      await refreshTokenService.deleteToken(mockToken);
-
-      expect(mockRefreshTokenRepository.delete).toHaveBeenCalledWith(mockToken);
-    });
-
-    it("should throw NotFoundError if token does not exist", async () => {
-      mockRefreshTokenRepository.find.mockResolvedValue(null);
-
-      await expect(refreshTokenService.deleteToken(mockToken)).rejects.toThrow(
-        new NotFoundError(ERROR_MESSAGES.RESOURCE_NOT_FOUND)
-      );
-    });
-
-    it("should handle unexpected errors from refresh Token repository find method", async () => {
-      const error = new Error("finding token failed");
-
-      mockRefreshTokenRepository.find.mockRejectedValue(error);
-
-      await expect(refreshTokenService.deleteToken(mockToken)).rejects.toThrow(
-        error
-      );
-    });
-
-    it("should handle unexpected errors from refresh Token repository delete method", async () => {
-      const error = new Error("deleting token failed");
-
-      mockRefreshTokenRepository.find.mockResolvedValue(mockRefreshToken);
-
-      mockRefreshTokenRepository.delete.mockRejectedValue(error);
-
-      await expect(refreshTokenService.deleteToken(mockToken)).rejects.toThrow(
-        error
-      );
-    });
-  });
-
-  describe("RefreshTokenService - deleteAllTokens", () => {
-    it("should delete all tokens for a user", async () => {
-      await refreshTokenService.deleteAllTokens(mockUserId);
-
-      expect(mockRefreshTokenRepository.deleteAll).toHaveBeenCalledWith(
-        mockUserId
-      );
-    });
-
-    it("should handle unexpected errors from refresh Token repository deleteAll method", async () => {
-      const error = new Error("deleting all tokens failed");
-
-      mockRefreshTokenRepository.deleteAll.mockRejectedValue(error);
-
-      await expect(
-        refreshTokenService.deleteAllTokens(mockUserId)
-      ).rejects.toThrow(error);
-    });
-  });
-
   describe("RefreshTokenService - refreshToken", () => {
     const refreshDto: RefreshDto = { userId: mockUserId, token: mockToken };
 
-    const mockPayload: JwtPayload = {
+    const decodedToken: JwtPayload = {
       userId: mockUserId,
       exp: 1123454,
       iat: 4445787,
     };
 
-    it("should successfully refresh tokens", async () => {
-      mockJwtService.verify.mockResolvedValue(mockPayload);
+    it("should successfully return new auth tokens for a valid refresh token", async () => {
+      mockJwtService.verify.mockResolvedValue(decodedToken);
 
-      jest
-        .spyOn(refreshTokenService, "findToken")
-        .mockResolvedValue(mockRefreshToken);
+      mockRefreshTokenRepository.findValidToken.mockResolvedValue(
+        mockRefreshToken
+      );
 
-      jest
-        .spyOn(refreshTokenService, "deleteToken")
-        .mockResolvedValue(undefined);
+      mockJwtService.generateAuthTokens.mockReturnValue(mockLoginResponse);
 
-      mockJwtService.generateAuthTokens.mockReturnValue({
-        accessToken: mockAccessToken,
-        refreshToken: mockNewRefreshToken,
-      });
-
-      jest
-        .spyOn(refreshTokenService, "createToken")
-        .mockResolvedValue(mockRefreshToken);
+      mockRefreshTokenRepository.saveToken.mockResolvedValue(mockRefreshToken);
 
       const result = await refreshTokenService.refreshToken(refreshDto);
 
-      expect(result).toEqual({
-        accessToken: mockAccessToken,
-        refreshToken: mockNewRefreshToken,
-      });
+      expect(result).toEqual(mockLoginResponse);
 
-      expect(refreshTokenService.findToken).toHaveBeenCalledWith(mockToken);
+      expect(mockJwtService.verify).toHaveBeenCalled();
 
-      expect(refreshTokenService.deleteToken).toHaveBeenCalledWith(mockToken);
+      expect(mockRefreshTokenRepository.delete).toHaveBeenCalledWith(mockToken);
 
       expect(mockJwtService.generateAuthTokens).toHaveBeenCalledWith(
         mockUserId
       );
 
-      expect(refreshTokenService.createToken).toHaveBeenCalledWith({
+      expect(mockRefreshTokenRepository.saveToken).toHaveBeenCalledWith({
         userId: mockUserId,
         token: mockNewRefreshToken,
+        expiresAt: expiresIn,
       });
     });
 
-    it("should throw ForbiddenError when JWT verification fails adn delete all user's tokens", async () => {
-      mockJwtService.verify.mockResolvedValue(null);
+    it("should throw BadRequestError for an invalid refresh token", async () => {
+      const error = new BadRequestError(ERROR_MESSAGES.INVALID_TOKEN);
 
-      jest
-        .spyOn(refreshTokenService, "deleteAllTokens")
-        .mockResolvedValue(undefined);
+      mockJwtService.verify.mockRejectedValue(error);
 
       await expect(
         refreshTokenService.refreshToken(refreshDto)
-      ).rejects.toThrow(new ForbiddenError(ERROR_MESSAGES.FORBIDDEN));
+      ).rejects.toThrow(error);
 
-      expect(refreshTokenService.deleteAllTokens).toHaveBeenCalledWith(
-        mockUserId
-      );
+      expect(mockJwtService.verify).toHaveBeenCalled();
+
+      expect(mockRefreshTokenRepository.findValidToken).not.toHaveBeenCalled();
+
+      expect(mockRefreshTokenRepository.delete).not.toHaveBeenCalled();
+
+      expect(mockJwtService.generateAuthTokens).not.toHaveBeenCalled();
     });
 
-    it("should throw UnauthorizedError when userId does not match payload", async () => {
+    it("should throw UnauthorizedError for mismatched user ID", async () => {
       mockJwtService.verify.mockResolvedValue({
-        ...mockPayload,
+        ...decodedToken,
         userId: "different-user-id",
       });
 
       await expect(
         refreshTokenService.refreshToken(refreshDto)
       ).rejects.toThrow(new UnauthorizedError(ERROR_MESSAGES.UNAUTHORIZED));
+
+      expect(mockRefreshTokenRepository.deleteAll).toHaveBeenCalledWith(
+        mockUserId
+      );
     });
 
     it("should throw ForbiddenError and delete all tokens when refresh token reuse is detected", async () => {
-      mockJwtService.verify.mockResolvedValue(mockPayload);
+      mockJwtService.verify.mockResolvedValue(decodedToken);
 
-      jest.spyOn(refreshTokenService, "findToken").mockResolvedValue(null);
-
-      jest
-        .spyOn(refreshTokenService, "deleteAllTokens")
-        .mockResolvedValue(undefined);
+      mockRefreshTokenRepository.findValidToken.mockResolvedValue(null);
 
       await expect(
         refreshTokenService.refreshToken(refreshDto)
       ).rejects.toThrow(new ForbiddenError(ERROR_MESSAGES.FORBIDDEN));
 
-      expect(refreshTokenService.deleteAllTokens).toHaveBeenCalledWith(
+      expect(mockRefreshTokenRepository.deleteAll).toHaveBeenCalledWith(
         mockUserId
       );
+    });
+
+    it("should throw DatabaseError for database operation failure", async () => {
+      const databaseError = new DatabaseError("Database error");
+
+      mockJwtService.verify.mockResolvedValue(decodedToken);
+
+      mockRefreshTokenRepository.findValidToken.mockResolvedValue(
+        mockRefreshToken
+      );
+
+      mockRefreshTokenRepository.delete.mockRejectedValue(databaseError);
+
+      await expect(
+        refreshTokenService.refreshToken(refreshDto)
+      ).rejects.toThrow(databaseError);
     });
   });
 });
