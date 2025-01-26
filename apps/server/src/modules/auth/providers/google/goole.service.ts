16cce20185e5e0eaff61512560ba262eb94d53c9
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-01-26 17:27:23 +0100

    refactor(google-auth): enhance googleLogin method to validate user ID and improve error handling

--- apps/server/src/modules/auth/providers/google/goole.service.ts
+++ apps/server/src/modules/auth/providers/google/goole.service.ts
@@ -17,21 +17,29 @@ export class GoogleAuthService implements IGoogleAuthService {
     private readonly _refreshTokenService: IRefreshTokenService
   ) {}
 
+  /**
+   * Authenticates a user using Google OAuth and generates access and refresh tokens.
+   * @param user - The user object retrieved from Google OAuth.
+   * @returns A promise resolving to a LoginResponseDto containing access and refresh tokens.
+   * @throws NotFoundError if the user is not found.
+   * @throws UnauthorizedError if the user's email is not verified.
+   */
   async googleLogin(user: User): Promise<LoginResponseDto> {
-    if (!user) {
+    if (!user || !user.id) {
       throw new NotFoundError(ERROR_MESSAGES.RESOURCE_NOT_FOUND);
     }
 
-    if (!user.isEmailVerified) {
+    const { isEmailVerified, id: userId } = user;
+
+    if (!isEmailVerified) {
       throw new UnauthorizedError(ERROR_MESSAGES.EMAIL_NOT_VERIFIED);
     }
 
-    const { accessToken, refreshToken } = this._jwtService.generateAuthTokens(
-      user.id
-    );
+    const { accessToken, refreshToken } =
+      this._jwtService.generateAuthTokens(userId);
 
     await this._refreshTokenService.saveToken({
-      userId: user.id,
+      userId,
       token: refreshToken,
       expiresAt: stringToDate(REFRESH_TOKEN_EXPIRES_IN),
     });
