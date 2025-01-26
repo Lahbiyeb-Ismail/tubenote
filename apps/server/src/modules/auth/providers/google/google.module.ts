68f9819652a78db99c5912ac75f00401de0d61db
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-01-26 17:40:56 +0100

    fix(google-auth): correct google.service.ts file name

--- apps/server/src/modules/auth/providers/google/google.module.ts
+++ apps/server/src/modules/auth/providers/google/google.module.ts
@@ -2,7 +2,7 @@ import { jwtService } from "@modules/auth/core/services/jwt/jwt.module";
 import { refreshTokenService } from "@modules/auth/features/refresh-token/refresh-token.module";
 
 import { GoogleController } from "./google.controller";
-import { GoogleAuthService } from "./goole.service";
+import { GoogleAuthService } from "./google.service";
 
 const googleAuthService = new GoogleAuthService(
   jwtService,
