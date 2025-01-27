8e88d3fc59940b64861a37ecc84068be551784d2
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-01-27 18:13:13 +0100

    refactor(auth): update googleLogin return type and add generateTemporaryCode method

--- apps/server/src/modules/auth/providers/google/google.types.ts
+++ apps/server/src/modules/auth/providers/google/google.types.ts
@@ -2,11 +2,16 @@ import type { Response } from "express";
 
 import type { TypedRequest } from "@/types";
 
-import type { LoginResponseDto } from "@modules/auth/dtos/login-response.dto";
 import type { User } from "@modules/user/user.model";
 
+import type { OauthLoginResponseDto } from "@modules/auth/dtos/oauth-login-response.dto";
+import type { OAuthTemporaryCodePayloadDto } from "@modules/auth/dtos/oauth-temp-code-payload.dto";
+
 export interface IGoogleAuthService {
-  googleLogin(user: User): Promise<LoginResponseDto>;
+  googleLogin(user: User): Promise<OauthLoginResponseDto>;
+  generateTemporaryCode(
+    temporaryCodePayloadDto: OAuthTemporaryCodePayloadDto
+  ): Promise<string>;
 }
 export interface IGoogleAuthController {
   googleLogin(req: TypedRequest, res: Response): Promise<void>;
