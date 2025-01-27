666134ebcbb4d9cb018f3c515def9988e133a8b6
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-01-27 18:20:18 +0100

    feat(auth): add exchangeOauthCodeForTokens method to IAuthService and IAuthController

--- apps/server/src/modules/auth/auth.types.ts
+++ apps/server/src/modules/auth/auth.types.ts
@@ -2,12 +2,19 @@ import type { Response } from "express";
 
 import type { TypedRequest } from "@/types";
 
+import type { LoginResponseDto } from "./dtos/login-response.dto";
 import type { LogoutUserDto } from "./dtos/logout-user.dto";
+import type { OauthCodeBodyDto } from "./dtos/oauth-temp-code-body.dto";
 
 export interface IAuthService {
   logoutUser(logoutUserDto: LogoutUserDto): Promise<void>;
+  exchangeOauthCodeForTokens(code: string): Promise<LoginResponseDto>;
 }
 
 export interface IAuthController {
   logout(req: TypedRequest, res: Response): Promise<void>;
+  exchangeOauthCodeForTokens(
+    req: TypedRequest<OauthCodeBodyDto>,
+    res: Response
+  ): Promise<void>;
 }
