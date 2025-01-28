9b7c81f898c8783bf64c01bf9df69dcddc338d99
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-01-28 14:12:20 +0100

    refactor(auth): refactor auth schemas and add OAuth schema
    - Renamed the \register-user.schema.ts\ file to \auth.schema.ts\ to better reflect its purpose.
    - Added a new \loginSchema\ to the \auth.schema.ts\ file for validating login credentials.
    - Created a new \oauth.schema.ts\ file to handle OAuth related schemas.
    - Removed the \login-user.schema.ts\ and \oauth-temp-code.schema.ts\ files.

--- apps/server/src/modules/auth/schemas/auth.schema.ts
+++ apps/server/src/modules/auth/schemas/auth.schema.ts
@@ -1,6 +1,13 @@
 import { z } from "zod";
 
-export const registerUserSchema = z.object({
+export const loginSchema = z.object({
+  email: z.string().email({ message: "Invalid email address." }),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters long." }),
+});
+
+export const registerSchema = z.object({
   username: z
     .string()
     .min(3, { message: "Username must be at least 3 characters long." }),
