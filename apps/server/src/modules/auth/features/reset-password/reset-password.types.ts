20ff891a256a634a1f387ddf5e92877e640f9932
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-01-29 20:44:51 +0100

    refactor(auth): remove unused reset password DTOs and repository implementation

--- apps/server/src/modules/auth/features/reset-password/reset-password.types.ts
+++ apps/server/src/modules/auth/features/reset-password/reset-password.types.ts
@@ -2,19 +2,9 @@ import type { Response } from "express";
 
 import type { EmptyRecord, TypedRequest } from "@/types";
 
-import type { ResetPasswordToken } from "./reset-password.model";
-
 import type { EmailBodyDto } from "@common/dtos/email-body.dto";
 import type { TokenParamDto } from "@common/dtos/token-param.dto";
 import type { PasswordBodyDto } from "./dtos/password-body.dto";
-import type { SaveTokenDto } from "./dtos/save-token.dto";
-
-export interface IResetPasswordRepository {
-  findByUserId(userId: string): Promise<ResetPasswordToken | null>;
-  findByToken(token: string): Promise<ResetPasswordToken | null>;
-  saveToken(saveTokenDto: SaveTokenDto): Promise<ResetPasswordToken>;
-  deleteMany(userId: string): Promise<void>;
-}
 
 export interface IResetPasswordService {
   sendResetToken(email: string): Promise<void>;
