f8aa3a7e58c869757f8fa8b33f1a7677027e491b
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-01-31 17:26:13 +0100

    refactor(user): add update user and update password validation schemas

---
+++ apps/server/src/modules/user/schemas/update-password.schema.ts
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+import { passwordSchema } from "@/common/schemas/user";
+
+export const updatePasswordSchema = z
+  .object({
+    currentPassword: passwordSchema,
+    newPassword: passwordSchema,
+  })
+  .refine((data) => data.currentPassword !== data.newPassword, {
+    message: "New password must be different from the current password.",
+    path: ["newPassword"],
+  });
