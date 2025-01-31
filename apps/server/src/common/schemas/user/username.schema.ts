d0a263873da713dba5ebcc797bb8b8fec7baa0e3
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-01-31 17:25:13 +0100

    refactor(user): add validation schemas for email, password, and username

---
+++ apps/server/src/common/schemas/user/username.schema.ts
@@ -0,0 +1,6 @@
+import { z } from "zod";
+
+export const usernameSchema = z
+  .string()
+  .min(3, { message: "Username must be at least 3 characters long." })
+  .max(20, { message: "Username must be at most 50 characters long." });
