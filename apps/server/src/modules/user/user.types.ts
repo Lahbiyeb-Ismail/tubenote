7371e6ec2180459bc6129f7e599043adbaa2e182
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-02-03 13:47:39 +0100

    refactor(user): add getUserByEmail and getUserById methods to IUserRepository interface

--- apps/server/src/modules/user/user.types.ts
+++ apps/server/src/modules/user/user.types.ts
@@ -13,6 +13,8 @@ import type { User } from "./user.model";
 export interface IUserRepository {
   transaction<T>(fn: (tx: IUserRepository) => Promise<T>): Promise<T>;
   createUser(dto: CreateUserDto): Promise<User>;
+  getUserByEmail(email: string): Promise<User | null>;
+  getUserById(id: string): Promise<User | null>;
   getUser(dto: GetUserDto): Promise<User | null>;
   updateUser(id: string, updateUserDto: UpdateUserDto): Promise<User>;
   updatePassword(userId: string, hashedPassword: string): Promise<User>;
