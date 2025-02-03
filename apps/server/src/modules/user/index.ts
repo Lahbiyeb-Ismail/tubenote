bd23899892f730f01fe718384ccd99fcd1ca8159
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-02-03 15:24:41 +0100

    refactor(user): export additional modules and components from user module

--- apps/server/src/modules/user/index.ts
+++ apps/server/src/modules/user/index.ts
@@ -1,3 +1,8 @@
 export * from "./user.module";
 export * from "./user.types";
 export * from "./user.model";
+export * from "./user.service";
+export * from "./user.controller";
+
+export * from "./dtos";
+export * from "./schemas";
