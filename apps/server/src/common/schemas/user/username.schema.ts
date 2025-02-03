1aa2cc31876e469baee2160fc0ebac736f2ddbc6
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-02-03 16:55:40 +0100

    fix(user): correct maximum length message in username schema to 20 characters

--- apps/server/src/common/schemas/user/username.schema.ts
+++ apps/server/src/common/schemas/user/username.schema.ts
@@ -3,4 +3,4 @@ import { z } from "zod";
 export const usernameSchema = z
   .string()
   .min(3, { message: "Username must be at least 3 characters long." })
-  .max(20, { message: "Username must be at most 50 characters long." });
+  .max(20, { message: "Username must be at most 20 characters long." });
