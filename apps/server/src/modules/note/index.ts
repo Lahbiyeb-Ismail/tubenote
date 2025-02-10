d338472cf35c16246bc43c7bd5b54caf1924126b
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-02-10 14:22:32 +0100

    refactor(note): add index file for module exports to streamline imports

---
+++ apps/server/src/modules/note/index.ts
@@ -0,0 +1,11 @@
+export * from "./dtos";
+export * from "./schemas";
+
+export * from "./note.model";
+export * from "./note.types";
+
+export * from "./note.repository";
+export * from "./note.service";
+export * from "./note.controller";
+
+export * from "./note.module";
