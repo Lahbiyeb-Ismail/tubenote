b6ba1acfb1319dde162d7cbaad086af3c1160604
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-02-10 14:19:32 +0100

    refactor(note): enhance validation for note schema fields and add index file for schema exports

--- apps/server/src/modules/note/schemas/create-note.schema.ts
+++ apps/server/src/modules/note/schemas/create-note.schema.ts
@@ -1,15 +1,25 @@
 import { z } from "zod";
 
-export const createNoteSchema = z.object({
-  title: z
-    .string()
-    .min(3, { message: "Title must be at least 3 characters long." }),
-  content: z
-    .string()
-    .min(10, { message: "Content must be at least 10 characters long." }),
-  videoTitle: z.string(),
-  thumbnail: z.string(),
-  videoId: z.string(),
-  youtubeId: z.string(),
-  timestamp: z.number(),
-});
+export const createNoteSchema = z
+  .object({
+    title: z
+      .string()
+      .min(3, { message: "Title must be at least 3 characters long." }),
+    content: z
+      .string()
+      .min(10, { message: "Content must be at least 10 characters long." }),
+    videoTitle: z
+      .string()
+      .min(3, { message: "Video title must be at least 3 characters long." }),
+    thumbnail: z
+      .string()
+      .min(3, { message: "Thumbnail must be at least 3 characters long." }),
+    videoId: z
+      .string()
+      .min(3, { message: "Video id must be at least 3 characters long." }),
+    youtubeId: z
+      .string()
+      .min(3, { message: "Youtube id must be at least 3 characters long." }),
+    timestamp: z.number(),
+  })
+  .strict();
---
+++ apps/server/src/modules/note/schemas/index.ts
@@ -0,0 +1,3 @@
+export * from "./create-note.schema";
+export * from "./update-note.schema";
+export * from "./note-id.schema";
