bdb2d0ed98e33565d017f0d604e639875bcacad3
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-02-25 12:00:10 +0000

    docs(user): enhance user DTOs for improved clarity and functionality

--- apps/server/src/modules/user/dtos/user.dto.ts
+++ apps/server/src/modules/user/dtos/user.dto.ts
@@ -1,24 +1,73 @@
 import type { ICreateDto, IUpdateDto } from "@/modules/shared";
 import type { User } from "@modules/user";
 
+/**
+ * Data Transfer Object (DTO) for creating a user.
+ *
+ * This interface extends the `ICreateDto` interface, omitting the `userId` property.
+ *
+ * @interface ICreateUserDto
+ * @extends {Omit<ICreateDto<User>, "userId">}
+ */
 export interface ICreateUserDto extends Omit<ICreateDto<User>, "userId"> {}
 
+/**
+ * Data Transfer Object (DTO) for retrieving user information.
+ *
+ * @interface IGetUserDto
+ *
+ * @property {string} [id] - The unique identifier of the user.
+ * @property {string} [email] - The email address of the user.
+ */
 export interface IGetUserDto {
   id?: string;
   email?: string;
 }
 
+/**
+ * Data Transfer Object (DTO) for updating a user.
+ *
+ * This interface extends the `IUpdateDto` interface for the `User` entity,
+ * omitting the `userId` property.
+ *
+ * @interface IUpdateUserDto
+ * @extends {Omit<IUpdateDto<User>, "userId">}
+ */
 export interface IUpdateUserDto extends Omit<IUpdateDto<User>, "userId"> {}
 
+/**
+ * Interface representing the data transfer object for updating a user's password.
+ */
 export interface IUpdatePasswordBodyDto {
+  /**
+   * The current password of the user.
+   */
   currentPassword: string;
+
+  /**
+   * The new password that the user wants to set.
+   */
   newPassword: string;
 }
 
+/**
+ * Data Transfer Object (DTO) for updating a user's password.
+ *
+ * This interface extends the `IUpdatePasswordBodyDto` and includes an additional
+ * `id` property to uniquely identify the user whose password is being updated.
+ *
+ * @interface IUpdatePasswordDto
+ * @extends IUpdatePasswordBodyDto
+ *
+ * @property {string} id - The unique identifier of the user.
+ */
 export interface IUpdatePasswordDto extends IUpdatePasswordBodyDto {
   id: string;
 }
 
+/**
+ * Data Transfer Object for resetting a user's password.
+ */
 export interface IResetPasswordDto {
   id: string;
   newPassword: string;
