6c3dfff8443bf232a0ecc847af89669b23f9aabf
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-02-25 15:50:51 +0000

    refactor(schemas): consolidate login && register schemas into a single file && add OAuth schema

--- apps/server/src/modules/shared/schemas/auth/login.schema.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-
-import { emailSchema, passwordSchema } from "@/common/schemas/user";
-
-export const loginSchema = z.object({
-  email: emailSchema,
-  password: passwordSchema,
-});
