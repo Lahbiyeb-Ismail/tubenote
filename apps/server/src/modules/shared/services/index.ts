3d71112726783829fc46b5bf23e1420277845df1
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-02-26 17:00:46 +0000

    feat(shared): implement logger service with configurable logging levels and transports

--- apps/server/src/modules/shared/services/index.ts
+++ apps/server/src/modules/shared/services/index.ts
@@ -2,3 +2,4 @@ export * from "./mail-sender";
 export * from "./cache";
 export * from "./crypto";
 export * from "./response-formatter";
+export * from "./logger";
---
+++ apps/server/src/modules/shared/services/logger/index.ts
@@ -0,0 +1,5 @@
+export * from "./logger.types";
+export * from "./logger.constants";
+
+export * from "./logger.service";
+export * from "./logger.module";
