df8ee87a52d1580f7664c5ab72a14133f9110b57
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-03-05 16:57:37 +0000

    feat(user): integrate account service into user service for enhanced functionality

--- apps/server/src/modules/user/user.module.ts
+++ apps/server/src/modules/user/user.module.ts
@@ -1,12 +1,17 @@
 import { prismaClient } from "@/modules/shared/config";
 import { cryptoService } from "@/modules/shared/services";
 
+import { accountService } from "./features/account/account.module";
 import { UserController } from "./user.controller";
 import { UserRepository } from "./user.repository";
 import { UserService } from "./user.service";
 
 const userRepository = new UserRepository(prismaClient);
-const userService = new UserService(userRepository, cryptoService);
+const userService = new UserService(
+  userRepository,
+  accountService,
+  cryptoService
+);
 const userController = new UserController(userService);
 
 export { userController, userService, userRepository };
