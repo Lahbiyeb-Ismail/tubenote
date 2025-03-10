154a9fe8897c5d2c0ace32f421a6afc3bd478dc2
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-03-10 17:54:34 +0000

    feat(user): add transaction support to createUserWithAccount method in IUserService

--- apps/server/src/modules/user/user.types.ts
+++ apps/server/src/modules/user/user.types.ts
@@ -83,6 +83,7 @@ export interface IUserRepository {
  */
 export interface IUserService {
   createUserWithAccount(
+    tx: Prisma.TransactionClient,
     createUserDto: ICreateUserDto,
     createAccountDto: ICreateAccountDto
   ): Promise<User>;
