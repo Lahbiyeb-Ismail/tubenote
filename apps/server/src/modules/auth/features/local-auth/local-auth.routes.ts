5549243e434e111b4b5cb14c07041c5cf5858dae
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-03-30 16:49:55 +0000

    refactor(auth): update login local auth route to use new rate limit middleware

--- apps/server/src/modules/auth/features/local-auth/local-auth.routes.ts
+++ apps/server/src/modules/auth/features/local-auth/local-auth.routes.ts
@@ -4,11 +4,7 @@ import { validateRequest } from "@/middlewares";
 
 import { loginSchema, registerSchema } from "@/modules/shared/schemas";
 
-import {
-  // createRateLimitIncrementer,
-  createRateLimitMiddleware,
-  // createRateLimitResetter,
-} from "@/middlewares/rate-limit.middleware";
+import { createRateLimitMiddleware } from "@/middlewares/rate-limit.middleware";
 import { ERROR_MESSAGES } from "@/modules/shared/constants";
 import { loggerService, rateLimitService } from "@/modules/shared/services";
 import { AUTH_RATE_LIMIT_CONFIG } from "../../config";
@@ -17,24 +13,13 @@ import { localAuthController } from "./local-auth.module";
 const localAuthRoutes = Router();
 
 // IP-based rate limiting middleware for login
-const ipRateLimiter = createRateLimitMiddleware({
+const loginRateLimiter = createRateLimitMiddleware({
   rateLimitService,
   logger: loggerService,
-  keyGenerator: (req) => `login:ip:${req.ip}`,
+  keyGenerator: (req) => `login:ip:email:${req.ip}-${req.body.email}`,
   limits: AUTH_RATE_LIMIT_CONFIG.login,
   errorMessage: ERROR_MESSAGES.TOO_MANY_ATTEMPTS,
 });
-
-// Middleware to increment rate limit on failure
-// const incrementRateLimit = createRateLimitIncrementer({
-//   rateLimitService,
-//   limits: AUTH_RATE_LIMIT_CONFIG.login,
-// });
-
-// Middleware to reset rate limit on success
-// const resetRateLimit = createRateLimitResetter({
-//   rateLimitService,
-// });
 
 // - POST /register: Register a new user (requires request body validation).
 localAuthRoutes
@@ -46,7 +31,7 @@ localAuthRoutes
 // - POST /login: Authenticate a user (requires request body validation).
 localAuthRoutes
   .route("/login")
-  .post(validateRequest({ body: loginSchema }), ipRateLimiter, (req, res) =>
+  .post(validateRequest({ body: loginSchema }), loginRateLimiter, (req, res) =>
     localAuthController.login(req, res)
   );
 
