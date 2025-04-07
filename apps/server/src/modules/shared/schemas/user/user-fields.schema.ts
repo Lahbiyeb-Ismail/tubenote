a4790e71200ab1bea62658c0b0db7a084b4839e6
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-04-07 16:37:04 +0100

    fix(schema): enhance username and password validation with additional constraints

--- apps/server/src/modules/shared/schemas/user/user-fields.schema.ts
+++ apps/server/src/modules/shared/schemas/user/user-fields.schema.ts
@@ -2,8 +2,15 @@ import { z } from "zod";
 
 export const usernameSchema = z
   .string()
+  .regex(/^[a-zA-Z0-9_]+$/, {
+    message:
+      "Invalid username format. Only letters, numbers, and underscores are allowed.",
+  })
   .min(3, { message: "Username must be at least 3 characters long." })
-  .max(20, { message: "Username must be at most 20 characters long." });
+  .max(20, { message: "Username must be at most 20 characters long." })
+  .refine((value) => !/['"<>;(){}]/.test(value), {
+    message: "Username cannot contain special characters like '\"<>;(){}.",
+  });
 
 export const emailSchema = z
   .string()
@@ -12,9 +19,12 @@ export const emailSchema = z
 
 export const passwordSchema = z
   .string()
-  .min(8, { message: "Password must be at least 8 characters." })
-  .max(128, { message: "Password must be at most 128 characters." })
   .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^a-zA-Z0-9]).*$/, {
     message:
       "Password must include a lowercase, uppercase, number, and special character.",
+  })
+  .min(8, { message: "Password must be at least 8 characters." })
+  .max(128, { message: "Password must be at most 128 characters." })
+  .refine((value) => !/['"<>;(){}]/.test(value), {
+    message: "Password cannot contain special characters like '\"<>;(){}.",
   });
