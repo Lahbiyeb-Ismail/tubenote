5a0bb0f7fe7dadf0d4628ce922f1f63af9a0501a
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-04-12 15:55:30 +0100

    refactor(hooks/useRegister): streamline success and error handling in registration process

--- apps/client/src/hooks/auth/useRegister.tsx
+++ apps/client/src/hooks/auth/useRegister.tsx
@@ -18,35 +18,27 @@ function useRegister(dispatch: React.Dispatch<AuthAction>) {
     onMutate: () => {
       toast.loading("Registering...", { id: "loadingToast" });
     },
-    onSuccess: (data) => {
+    onSuccess: (responseData) => {
+      const { payload } = responseData;
+
       toast.dismiss("loadingToast");
 
-      toast.success(data.message);
+      toast.success(payload.message);
 
       queryClient.invalidateQueries({ queryKey: ["user"] });
 
-      localStorage.setItem("userEmail", data.email);
+      localStorage.setItem("userEmail", payload.data);
 
       router.push("/verify-email");
     },
     onError: (error: TypedError) => {
       toast.dismiss("loadingToast");
+      toast.error(error.message);
 
-      if (error.response) {
-        toast.error(error.response.data.message);
-
-        dispatch({
-          type: "REQUEST_FAIL",
-          payload: { errorMessage: error.response.data.message },
-        });
-      } else {
-        toast.error("Failed Registration. Please try again.");
-
-        dispatch({
-          type: "REQUEST_FAIL",
-          payload: { errorMessage: "Failed Registration. Please try again." },
-        });
-      }
+      dispatch({
+        type: "REQUEST_FAIL",
+        payload: { errorMessage: error.message },
+      });
     },
   });
 }
