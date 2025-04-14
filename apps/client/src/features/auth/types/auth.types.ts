44b5a9b7ccf845c390e5e6056b9566da334e65d1
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-04-14 16:43:18 +0100

    refactor(auth): update authentication types and remove unused schemas

--- apps/client/src/features/auth/types/auth.types.ts
+++ apps/client/src/features/auth/types/auth.types.ts
@@ -1,18 +1,6 @@
-import type { z } from "zod";
-
-import type {
-  loginFormSchema,
-  registerFormSchema,
-  updatePasswordSchema,
-  updateProfileSchema,
-} from "@/lib/schemas";
 import type { ReactNode } from "react";
 
-export type LoginFormData = z.infer<typeof loginFormSchema>;
-export type RegisterFormData = z.infer<typeof registerFormSchema>;
-
-export type UpdateProfileData = z.infer<typeof updateProfileSchema>;
-export type UpdatePasswordData = z.infer<typeof updatePasswordSchema>;
+import type { ILoginDto, IRegisterDto } from "@tubenote/dtos";
 
 export type AuthState = {
   accessToken: string | null;
@@ -32,8 +20,8 @@ export type AuthAction =
 
 export type AuthContextType = {
   state: AuthState;
-  login: (loginCredentials: LoginFormData) => void;
-  register: (registerCredentials: RegisterFormData) => void;
+  login: (loginDto: ILoginDto) => void;
+  register: (registerDto: IRegisterDto) => void;
   logout: () => void;
   isLoading: boolean;
 };
