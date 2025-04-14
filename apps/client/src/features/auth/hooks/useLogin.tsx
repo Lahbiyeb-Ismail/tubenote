41605b8cd5ff789ea57d7e0c5d6360b8bf7cb2e3
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-04-14 15:39:29 +0100

    feat(auth/hooks): add useLogin, useLogout, and useRegister hooks for authentication

---
+++ apps/client/src/features/auth/hooks/useLogin.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
+import toast from "react-hot-toast";
+
+import { loginUser } from "@/actions/auth.actions";
+
+import useGetCurrentUser from "@/hooks/user/useGetCurrentUser";
+import type { AuthAction } from "@/types/auth.types";
+import { setStorageValue } from "@/utils/localStorage";
+
+export function useLogin(dispatch: React.Dispatch<AuthAction>) {
+  const queryClient = useQueryClient();
+  const router = useRouter();
+
+  const { refetch: refetchCurrentUser } = useGetCurrentUser();
+
+  return useMutation({
+    mutationFn: loginUser,
+    onMutate: () => {
+      toast.loading("Logging in...", { id: "loadingToast" });
+    },
+    onSuccess: async (responseData) => {
+      const { payload } = responseData;
+
+      toast.dismiss("loadingToast");
+
+      toast.success(payload.message);
+
+      queryClient.invalidateQueries({ queryKey: ["user", "current-user"] });
+
+      setStorageValue("accessToken", payload.data);
+
+      dispatch({
+        type: "LOGIN_SUCCESS",
+        payload: {
+          message: payload.message,
+          accessToken: payload.data,
+        },
+      });
+
+      await refetchCurrentUser();
+
+      // Redirect to dashboard after successful login
+      router.push("/dashboard");
+    },
+    onError: (error) => {
+      toast.dismiss("loadingToast");
+      toast.error(error.message);
+    },
+  });
+}
---
+++ apps/client/src/features/auth/hooks/useRegister.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
+import toast from "react-hot-toast";
+
+import { registerUser } from "@/actions/auth.actions";
+
+import type { TypedError } from "@/types";
+import type { AuthAction } from "@/types/auth.types";
+
+export function useRegister(dispatch: React.Dispatch<AuthAction>) {
+  const queryClient = useQueryClient();
+  const router = useRouter();
+
+  return useMutation({
+    mutationFn: registerUser,
+    onMutate: () => {
+      toast.loading("Registering...", { id: "loadingToast" });
+    },
+    onSuccess: (responseData) => {
+      const { payload } = responseData;
+
+      toast.dismiss("loadingToast");
+
+      toast.success(payload.message);
+
+      queryClient.invalidateQueries({ queryKey: ["user"] });
+
+      localStorage.setItem("userEmail", payload.data);
+
+      router.push("/verify-email");
+    },
+    onError: (error: TypedError) => {
+      toast.dismiss("loadingToast");
+      toast.error(error.message);
+
+      dispatch({
+        type: "REQUEST_FAIL",
+        payload: { errorMessage: error.message },
+      });
+    },
+  });
+}
