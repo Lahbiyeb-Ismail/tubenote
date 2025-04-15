bc2e7fd16a4195acbbf3caf98a1616f99e5c29fa
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-04-15 15:00:04 +0100

    refactor(auth/hooks): update resetPassword mutation to use new service path & improve error handling

--- apps/client/src/features/auth/hooks/useResetPassword.tsx
+++ apps/client/src/features/auth/hooks/useResetPassword.tsx
@@ -2,8 +2,7 @@ import { useMutation } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
 import toast from "react-hot-toast";
 
-import { resetPassword } from "@/actions/password.actions";
-import type { TypedError } from "@/types";
+import { resetPassword } from "../services";
 
 export function useResetPassword() {
   const router = useRouter();
@@ -13,20 +12,19 @@ export function useResetPassword() {
     onMutate: () => {
       toast.loading("Sending...", { id: "loadingToast" });
     },
-    onSuccess: async (data) => {
+    onSuccess: async (response) => {
+      const { payload } = response;
+
       toast.dismiss("loadingToast");
 
-      toast.success(data.message);
+      toast.success(payload.message);
 
       router.push("/login?resetSuccess=true");
     },
-    onError: (error: TypedError) => {
+    onError: (error) => {
       toast.dismiss("loadingToast");
-      if (error.response) {
-        toast.error(error.response.data.message);
-      } else {
-        toast.error("Password Reset Faild. Please try again.");
-      }
+
+      toast.error(error.message);
     },
   });
 }
