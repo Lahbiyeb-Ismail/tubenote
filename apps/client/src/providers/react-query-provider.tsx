cad9fb972ee6b6a53a80f2d8c39ddb554f9d313a
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-04-24 15:02:23 +0100

    refactor(providers): export ReactQueryProvider for better accessibility

--- apps/client/src/providers/react-query-provider.tsx
+++ apps/client/src/providers/react-query-provider.tsx
@@ -3,12 +3,12 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
 
-function ReactQueryProvider({ children }: { children: React.ReactNode }) {
+export function ReactQueryProvider({
+  children,
+}: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
 }
-
-export default ReactQueryProvider;
