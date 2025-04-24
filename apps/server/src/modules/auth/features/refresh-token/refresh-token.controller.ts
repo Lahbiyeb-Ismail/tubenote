75b55ef73235e6027e841f1b79dbf371400a125b
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-04-24 17:55:49 +0100

    refactor(refresh-token): remove unused cookie configurations and streamline refreshToken method

--- apps/server/src/modules/auth/features/refresh-token/refresh-token.controller.ts
+++ apps/server/src/modules/auth/features/refresh-token/refresh-token.controller.ts
@@ -6,8 +6,8 @@ import type { TypedRequest } from "@/modules/shared/types";
 
 import {
   accessTokenCookieConfig,
-  clearAuthTokenCookieConfig,
-  refreshTokenCookieConfig,
+  // clearAuthTokenCookieConfig,
+  // refreshTokenCookieConfig,
 } from "@/modules/auth/config";
 import {
   ACCESS_TOKEN_NAME,
@@ -52,39 +52,20 @@ export class RefreshTokenController implements IRefreshTokenController {
    * @throws {UnauthorizedError} If the refresh token is not provided.
    */
   async refreshToken(req: TypedRequest, res: Response): Promise<void> {
-    try {
-      const cookies = req.cookies;
-      const userId = req.userId;
+    const cookies = req.cookies;
+    const userId = req.userId;
 
-      const refreshTokenCookie = cookies[REFRESH_TOKEN_NAME];
+    const refreshTokenCookie = cookies[REFRESH_TOKEN_NAME];
 
-      if (!refreshTokenCookie || typeof refreshTokenCookie !== "string") {
-        throw new UnauthorizedError(ERROR_MESSAGES.UNAUTHORIZED);
-      }
-
-      const { accessToken, refreshToken } =
-        await this._refreshTokenService.refreshToken(
-          userId,
-          refreshTokenCookie
-        );
-
-      const formattedResponse =
-        this._responseFormatter.formatSuccessResponse<string>({
-          responseOptions: {
-            data: accessToken,
-            message: "Access token refreshed successfully.",
-          },
-        });
-
-      res.cookie(REFRESH_TOKEN_NAME, refreshToken, refreshTokenCookieConfig);
-      res.cookie(ACCESS_TOKEN_NAME, accessToken, accessTokenCookieConfig);
-
-      res.status(formattedResponse.statusCode).json(formattedResponse);
-    } catch (error) {
-      res.clearCookie(REFRESH_TOKEN_NAME, clearAuthTokenCookieConfig);
-      res.clearCookie(ACCESS_TOKEN_NAME, clearAuthTokenCookieConfig);
-
-      throw error;
+    if (!refreshTokenCookie || typeof refreshTokenCookie !== "string") {
+      throw new UnauthorizedError(ERROR_MESSAGES.UNAUTHORIZED);
     }
+
+    const accessToken = await this._refreshTokenService.refreshToken(
+      userId,
+      refreshTokenCookie
+    );
+
+    res.cookie(ACCESS_TOKEN_NAME, accessToken, accessTokenCookieConfig);
   }
 }
