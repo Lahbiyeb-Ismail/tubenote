271b0643362b624ba7fa2d509acf61fd99476890
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-05-02 14:53:41 +0100

    refactor(refresh): simplify tokenValidation by removing hint-based lookup & directly using tokenHash

--- apps/server/src/modules/auth/features/refresh-token/refresh-token.service.ts
+++ apps/server/src/modules/auth/features/refresh-token/refresh-token.service.ts
@@ -53,24 +53,9 @@ export class RefreshTokenService implements IRefreshTokenService {
   private async validateRefreshToken(
     rawToken: string
   ): Promise<RefreshToken | null> {
-    const hint = rawToken.slice(0, 16);
+    const tokenHash = this._cryptoService.generateUnsaltedHash(rawToken);
 
-    // First filter by hint
-    const candidates = await this._refreshTokenRepository.findByHint(hint);
-
-    // Then compare full hashes
-    for (const token of candidates) {
-      if (
-        await this._cryptoService.validateHashMatch({
-          hashedValue: token.tokenHash,
-          unhashedValue: rawToken,
-        })
-      ) {
-        return token;
-      }
-    }
-
-    return null;
+    return this._refreshTokenRepository.findByToken(tokenHash);
   }
 
   async refreshTokens(
@@ -81,25 +66,8 @@ export class RefreshTokenService implements IRefreshTokenService {
 
     // Check if the token is valid
     if (!tokenRecord) {
-      // this._loggerService.warn(
-      //   `Invalid refresh token attempt for user ${userId}`
-      // );
-
       throw new UnauthorizedError(ERROR_MESSAGES.UNAUTHORIZED);
     }
-
-    // if (tokenRecord.userId !== userId) {
-    //   await this._refreshTokenRepository.markAsRevoked(
-    //     tokenRecord.id,
-    //     "suspicious_activity"
-    //   );
-
-    //   this._loggerService.warn(
-    //     `Invalid refresh token attempt for user ${userId}`
-    //   );
-
-    //   throw new UnauthorizedError(ERROR_MESSAGES.UNAUTHORIZED);
-    // }
 
     // Token rotation (revoke old, create new)
     const newRefreshToken = await this._prismaService.transaction(
@@ -132,34 +100,23 @@ export class RefreshTokenService implements IRefreshTokenService {
     clientContext: IClientContext,
     tx?: Prisma.TransactionClient
   ): Promise<string> {
-    // Enforce token limits
-    const activeTokensCount =
-      await this._refreshTokenRepository.countActiveTokens(userId);
+    const randomToken = this._cryptoService.generateSecureToken();
 
-    if (activeTokensCount >= this.MAX_TOKENS_PER_USER) {
-      const excess = activeTokensCount - this.MAX_TOKENS_PER_USER + 1;
-      await this._refreshTokenRepository.revokeOldestTokens(userId, excess, tx);
-    }
-
-    const token = this._cryptoService.generateSecureToken();
-
-    const tokenHash = await this._cryptoService.generateHash(token);
-    const hint = token.slice(0, 16); // First 16 chars as lookup hint
+    const tokenHash = this._cryptoService.generateUnsaltedHash(randomToken);
 
     const expiresAt = stringToDate(REFRESH_TOKEN_EXPIRES_IN);
 
     await this._refreshTokenRepository.create(
       userId,
       {
-        tokenHash,
+        token: tokenHash,
         expiresAt,
-        hint,
         ...clientContext,
       },
       tx
     );
 
-    return token;
+    return randomToken;
   }
 
   async markTokenAsRevoked(
