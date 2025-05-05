adfd881fca31fc3694573f6aa46fcae2d31caa2f
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-05-05 14:42:46 +0100

    refactor(user): enhance useGetCurrentUser hook with type annotations and data selection

--- apps/client/src/features/user/hooks/use-get-current-user.tsx
+++ apps/client/src/features/user/hooks/use-get-current-user.tsx
@@ -1,13 +1,18 @@
 "use client";
 
-import { useQuery } from "@tanstack/react-query";
+import { type UseQueryResult, useQuery } from "@tanstack/react-query";
+
+import type { User } from "@tubenote/types";
 
 import { getCurrentUser } from "../services";
 
-export function useGetCurrentUser(isAuthenticated: boolean) {
+export function useGetCurrentUser(
+  isAuthenticated: boolean
+): UseQueryResult<User, Error> {
   return useQuery({
     queryKey: ["current-user", isAuthenticated],
     queryFn: getCurrentUser,
+    select: (response) => response.payload.data,
     // Enable the query only if the user is authenticated.
     enabled: isAuthenticated,
     // The data is considered fresh for 5 minutes, after which it will be refetched.
