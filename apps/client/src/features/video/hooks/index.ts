760c63bff4dc8a7f3d5a4fb05b9e307602d5a335
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-05-05 17:15:51 +0100

    refactor(video): remove useGetVideoData hook and update exports

--- apps/client/src/features/video/hooks/index.ts
+++ apps/client/src/features/video/hooks/index.ts
@@ -1,4 +1,4 @@
 export * from "./use-get-user-videos";
 export * from "./use-get-video-by-id";
-export * from "./use-get-video-data";
 export * from "./use-get-video-notes";
+export * from "./use-save-video-data";
