a0f7db111de178da31a2adc44c486e9aca19b8e2
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-05-07 14:16:16 +0100

    feat(csrf): add request interceptor to include CSRF token for state-changing requests

--- apps/client/src/lib/axios-lib.ts
+++ apps/client/src/lib/axios-lib.ts
@@ -1,8 +1,20 @@
-import axios from "axios";
-
 import { API_URL } from "@/utils";
+import { addCsrfTokenToRequest } from "@/utils/csrf/csrf.utils";
+import axios from "axios";
 
 export const axiosInstance = axios.create({
   baseURL: API_URL,
   withCredentials: true,
+});
+
+// Add request interceptor to include CSRF token in all state-changing requests
+axiosInstance.interceptors.request.use((config) => {
+  // Only add CSRF token for state-changing methods
+  if (
+    config.method &&
+    ["post", "put", "patch", "delete"].includes(config.method.toLowerCase())
+  ) {
+    return addCsrfTokenToRequest(config);
+  }
+  return config;
 });
