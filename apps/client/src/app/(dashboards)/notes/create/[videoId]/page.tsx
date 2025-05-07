640306c8da1139a4b2bbb796fc0d94d8cf0a7a87
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-05-07 17:10:45 +0100

    feat(note): enhance CreateNotePage and UpdateNotePage with modal confirmation and save functionality

--- apps/client/src/app/(dashboards)/notes/create/[videoId]/page.tsx
+++ apps/client/src/app/(dashboards)/notes/create/[videoId]/page.tsx
@@ -1,16 +1,35 @@
 "use client";
 
+import type { MDXEditorMethods } from "@mdxeditor/editor";
+import { useRef } from "react";
+
+import { useModal } from "@/context";
+import { useNote } from "@/features/note/contexts";
+import { useVideo } from "@/features/video/contexts";
+
 import { useGetVideoById } from "@/features/video/hooks";
 
-import { EditorPage } from "@/components/editor";
-import { Loader } from "@/components/global";
+import { VideoPlayer } from "@/features/video/components";
+
+import { AppMDXEditor, SaveNoteForm } from "@/components/editor";
+import {
+  ConfirmationModal,
+  Loader,
+  ResizablePanels,
+  SaveButton,
+} from "@/components/global";
 
 function CreateNotePage({ params }: { params: { videoId: string } }) {
   const { videoId } = params;
+  const editorRef = useRef<MDXEditorMethods | null>(null);
 
-  const { data, isLoading } = useGetVideoById(videoId);
+  const { openModal, closeModal } = useModal();
+  const { createNote, isLoading: isSavingNote } = useNote();
+  const { videoCurrentTime } = useVideo();
 
-  if (isLoading || !data) {
+  const { data: videoData, isLoading } = useGetVideoById(videoId);
+
+  if (isLoading || !videoData) {
     return (
       <div className="min-h-screen flex items-center justify-center container max-w-4xl mx-auto px-4 py-8">
         <Loader />
@@ -18,7 +37,47 @@ function CreateNotePage({ params }: { params: { videoId: string } }) {
     );
   }
 
-  return <EditorPage video={data} />;
+  const handleCreateNote = (noteTitle: string) => {
+    const noteContent = editorRef.current?.getMarkdown() || "";
+
+    createNote({
+      videoId: videoData.id,
+      createNoteData: {
+        title: noteTitle,
+        content: noteContent,
+        thumbnail: videoData.thumbnails.medium.url,
+        videoTitle: videoData.title,
+        youtubeId: videoData.youtubeId,
+        timestamp: videoCurrentTime,
+      },
+    });
+  };
+
+  return (
+    <>
+      <div className="flex h-screen bg-white">
+        <ResizablePanels
+          leftSideContent={<AppMDXEditor editorRef={editorRef} />}
+          rightSideContent={<VideoPlayer videoId={videoId} />}
+        />
+
+        <SaveButton
+          className="absolute bottom-3 right-[48%]"
+          onClick={openModal}
+        />
+      </div>
+      <ConfirmationModal
+        title="Confirm Save Note"
+        description="Are you sure you want to save this note?"
+      >
+        <SaveNoteForm
+          isLoading={isSavingNote}
+          closeModal={closeModal}
+          handleSaveSubmit={handleCreateNote}
+        />
+      </ConfirmationModal>
+    </>
+  );
 }
 
 export default CreateNotePage;
--- apps/client/src/app/(dashboards)/notes/update/[noteId]/page.tsx
+++ apps/client/src/app/(dashboards)/notes/update/[noteId]/page.tsx
@@ -1,17 +1,30 @@
 "use client";
 
 import type { MDXEditorMethods } from "@mdxeditor/editor";
-import { Suspense, useRef } from "react";
+import { useRef } from "react";
 
-import AppMDXEditor from "@/components/editor/mdx-editor";
-
-import { Loader, ResizablePanels } from "@/components/global";
 import { useGetNoteById } from "@/features/note/hooks";
+
 import { VideoPlayer } from "@/features/video/components";
+
+import { AppMDXEditor, SaveNoteForm } from "@/components/editor";
+import {
+  ConfirmationModal,
+  Loader,
+  ResizablePanels,
+  SaveButton,
+} from "@/components/global";
+import { useModal } from "@/context";
+import { useNote } from "@/features/note/contexts";
+import { useVideo } from "@/features/video/contexts";
 
 function UpdateNotePage({ params }: { params: { noteId: string } }) {
   const { noteId } = params;
   const editorRef = useRef<MDXEditorMethods | null>(null);
+
+  const { openModal, closeModal } = useModal();
+  const { updateNote, isLoading: isUpdatingNote } = useNote();
+  const { videoCurrentTime } = useVideo();
 
   const { data: note, isLoading, isError } = useGetNoteById(noteId);
 
@@ -27,17 +40,45 @@ function UpdateNotePage({ params }: { params: { noteId: string } }) {
     return <div>Error...</div>;
   }
 
+  const handleUpdateNote = (noteTitle: string) => {
+    const noteContent = editorRef.current?.getMarkdown() || "";
+
+    updateNote({
+      noteId: note.id,
+      updateData: {
+        title: noteTitle,
+        content: noteContent,
+        timestamp: videoCurrentTime,
+      },
+    });
+  };
+
   return (
-    <div className="flex h-screen bg-white">
-      <ResizablePanels
-        leftSideContent={
-          <Suspense fallback={null}>
+    <>
+      <div className="flex h-screen bg-white">
+        <ResizablePanels
+          leftSideContent={
             <AppMDXEditor editorRef={editorRef} noteContent={note.content} />
-          </Suspense>
-        }
-        rightSideContent={<VideoPlayer videoId={note.youtubeId} />}
-      />
-    </div>
+          }
+          rightSideContent={<VideoPlayer videoId={note.youtubeId} />}
+        />
+
+        <SaveButton
+          className="absolute bottom-3 right-[48%]"
+          onClick={openModal}
+        />
+      </div>
+      <ConfirmationModal
+        title="Confirm Update Note"
+        description="Are you sure you want to update this note?"
+      >
+        <SaveNoteForm
+          isLoading={isUpdatingNote}
+          closeModal={closeModal}
+          handleSaveSubmit={handleUpdateNote}
+        />
+      </ConfirmationModal>
+    </>
   );
 }
 
