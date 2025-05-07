4305b98602a6ece64857b3a8bc85bae6c6e900c4
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-05-07 17:29:03 +0100

    feat(note): refactor CreateNotePage and UpdateNotePage to use NotePageLayout for improved structure

--- apps/client/src/app/(dashboards)/notes/create/[videoId]/page.tsx
+++ apps/client/src/app/(dashboards)/notes/create/[videoId]/page.tsx
@@ -1,50 +1,25 @@
 "use client";
 
-import type { MDXEditorMethods } from "@mdxeditor/editor";
-import { useRef } from "react";
-
-import { useModal } from "@/context";
 import { useNote } from "@/features/note/contexts";
 import { useVideo } from "@/features/video/contexts";
-
 import { useGetVideoById } from "@/features/video/hooks";
 
-import { VideoPlayer } from "@/features/video/components";
-
-import { AppMDXEditor, SaveNoteForm } from "@/components/editor";
-import {
-  ConfirmationModal,
-  Loader,
-  ResizablePanels,
-  SaveButton,
-} from "@/components/global";
+import { NotePageLayout } from "@/features/note/components";
 
 function CreateNotePage({ params }: { params: { videoId: string } }) {
   const { videoId } = params;
-  const editorRef = useRef<MDXEditorMethods | null>(null);
-
-  const { openModal, closeModal } = useModal();
   const { createNote, isLoading: isSavingNote } = useNote();
   const { videoCurrentTime } = useVideo();
-
   const { data: videoData, isLoading } = useGetVideoById(videoId);
 
-  if (isLoading || !videoData) {
-    return (
-      <div className="min-h-screen flex items-center justify-center container max-w-4xl mx-auto px-4 py-8">
-        <Loader />
-      </div>
-    );
-  }
-
-  const handleCreateNote = (noteTitle: string) => {
-    const noteContent = editorRef.current?.getMarkdown() || "";
+  const handleCreateNote = (noteTitle: string, content: string) => {
+    if (!videoData) return;
 
     createNote({
       videoId: videoData.id,
       createNoteData: {
         title: noteTitle,
-        content: noteContent,
+        content: content,
         thumbnail: videoData.thumbnails.medium.url,
         videoTitle: videoData.title,
         youtubeId: videoData.youtubeId,
@@ -54,29 +29,14 @@ function CreateNotePage({ params }: { params: { videoId: string } }) {
   };
 
   return (
-    <>
-      <div className="flex h-screen bg-white">
-        <ResizablePanels
-          leftSideContent={<AppMDXEditor editorRef={editorRef} />}
-          rightSideContent={<VideoPlayer videoId={videoId} />}
-        />
-
-        <SaveButton
-          className="absolute bottom-3 right-[48%]"
-          onClick={openModal}
-        />
-      </div>
-      <ConfirmationModal
-        title="Confirm Save Note"
-        description="Are you sure you want to save this note?"
-      >
-        <SaveNoteForm
-          isLoading={isSavingNote}
-          closeModal={closeModal}
-          handleSaveSubmit={handleCreateNote}
-        />
-      </ConfirmationModal>
-    </>
+    <NotePageLayout
+      videoId={videoId}
+      isLoading={isLoading || !videoData}
+      isSavingNote={isSavingNote}
+      modalTitle="Confirm Save Note"
+      modalDescription="Are you sure you want to save this note?"
+      handleSaveNote={handleCreateNote}
+    />
   );
 }
 
--- apps/client/src/app/(dashboards)/notes/update/[noteId]/page.tsx
+++ apps/client/src/app/(dashboards)/notes/update/[noteId]/page.tsx
@@ -1,84 +1,44 @@
 "use client";
 
-import type { MDXEditorMethods } from "@mdxeditor/editor";
-import { useRef } from "react";
-
-import { useGetNoteById } from "@/features/note/hooks";
-
-import { VideoPlayer } from "@/features/video/components";
-
-import { AppMDXEditor, SaveNoteForm } from "@/components/editor";
-import {
-  ConfirmationModal,
-  Loader,
-  ResizablePanels,
-  SaveButton,
-} from "@/components/global";
-import { useModal } from "@/context";
 import { useNote } from "@/features/note/contexts";
+import { useGetNoteById } from "@/features/note/hooks";
 import { useVideo } from "@/features/video/contexts";
+
+import { NotePageLayout } from "@/features/note/components";
 
 function UpdateNotePage({ params }: { params: { noteId: string } }) {
   const { noteId } = params;
-  const editorRef = useRef<MDXEditorMethods | null>(null);
-
-  const { openModal, closeModal } = useModal();
   const { updateNote, isLoading: isUpdatingNote } = useNote();
   const { videoCurrentTime } = useVideo();
-
   const { data: note, isLoading, isError } = useGetNoteById(noteId);
 
-  if (isLoading || !note) {
-    return (
-      <div className="min-h-screen flex items-center justify-center container max-w-4xl mx-auto px-4 py-8">
-        <Loader />
-      </div>
-    );
-  }
-
-  if (isError) {
-    return <div>Error...</div>;
-  }
-
-  const handleUpdateNote = (noteTitle: string) => {
-    const noteContent = editorRef.current?.getMarkdown() || "";
+  const handleUpdateNote = (noteTitle: string, content: string) => {
+    if (!note) return;
 
     updateNote({
       noteId: note.id,
       updateData: {
         title: noteTitle,
-        content: noteContent,
+        content: content,
         timestamp: videoCurrentTime,
       },
     });
   };
 
-  return (
-    <>
-      <div className="flex h-screen bg-white">
-        <ResizablePanels
-          leftSideContent={
-            <AppMDXEditor editorRef={editorRef} noteContent={note.content} />
-          }
-          rightSideContent={<VideoPlayer videoId={note.youtubeId} />}
-        />
+  if (isError) {
+    return <div>Error...</div>;
+  }
 
-        <SaveButton
-          className="absolute bottom-3 right-[48%]"
-          onClick={openModal}
-        />
-      </div>
-      <ConfirmationModal
-        title="Confirm Update Note"
-        description="Are you sure you want to update this note?"
-      >
-        <SaveNoteForm
-          isLoading={isUpdatingNote}
-          closeModal={closeModal}
-          handleSaveSubmit={handleUpdateNote}
-        />
-      </ConfirmationModal>
-    </>
+  return (
+    <NotePageLayout
+      videoId={note?.youtubeId || ""}
+      noteContent={note?.content}
+      isLoading={isLoading || !note}
+      isSavingNote={isUpdatingNote}
+      modalTitle="Confirm Update Note"
+      modalDescription="Are you sure you want to update this note?"
+      handleSaveNote={handleUpdateNote}
+    />
   );
 }
 
