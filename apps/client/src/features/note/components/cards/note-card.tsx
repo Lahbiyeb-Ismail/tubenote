fa08417f6d4cb5ee683e889e90f32c0255fbf222
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-05-08 14:33:53 +0100

    feat(note): refactor NoteCard component to streamline delete functionality and improve layout

--- apps/client/src/features/note/components/cards/note-card.tsx
+++ apps/client/src/features/note/components/cards/note-card.tsx
@@ -11,75 +11,58 @@ import {
   CardImage,
   CardSettingsButton,
   CardWrapper,
-  ConfirmationModal,
 } from "@/components/global";
 
-import { Button, DialogFooter } from "@/components/ui";
 import { DeleteNoteButton, EditNoteButton } from "../buttons";
 
 type NoteCardProps = {
   note: Note;
+  onDeleteClick: () => void;
 };
 
-export function NoteCard({ note }: NoteCardProps) {
+export function NoteCard({ note, onDeleteClick }: NoteCardProps) {
   const { isGridLayout } = useLayout();
-  const { deleteNote, isLoading: isDeletingNote } = useNote();
-  const { openModal, closeModal } = useModal();
+  const { isLoading: isDeletingNote } = useNote();
+  const { openModal } = useModal();
+
+  const handleDelete = () => {
+    onDeleteClick();
+    openModal();
+  };
 
   return (
-    <>
-      <CardWrapper>
-        <CardImage
-          src={note.thumbnail}
-          alt={note.title}
+    <CardWrapper>
+      <CardImage
+        src={note.thumbnail}
+        alt={note.title}
+        isGridLayout={isGridLayout}
+      />
+      <div
+        className={`flex-grow ${isGridLayout ? "" : "flex flex-col justify-between"}`}
+      >
+        <div className="flex justify-end p-2">
+          <CardSettingsButton
+            noteId={note.id}
+            onDelete={handleDelete}
+            onExport={() => {
+              console.log("Export as PDF");
+            }}
+          />
+        </div>
+        <CardContent
+          cardTitle={note.videoTitle}
+          cardDescription={`Note Title: ${note.title}`}
+          href={`/notes/${note.id}`}
           isGridLayout={isGridLayout}
         />
-        <div
-          className={`flex-grow ${
-            isGridLayout ? "" : "flex flex-col justify-between"
-          }`}
-        >
-          <div className="flex justify-end p-2">
-            <CardSettingsButton
-              noteId={note.id}
-              onDelete={openModal}
-              onExport={() => {
-                console.log("Export as PDF");
-              }}
-            />
-          </div>
-          <CardContent
-            cardTitle={note.videoTitle}
-            cardDescription={`Note Title: ${note.title}`}
-            href={`/notes/${note.id}`}
-            isGridLayout={isGridLayout}
+        <CardFooterWrapper>
+          <EditNoteButton noteId={note.id} isLoading={isDeletingNote} />
+          <DeleteNoteButton
+            isLoading={isDeletingNote}
+            onDelete={handleDelete}
           />
-          <CardFooterWrapper>
-            <EditNoteButton noteId={note.id} isLoading={isDeletingNote} />
-            <DeleteNoteButton isLoading={isDeletingNote} onDelete={openModal} />
-          </CardFooterWrapper>
-        </div>
-      </CardWrapper>
-
-      <ConfirmationModal
-        title="Confirm Note Deletion"
-        description="Are you sure you want to delete this note? This action cannot be undone."
-      >
-        <DialogFooter>
-          <Button variant="outline" onClick={closeModal}>
-            Cancel
-          </Button>
-          <Button
-            variant="destructive"
-            onClick={() => {
-              deleteNote(note.id);
-              closeModal();
-            }}
-          >
-            Delete
-          </Button>
-        </DialogFooter>
-      </ConfirmationModal>
-    </>
+        </CardFooterWrapper>
+      </div>
+    </CardWrapper>
   );
 }
