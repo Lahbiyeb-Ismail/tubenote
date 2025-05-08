63549fd420cc0d85f6aa1f07adda7668aa378a7b
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-05-08 17:40:13 +0100

    feat(client): add index file to export all Zustand stores

---
+++ apps/client/src/stores/index.ts
@@ -0,0 +1,6 @@
+// Export all stores for easy access
+export * from "./ui-store";
+export * from "./auth-store";
+export * from "./note-store";
+export * from "./video-store";
+export * from "./user-store";
