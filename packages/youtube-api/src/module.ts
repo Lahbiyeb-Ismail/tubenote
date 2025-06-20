1892c664a246e0569e36603d83d9980d3da28d05
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-06-20 17:16:21 +0100

    refactor(youtube-api): update module exports with new service structure
    Updated the module exports to use the new dedicated service classes
    (YoutubeApiService and TranscriptService) instead of the monolithic service,
    providing a cleaner API for consumers of the package.

--- packages/youtube-api/src/module.ts
+++ packages/youtube-api/src/module.ts
@@ -1,3 +1,5 @@
-import { YoutubeApiService } from "./service";
+import { TranscriptService, YoutubeApiService } from "./services";
 
 export const youtubeApiService = new YoutubeApiService();
+
+export const youtubeTranscriptService = new TranscriptService();
