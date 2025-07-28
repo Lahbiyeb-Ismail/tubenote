dc7665fc70128cf4e3df0e182d0a48ae9aae7c0c
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-07-28 14:47:20 +0100

    feat(bff): integrate video routes into the main application
    This commit integrates the newly created video routes into the main application router.
    By adding videoRoutes to the appRoutes, all endpoints defined within the video module are
    now exposed under the /videos path.
    
    This change makes the video API accessible to the client, enabling functionalities such as
    fetching, viewing, and managing videos through the BFF layer.

--- apps/bff/src/routes/index.ts
+++ apps/bff/src/routes/index.ts
@@ -3,11 +3,13 @@ import { Router } from "express";
 import { authRoutes } from "@/modules/auth";
 import { noteRoutes } from "@/modules/note";
 import { userRoutes } from "@/modules/user";
+import { videoRoutes } from "@/modules/video";
 
 const appRoutes: Router = Router();
 
 appRoutes.use("/auth", authRoutes);
 appRoutes.use("/users", userRoutes);
 appRoutes.use("/notes", noteRoutes);
+appRoutes.use("/videos", videoRoutes);
 
 export { appRoutes };
