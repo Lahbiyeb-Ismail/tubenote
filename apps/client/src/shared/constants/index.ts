d16e79347935a5ce04dec9a0688493d3c0d61ffc
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-07-28 15:41:21 +0100

    chore(client): update api url to point to bff
    This commit updates the API_URL constant to point to the BFF's API endpoint
    (NEXT_PUBLIC_BFF_API_URL) instead of the main backend's.
    
    This change is a crucial step in migrating the client application to communicate exclusively
    through the BFF, which will improve security, performance, and maintainability.
    All subsequent API requests from the client will now be routed through the BFF.

--- apps/client/src/shared/constants/index.ts
+++ apps/client/src/shared/constants/index.ts
@@ -1,4 +1,4 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL;
+export const API_URL = process.env.NEXT_PUBLIC_BFF_API_URL;
 
 export const GOOGLE_CLIENT_ID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
 
