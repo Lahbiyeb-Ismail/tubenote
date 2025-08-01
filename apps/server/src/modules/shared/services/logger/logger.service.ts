f08d9014ad2781ac8a0691e8182e9ca060a62049
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-08-01 15:06:34 +0100

    refactor(server): simplify LoggerService implementation
    The LoggerService is simplified by directly extending the base Logger class from the
    @tubenote/logger package.
    
    This change removes the need for wrapper methods for each log level (error, warn, info, http,
    debug), resulting in a cleaner and more concise implementation. By leveraging inheritance, the
    LoggerService now directly inherits all the functionality of the base Logger, improving code
    readability and maintainability.

--- apps/server/src/modules/shared/services/logger/logger.service.ts
+++ apps/server/src/modules/shared/services/logger/logger.service.ts
@@ -1,50 +1,8 @@
-import type { LogMethod } from "@tubenote/logger";
-
 import { Logger } from "@tubenote/logger";
 import { injectable } from "inversify";
 
 import type { ILoggerService } from "./logger.types";
 
 @injectable()
-export class LoggerService implements ILoggerService {
-  private logger: ILoggerService;
-
-  constructor() {
-    // Create logger instance
-    this.logger = new Logger();
-  }
-
-  // Log methods
-  public error: LogMethod = (message: string, meta?: any) => {
-    this.logger.error(message, meta);
-  };
-
-  public warn: LogMethod = (message: string, meta?: any) => {
-    this.logger.warn(message, meta);
-  };
-
-  public info: LogMethod = (message: string, meta?: any) => {
-    this.logger.info(message, meta);
-  };
-
-  public http: LogMethod = (message: string, meta?: any) => {
-    this.logger.http(message, meta);
-  };
-
-  public debug: LogMethod = (message: string, meta?: any) => {
-    this.logger.debug(message, meta);
-  };
-
-  // Utility methods
-  // public getLogger(): winston.Logger {
-  //   return this.logger;
-  // }
-
-  // public addTransport(transport: winston.transport): void {
-  //   this.logger.add(transport);
-  // }
-
-  // public clearTransports(): void {
-  //   this.logger.clear();
-  // }
+export class LoggerService extends Logger implements ILoggerService {
 }
